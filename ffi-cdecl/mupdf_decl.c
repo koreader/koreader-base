16098b2056b7c0424d980fd28db2370b8b6ffce6
HW <hwhilse@googlemail.com>
2013-10-17 20:44:05 +0200

    factor out mupdf declarations to seperate, autogenerated file

---
+++ ffi-cdecl/mupdf_decl.c
@@ -0,0 +1,23 @@
+#include <mupdf/fitz.h>
+
+#include "cdecl.h"
+
+cdecl_type(fz_alloc_context)
+cdecl_type(fz_colorspace)
+cdecl_type(fz_context)
+cdecl_type(fz_store_free_fn)
+
+cdecl_struct(fz_storable_s)
+cdecl_type(fz_storable)
+
+cdecl_struct(fz_pixmap_s)
+cdecl_type(fz_pixmap)
+
+cdecl_func(fz_new_context_imp)
+cdecl_func(fz_new_pixmap)
+cdecl_func(fz_convert_pixmap)
+cdecl_func(fz_drop_pixmap)
+cdecl_func(fz_device_gray)
+cdecl_func(fz_free_context)
+
+cdecl_func(fz_load_png)
