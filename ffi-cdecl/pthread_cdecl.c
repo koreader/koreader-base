a57d36ca636c8f067a17e00289cf3a668373baad
Hans-Werner Hilse <hwhilse@gmail.com>
2014-10-19 00:11:49 +0200

    pthread API declarations
    TODO: check if this applies the same way to Android. Otherwise, split
    this up conditionally on the system it runs on.

---
+++ ffi-cdecl/pthread_cdecl.c
@@ -0,0 +1,18 @@
+#include <pthread.h>
+
+#include "ffi-cdecl.h"
+#include "ffi-cdecl-luajit.h"
+
+cdecl_type(pthread_t)
+
+cdecl_type(pthread_attr_t)
+cdecl_union(pthread_attr_t)
+
+cdecl_func(pthread_attr_init)
+cdecl_func(pthread_attr_setdetachstate)
+cdecl_func(pthread_attr_destroy)
+
+cdecl_const(PTHREAD_CREATE_DETACHED)
+
+cdecl_func(pthread_create)
+
