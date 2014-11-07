6914bbfa46184acbfc37db164ab10897690a90ef
NiLuJe <ninuje@gmail.com>
2014-11-08 00:46:32 +0100

    Update ffi mxcfb stuff...
    Preparation for the next commit :).

--- ffi/mxcfb_kobo_h.lua
+++ ffi/mxcfb_kobo_h.lua
@@ -38,6 +38,16 @@ struct mxcfb_update_data_org {
   unsigned int flags;
   struct mxcfb_alt_buffer_data_org alt_buffer_data;
 };
+static const int UPDATE_MODE_PARTIAL = 0;
+static const int UPDATE_MODE_FULL = 1;
+static const int WAVEFORM_MODE_KOBO_REGAL = 7;
+static const int WAVEFORM_MODE_AUTO = 257;
+static const int TEMP_USE_AMBIENT = 4096;
+static const int EPDC_FLAG_ENABLE_INVERSION = 1;
+static const int EPDC_FLAG_FORCE_MONOCHROME = 2;
+static const int EPDC_FLAG_USE_CMAP = 4;
+static const int EPDC_FLAG_USE_ALT_BUFFER = 256;
+static const int EPDC_FLAG_USE_AAD = 4096;
 static const int MXCFB_SEND_UPDATE = 1078216238;
 static const int MXCFB_WAIT_FOR_UPDATE_COMPLETE = 1074021935;
 static const int MXCFB_SEND_UPDATE_ORG = 1077954094;
