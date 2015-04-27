1239cb00adad68ddda7777389095f3446161e801
NiLuJe <ninuje@gmail.com>
2015-04-27 19:20:46 +0200

    Test run for the new gcc-lua stuff. It works, yay.

--- ffi-cdecl/mxcfb_kindle_decl.c
+++ ffi-cdecl/mxcfb_kindle_decl.c
@@ -7,7 +7,6 @@ typedef unsigned int uint;
 #include "include/mxcfb-kindle.h"
 
 #include "ffi-cdecl.h"
-#include "ffi-cdecl-luajit.h"
 
 cdecl_const(UPDATE_MODE_PARTIAL)
 cdecl_const(UPDATE_MODE_FULL)
