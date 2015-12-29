62dd8c0727caae8197aad6a6125d66daf07d07f5
Qingping Hou <dave2008713@gmail.com>
2015-12-28 22:08:35 -0800

    fix: fix mupdf build & bump to latest release

--- ffi/mupdf.lua
+++ ffi/mupdf.lua
@@ -29,7 +29,7 @@ local mupdf = {
 }
 -- this cannot get adapted by the cdecl file because it is a
 -- string constant. Must match the actual mupdf API:
-local FZ_VERSION = "1.7"
+local FZ_VERSION = "1.8"
 
 local document_mt = { __index = {} }
 local page_mt = { __index = {} }
