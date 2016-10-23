e4f185bee6fc36befeb7a3094e060a1a4b84313e
Qingping Hou <dave2008713@gmail.com>
2016-10-22 21:33:50 -0700

    build: use docker for kobo/kindle/pocketbook build

--- .ci/build_script.sh
+++ .ci/build_script.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
 CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DOCKER_HOME=/home/ko
 source "${CI_DIR}/common.sh"
 
 travis_retry make fetchthirdparty
-make all
+
+if [ "$TARGET" = kobo ]; then
+    chmod -R 777 $HOME/.ccache
+    docker run -t \
+        -v $HOME/.ccache:${DOCKER_HOME}/.ccache \
+        -v `pwd`:${DOCKER_HOME}/base ${DOCKER_IMG} \
+        /bin/bash -c 'source /home/ko/.bashrc && cd /home/ko/base && make TARGET=kobo all'
+elif [ "$TARGET" = kindle ]; then
+    chmod -R 777 $HOME/.ccache
+    docker run -t \
+        -v $HOME/.ccache:${DOCKER_HOME}/.ccache \
+        -v `pwd`:${DOCKER_HOME}/base ${DOCKER_IMG} \
+        /bin/bash -c 'source /home/ko/.bashrc && cd /home/ko/base && make TARGET=kindle all'
+elif [ "$TARGET" = pocketbook ]; then
+    chmod -R 777 $HOME/.ccache
+    docker run -t \
+        -v $HOME/.ccache:${DOCKER_HOME}/.ccache \
+        -v `pwd`:${DOCKER_HOME}/base ${DOCKER_IMG} \
+        /bin/bash -c "source /home/ko/.bashrc && cd /home/ko/base && make pocketbook-toolchain && make VERBOSE=1 TARGET=pocketbook all"
+else
+    make all
+fi
--- .ci/kobo_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install libc6-dev-i386 linux-libc-dev:i386
-
-sudo apt-get install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
