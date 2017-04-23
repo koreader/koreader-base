b0545b56d57602b1fbc341fbf44fba4025f7494b
Frans de Jonge <fransdejonge@gmail.com>
2017-04-23 16:06:09 +0200

    (minor) some shell code quality and formatting fixes
    Build array of files with:
    
    ```
    mapfile -t shellscript_locations < <({ git grep -lE '^#!(/usr)?/bin/(env )?(bash|sh)' && git ls-files ./*.sh; } | sort | uniq)
    ```
    
    Check with:
    
    ```
    for shellscript in "${shellscript_locations[@]}"; do
        echo -e "${ANSI_GREEN}Running shellcheck on ${shellscript}"
        shellcheck "${shellscript}" || SHELLSCRIPT_ERROR=1
        echo -e "${ANSI_GREEN}Running shfmt on ${shellscript}"
        if [ "$(cat "${shellscript}")" != "$(shfmt -i 4 "${shellscript}")" ]; then
            echo -e "${ANSI_RED}Warning: ${shellscript} does not abide by coding style"
            shfmt -i 4 "${shellscript}" | diff "${shellscript}" - || SHELLSCRIPT_ERROR=1
        fi
    done
    ```
    
    See https://github.com/koreader/koreader/blob/6f037cec30673a41492b727648988cc598d40432/.ci/helper_shellchecks.sh

--- .ci/build_script.sh
+++ .ci/build_script.sh
@@ -9,19 +9,19 @@ travis_retry make fetchthirdparty
 if [ "$TARGET" = kobo ]; then
     chmod -R 777 $HOME/.ccache
     docker run -t \
-        -v $HOME/.ccache:${DOCKER_HOME}/.ccache \
+        -v "${HOME}"/.ccache:${DOCKER_HOME}/.ccache \
         -v `pwd`:${DOCKER_HOME}/base ${DOCKER_IMG} \
         /bin/bash -c 'source /home/ko/.bashrc && cd /home/ko/base && make TARGET=kobo all'
 elif [ "$TARGET" = kindle ]; then
     chmod -R 777 $HOME/.ccache
     docker run -t \
-        -v $HOME/.ccache:${DOCKER_HOME}/.ccache \
+        -v "${HOME}"/.ccache:${DOCKER_HOME}/.ccache \
         -v `pwd`:${DOCKER_HOME}/base ${DOCKER_IMG} \
         /bin/bash -c 'source /home/ko/.bashrc && cd /home/ko/base && make TARGET=kindle all'
 elif [ "$TARGET" = pocketbook ]; then
     chmod -R 777 $HOME/.ccache
     docker run -t \
-        -v $HOME/.ccache:${DOCKER_HOME}/.ccache \
+        -v "${HOME}"/.ccache:${DOCKER_HOME}/.ccache \
         -v `pwd`:${DOCKER_HOME}/base ${DOCKER_IMG} \
         /bin/bash -c "source /home/ko/.bashrc && cd /home/ko/base && make pocketbook-toolchain && make VERBOSE=1 TARGET=pocketbook all"
 else
