d4cb2e4c8ec57eb89a95a893d2b4b06b8fb7d069
Qingping Hou <dave2008713@gmail.com>
2017-04-23 18:33:20 -0700

    add documentation for framebuffer_linux.lua

--- ffi/framebuffer_linux.lua
+++ ffi/framebuffer_linux.lua
@@ -2,8 +2,8 @@ local ffi = require("ffi")
 local bit = require("bit")
 local BB = require("ffi/blitbuffer")
 
-local dummy = require("ffi/linux_fb_h")
-local dummy = require("ffi/posix_h")
+require("ffi/linux_fb_h")
+require("ffi/posix_h")
 
 local framebuffer = {
     device_node = "/dev/fb0",
@@ -12,6 +12,58 @@ local framebuffer = {
     data = nil,
 }
 
+--[[
+
+The raw framebuffer memory is managed through Blitbuffer. When creating the
+Blitbuffer, we bind it to a framebuffer memory size of `vinfo.xres *
+finfo.line_length` assuming portrait mode by default.
+
+Here are couple interesting framebuffer attributes to watch out when porting to
+new devices:
+
+* vinfo.bits_per_pixel: Size of each pixel, for example, 16bits, 32bits, etc.
+* finfo.smem_len: Size of the actual framebuffer memory provided by the kernel.
+* finfo.line_length: Size of each row for the framebuffer.
+                     Usually, it would be close to `vinfo.xres_virtual * vinfo.bits_per_pixel / 8`.
+* vinfo.xres: Number of pixels in one row on physical screen, i.e. physical screen width
+* vinfo.yres: Number of pixels in one column on physical screen, i.e. physical screen height
+* vinfo.xres_virtual: Number of pixels in one row on scrollable virtual screen, for fb_pan_display.
+                      Usually `vinfo.xres_virtual` >= `vinfo.xres`.
+* vinfo.yres_virtual: Number of pixels in one column on scrollable virtual screen, for fb_pan_display.
+                      Usually `vinfo.yres_virtual` >= `vinfo.yres`.
+
+NOTE for Kobo:
+
+By definition, `finfo.smem_len` should always be larger than or equal to
+`vinfo.xres_virtual * vinfo.yres_virtual * vinfo.bits_per_pixel / 8`.
+
+However, turns out this is not the case on Kobo when the framebuffer is
+operating at 32bits mode.
+
+On Kobo Aura One, under 16bits mode, we got:
+
+    finfo.line_length: 2816
+    finfo.smem_len: 10813440
+    vinfo.bits_per_pixel: 16
+    vinfo.xres: 1404
+    vinfo.yres: 1872
+    vinfo.xres_virtual: 1408
+    vinfo.yres_virtual: 3840
+
+But under 32bits mode, we got:
+
+    finfo.line_length: 5632
+    finfo.smem_len: 10813440
+    vinfo.bits_per_pixel: 32
+    vinfo.xres: 1404
+    vinfo.yres: 1872
+    vinfo.xres_virtual: 1408
+    vinfo.yres_virtual: 3840
+
+The only settings that got changed are `finfo.line_length` and `vinfo.bits_per_pixel`.
+`finfo.smem_len` still remains at 10813440.
+
+--]]
 function framebuffer:init()
     local finfo = ffi.new("struct fb_fix_screeninfo")
     local vinfo = ffi.new("struct fb_var_screeninfo")
@@ -20,15 +72,11 @@ function framebuffer:init()
     assert(self.fd ~= -1, "cannot open framebuffer")
 
     -- Get fixed screen information
-    assert(ffi.C.ioctl(self.fd, ffi.C.FBIOGET_FSCREENINFO, finfo) == 0,
-        "cannot get screen info")
-
+    assert(ffi.C.ioctl(self.fd, ffi.C.FBIOGET_FSCREENINFO, finfo) == 0, "cannot get screen info")
     assert(ffi.C.ioctl(self.fd, ffi.C.FBIOGET_VSCREENINFO, vinfo) == 0,
-        "cannot get variable screen info")
+           "cannot get variable screen info")
 
-    assert(finfo.type == ffi.C.FB_TYPE_PACKED_PIXELS,
-        "video type not supported")
-
+    assert(finfo.type == ffi.C.FB_TYPE_PACKED_PIXELS, "video type not supported")
     assert(vinfo.xres_virtual > 0 and vinfo.yres_virtual > 0, "invalid framebuffer resolution")
 
     -- Classic eink framebuffer (Kindle 2, 3, DXG, 4)
@@ -52,7 +100,12 @@ function framebuffer:init()
         error("framebuffer model not supported");
     end
 
-    self.data = ffi.C.mmap(nil, self.fb_size, bit.bor(ffi.C.PROT_READ, ffi.C.PROT_WRITE), ffi.C.MAP_SHARED, self.fd, 0)
+    self.data = ffi.C.mmap(nil,
+                           self.fb_size,
+                           bit.bor(ffi.C.PROT_READ, ffi.C.PROT_WRITE),
+                           ffi.C.MAP_SHARED,
+                           self.fd,
+                           0)
     assert(self.data ~= ffi.C.MAP_FAILED, "can not mmap() framebuffer")
     if vinfo.bits_per_pixel == 32 then
         self.bb = BB.new(vinfo.xres, vinfo.yres, BB.TYPE_BBRGB32, self.data, finfo.line_length)
