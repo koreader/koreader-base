172397f6584c13a6fb5cf7fb307c25f5d1cbfbf9
Frans de Jonge <fransdejonge@gmail.com>
2017-09-17 14:50:12 +0200

    Add new Android NDK target

--- .ci/android_install.sh
+++ .ci/android_install.sh
@@ -6,19 +6,24 @@ sudo apt-get update
 sudo apt-get install zlib1g:i386 libc6-dev-i386 linux-libc-dev:i386
 
 if [ "$NDKREV" = "r9c" ]; then
-    curl -L http://dl.google.com/android/ndk/android-ndk-${NDKREV}-linux-x86_64.tar.bz2 -O
+    curl -L "http://dl.google.com/android/ndk/android-ndk-${NDKREV}-linux-x86_64.tar.bz2" -O
     echo "extracting android ndk"
-    bzip2 -dc android-ndk-${NDKREV}-linux-x86_64.tar.bz2 | tar xf -
+    bzip2 -dc "android-ndk-${NDKREV}-linux-x86_64.tar.bz2" | tar xf -
 elif [ "$NDKREV" = "r11c" ]; then
-    curl -L http://dl.google.com/android/repository/android-ndk-${NDKREV}-linux-x86_64.zip -O
+    curl -L "http://dl.google.com/android/repository/android-ndk-${NDKREV}-linux-x86_64.zip" -O
     echo "extracting android ndk"
-    unzip -q android-ndk-${NDKREV}-linux-x86_64.zip
+    unzip -q "android-ndk-${NDKREV}-linux-x86_64.zip"
 elif [ "$NDKREV" = "r12b" ]; then
-    wget https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip
+    wget "https://dl.google.com/android/repository/android-ndk-${NDKREV}-linux-x86_64.zip"
     echo "extracting android ndk"
-    unzip -q android-ndk-${NDKREV}-linux-x86_64.zip
+    unzip -q "android-ndk-${NDKREV}-linux-x86_64.zip"
+elif [ "$NDKREV" = "r15c" ]; then
+    wget "https://dl.google.com/android/repository/android-ndk-${NDKREV}-linux-x86_64.zip"
+    echo "extracting android ndk"
+    unzip -q "android-ndk-${NDKREV}-linux-x86_64.zip"
 fi
 
-export NDK=`pwd`/android-ndk-${NDKREV}
+NDK=$(pwd)/android-ndk-${NDKREV}
+export NDK
 export PATH=$PATH:${NDK}
 make android-toolchain
