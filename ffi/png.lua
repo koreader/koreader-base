650ecce9c96896c04637a4f38047dcbf98419fd3
Frans de Jonge <fransdejonge@gmail.com>
2017-10-06 19:46:38 +0200

    [fix, luacheck] typo: loadpng → lodepng

--- ffi/png.lua
+++ ffi/png.lua
@@ -9,11 +9,11 @@ Currently, this is a LuaJIT FFI wrapper for lodepng lib.
 local ffi = require("ffi")
 local _ = require("ffi/lodepng_h")
 
-local loadpng
+local lodepng
 if ffi.os == "Windows" then
     lodepng = ffi.load("libs/liblodepng.dll")
 elseif ffi.os == "OSX" then
-	lodepng = ffi.load("libs/liblodepng.dylib")
+    lodepng = ffi.load("libs/liblodepng.dylib")
 else
     lodepng = ffi.load("libs/liblodepng.so")
 end
@@ -23,7 +23,7 @@ local Png = {}
 function Png.encodeToFile(filename, mem, w, h)
     local err = lodepng.lodepng_encode32_file(filename, mem, w, h)
     if err ~= 0 then
-        err_msg = lodepng.lodepng_error_text(err)
+        local err_msg = lodepng.lodepng_error_text(err)
         return false, err_msg
     else
         return true
