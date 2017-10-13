f0cad8faa0178f6543484e3477e4ef7e49bd02ea
Frans de Jonge <fransdejonge@gmail.com>
2017-10-13 17:48:06 +0200

    [chore] Add ANDROID_ARCH environment variable (#544)
    * [chore] Add ANDROID_ARCH environment variable
    
    This makes the build less stuck to its current ARM paradigm.
    
    Use `ANDROID_ARCH=x86` to build for x86.
    
    References https://github.com/koreader/koreader/issues/1815
    
    * Deprecate NDK 9 build tests. The android-luajit-launcher says at least 11 is required.
    
    * No x86 Android test on NDK 11 and 12; it's theoretically possible but for the moment it fails.

--- .ci/android_install.sh
+++ .ci/android_install.sh
@@ -5,11 +5,7 @@ sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get install zlib1g:i386 libc6-dev-i386 linux-libc-dev:i386
 
-if [ "$NDKREV" = "r9c" ]; then
-    curl -L "http://dl.google.com/android/ndk/android-ndk-${NDKREV}-linux-x86_64.tar.bz2" -O
-    echo "extracting android ndk"
-    bzip2 -dc "android-ndk-${NDKREV}-linux-x86_64.tar.bz2" | tar xf -
-elif [ "$NDKREV" = "r11c" ]; then
+if [ "$NDKREV" = "r11c" ]; then
     curl -L "http://dl.google.com/android/repository/android-ndk-${NDKREV}-linux-x86_64.zip" -O
     echo "extracting android ndk"
     unzip -q "android-ndk-${NDKREV}-linux-x86_64.zip"
