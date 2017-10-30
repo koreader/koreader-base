844188bc5131a6d8384f25457e9ba69c20fcd28b
Frans de Jonge <fransdejonge@gmail.com>
2017-10-30 12:56:24 +0100

    [CI, fix] KODEBUG tests directory (#570)

--- .ci/test_script.sh
+++ .ci/test_script.sh
@@ -14,7 +14,10 @@ mv ../../tesseract-ocr/tessdata/* data/tessdata/ && cd ../../ || exit
 # fetch font for base test
 travis_retry wget https://github.com/koreader/koreader/raw/master/resources/fonts/droid/DroidSansMono.ttf
 export OUTPUT_DIR
-OUTPUT_DIR=$(ls -d ./build/x86_64-*linux-gnu)
+if [ -n "${KODEBUG+x}" ]; then
+    KODEBUG_SUFFIX=-debug
+fi
+OUTPUT_DIR=$(ls -d ./build/x86_64-*linux-gnu${KODEBUG_SUFFIX})
 mkdir -p "${OUTPUT_DIR}/fonts/droid/"
 cp DroidSansMono.ttf "${OUTPUT_DIR}/fonts/droid/DroidSansMono.ttf"
 # finally make test
