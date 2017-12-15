2b9058a7b78f123dab8a05ebced5c5131626aed8
TnS-hun <TnS-hun@users.noreply.github.com>
2017-12-15 17:50:23 +0100

    Unit test update

--- spec/unit/mupdf_spec.lua
+++ spec/unit/mupdf_spec.lua
@@ -122,10 +122,10 @@ describe("mupdf module", function()
             local page = doc:openPage(1)
             assert.is_not_nil(page)
             page:addMarkupAnnotation(ffi.new("float[8]", {
-                {  70,  930,
-                  510,  930,
-                  510,  970,
-                   70,  970 }),
+                 70,  930,
+                510,  930,
+                510,  970,
+                 70,  970 }),
                 1, ffi.C.PDF_ANNOT_HIGHLIGHT)
             page:close()
             doc:writeDocument(simple_pdf_out)
@@ -154,7 +154,7 @@ describe("mupdf module", function()
                 assert.equals(math.floor(bbox[1]*1000), 56145)
                 assert.equals(math.floor(bbox[2]*1000), 69233)
                 assert.equals(math.floor(bbox[3]*1000), 144790)
-                assert.equals(math.floor(bbox[4]*1000), 103669)
+                assert.equals(math.floor(bbox[4]*1000), 106089)
             end)
             it("should get page text", function()
                 local text = page:getPageText()
