c80ef98a0d1e8f991647f97e4f0d942dee13c608
NiLuJe <ninuje@gmail.com>
2018-06-17 23:34:36 +0200

    Refresh Kobo ffi stuff

--- ffi-cdecl/mxcfb_kobo_decl.c
+++ ffi-cdecl/mxcfb_kobo_decl.c
@@ -3,7 +3,6 @@
 
 #include <linux/ioctl.h>
 // specialized eink framebuffer headers
-typedef unsigned int uint;
 #include "include/mxcfb-kobo.h"
 
 #include "ffi-cdecl.h"
@@ -30,6 +29,10 @@ cdecl_const(WAVEFORM_MODE_REAGL)
 cdecl_const(WAVEFORM_MODE_REAGLD)
 cdecl_const(WAVEFORM_MODE_AUTO)
 
+cdecl_const(WAVEFORM_MODE_GLR32)
+cdecl_const(WAVEFORM_MODE_GLR16)
+cdecl_const(WAVEFORM_MODE_GLD16)
+
 cdecl_const(TEMP_USE_AMBIENT)
 
 cdecl_const(EPDC_FLAG_ENABLE_INVERSION)
@@ -38,16 +41,28 @@ cdecl_const(EPDC_FLAG_USE_CMAP)
 cdecl_const(EPDC_FLAG_USE_ALT_BUFFER)
 cdecl_const(EPDC_FLAG_USE_AAD)
 
+cdecl_const(EPDC_FLAG_TEST_COLLISION)
+cdecl_const(EPDC_FLAG_GROUP_UPDATE)
+cdecl_const(EPDC_FLAG_USE_DITHERING_Y1)
+cdecl_const(EPDC_FLAG_USE_DITHERING_Y4)
+cdecl_const(EPDC_FLAG_USE_REGAL)
+cdecl_const(EPDC_FLAG_USE_DITHERING_NTX_D8)
+
+cdecl_enum(mxcfb_dithering_mode)
+
 cdecl_struct(mxcfb_rect)
+cdecl_struct(mxcfb_alt_buffer_data_ntx)
+cdecl_struct(mxcfb_update_data_v1_ntx)
+
 cdecl_struct(mxcfb_alt_buffer_data)
-cdecl_struct(mxcfb_update_data)
+cdecl_struct(mxcfb_update_data_v1)		// Aura
+cdecl_struct(mxcfb_update_data_v2)		// Mark 7
 
-cdecl_const(MXCFB_SEND_UPDATE)
-cdecl_const(MXCFB_WAIT_FOR_UPDATE_COMPLETE)
+cdecl_struct(mxcfb_update_marker_data)		// Mark 7
 
-/* Aura */
-cdecl_struct(mxcfb_alt_buffer_data_org)
-cdecl_struct(mxcfb_update_data_org)
+cdecl_const(MXCFB_SEND_UPDATE_V1_NTX)
+cdecl_const(MXCFB_WAIT_FOR_UPDATE_COMPLETE_V1)
 
-cdecl_const(MXCFB_SEND_UPDATE_ORG)
-
+cdecl_const(MXCFB_SEND_UPDATE_V1)		// Aura
+cdecl_const(MXCFB_SEND_UPDATE_V2)		// Mark 7
+cdecl_const(MXCFB_WAIT_FOR_UPDATE_COMPLETE_V3)	// Mark 7
