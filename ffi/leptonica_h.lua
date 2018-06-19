f8532d0f59fc58ed86f7e6ab35735a128ba31d19
NiLuJe <ninuje@gmail.com>
2018-06-19 21:45:45 +0200

    Write a leptonica cdecl file and refresh it

--- ffi/leptonica_h.lua
+++ ffi/leptonica_h.lua
@@ -1,108 +1,101 @@
 local ffi = require("ffi")
+
 ffi.cdef[[
-typedef signed char             l_int8;
-typedef unsigned char           l_uint8;
-typedef short                   l_int16;
-typedef unsigned short          l_uint16;
-typedef int                     l_int32;
-typedef unsigned int            l_uint32;
-typedef float                   l_float32;
-typedef double                  l_float64;
-
-struct Box
-{
-    l_int32            x;
-    l_int32            y;
-    l_int32            w;
-    l_int32            h;
-    l_uint32           refcount;      /* reference count (1 if no clones)  */
-
+typedef signed char l_int8;
+typedef unsigned char l_uint8;
+typedef short int l_int16;
+typedef short unsigned int l_uint16;
+typedef int l_int32;
+typedef unsigned int l_uint32;
+typedef float l_float32;
+typedef double l_float64;
+struct Box {
+  l_int32 x;
+  l_int32 y;
+  l_int32 w;
+  l_int32 h;
+  l_uint32 refcount;
 };
-typedef struct Box    BOX;
-
-struct Boxa
-{
-    l_int32            n;             /* number of box in ptr array        */
-    l_int32            nalloc;        /* number of box ptrs allocated      */
-    l_uint32           refcount;      /* reference count (1 if no clones)  */
-    struct Box       **box;           /* box ptr array                     */
+typedef struct Box BOX;
+struct Boxa {
+  l_int32 n;
+  l_int32 nalloc;
+  l_uint32 refcount;
+  struct Box **box;
 };
-typedef struct Boxa  BOXA;
-
-struct Numa
-{
-    l_int32          nalloc;    /* size of allocated number array      */
-    l_int32          n;         /* number of numbers saved             */
-    l_int32          refcount;  /* reference count (1 if no clones)    */
-    l_float32        startx;    /* x value assigned to array[0]        */
-    l_float32        delx;      /* change in x value as i --> i + 1    */
-    l_float32       *array;     /* number array                        */
+typedef struct Boxa BOXA;
+struct Numa {
+  l_int32 nalloc;
+  l_int32 n;
+  l_int32 refcount;
+  l_float32 startx;
+  l_float32 delx;
+  l_float32 *array;
 };
-typedef struct Numa  NUMA;
-
-enum {
-    L_INSERT = 0,     /* stuff it in; no copy, clone or copy-clone    */
-    L_COPY = 1,       /* make/use a copy of the object                */
-    L_CLONE = 2,      /* make/use clone (ref count) of the object     */
-    L_COPY_CLONE = 3  /* make a new object and fill with with clones  */
-                      /* of each object in the array(s)               */
+typedef struct Numa NUMA;
+static const int L_INSERT = 0;
+static const int L_COPY = 1;
+static const int L_CLONE = 2;
+static const int L_COPY_CLONE = 3;
+struct PixColormap {
+  void *array;
+  l_int32 depth;
+  l_int32 nalloc;
+  l_int32 n;
 };
-
 struct Pix {
-    l_uint32             w;           /* width in pixels                   */
-    l_uint32             h;           /* height in pixels                  */
-    l_uint32             d;           /* depth in bits                     */
-    l_uint32             wpl;         /* 32-bit words/line                 */
-    l_uint32             refcount;    /* reference count (1 if no clones)  */
-    l_int32              xres;        /* image res (ppi) in x direction    */
-                                      /* (use 0 if unknown)                */
-    l_int32              yres;        /* image res (ppi) in y direction    */
-                                      /* (use 0 if unknown)                */
-    l_int32              informat;    /* input file format, IFF_*          */
-    char                *text;        /* text string associated with pix   */
-    struct PixColormap  *colormap;    /* colormap (may be null)            */
-    l_uint32            *data;        /* the image data                    */
+  l_uint32 w;
+  l_uint32 h;
+  l_uint32 d;
+  l_uint32 spp;
+  l_uint32 wpl;
+  l_uint32 refcount;
+  l_int32 xres;
+  l_int32 yres;
+  l_int32 informat;
+  l_int32 special;
+  char *text;
+  struct PixColormap *colormap;
+  l_uint32 *data;
 };
 typedef struct Pix PIX;
-
-/* Leptonica */
-BOX * boxCreate(l_int32 x, l_int32 y, l_int32 w, l_int32 h);
-BOXA * boxaCreate(l_int32 n);
-BOX * boxaGetBox(BOXA *boxa, l_int32 index, l_int32 accessflag);
-NUMA * numaCreate(l_int32 n);
-NUMA * numaCreateFromFArray(l_float32 *farray, l_int32 size, l_int32 copyflag);
-BOXA * boxaCombineOverlaps(BOXA *boxas);
-BOXA * boxaClipToBox(BOXA *boxas, BOX *box);
-BOX * boxCopy(BOX *box);
-BOX * boxClone(BOX *box);
-BOX * boxOverlapRegion(BOX *box1, BOX *box2);
-BOX * boxAdjustSides(BOX *boxd, BOX *boxs, l_int32 delleft, l_int32 delright, l_int32 deltop, l_int32 delbot);
-l_int32 boxaAddBox(BOXA *boxa, BOX *box, l_int32 copyflag);
-l_int32 numaGetMax(NUMA *na, l_float32 *pmaxval, l_int32 *pimaxloc);
-l_int32 numaGetIValue(NUMA *na, l_int32 index, l_int32 *pival);
-l_int32 boxaGetCount(BOXA *boxa);
-l_int32 numaGetCount(NUMA *na);
-l_int32 boxaWrite(const char *filename, BOXA *boxa);
-void boxDestroy(BOX **pbox);
-void boxaDestroy(BOXA **pboxa);
-void numaDestroy(NUMA **pna);
-void pixDestroy(PIX **ppix);
-l_int32 pixWritePng(const char *filename, PIX *pix, l_float32 gamma);
-l_int32 pixWriteMemPng(l_uint8 **pdata, size_t *psize, PIX *pix, l_float32 gamma);
-l_int32 pixGetWidth(PIX *pix);
-l_int32 pixGetHeight(PIX *pix);
-l_int32 pixGetDepth(PIX *pix);
-l_int32 pixGetWpl(PIX *pix);
-l_int32 pixSetPixel(PIX *pix, l_int32 x, l_int32 y, l_uint32 val);
-l_uint32 * pixGetData(PIX *pix);
-PIX * pixCreate(l_int32 width, l_int32 height, l_int32 depth);
-PIX * pixConvertTo1(PIX *pixs, l_int32 threshold);
-PIX * pixThresholdToBinary(PIX *pixs, l_int32 thresh);
-PIX * pixConvertTo32(PIX *pixs);
-PIX * pixDrawBoxaRandom(PIX *pixs, BOXA *boxa, l_int32 width);
-PIX * pixMultiplyByColor(PIX *pixd, PIX *pixs, BOX *box, l_uint32 color);
-PIX * pixBlendBackgroundToColor(PIX *pixd, PIX *pixs, BOX *box, l_uint32 color, l_float32 gamma, l_int32 minval, l_int32 maxval);
-l_int32 pixGetRegionsBinary(PIX *pixs, PIX **ppixhm, PIX **ppixtm, PIX **ppixtb, l_int32 debug);
-BOXA * pixSplitIntoBoxa(PIX *pixs, l_int32 minsum, l_int32 skipdist, l_int32 delta, l_int32 maxbg, l_int32 maxcomps, l_int32 remainder);
-PIX * pixReduceRankBinaryCascade(PIX *pixs, l_int32 level1, l_int32 level2, l_int32 level3, l_int32 level4);
+BOX *boxCreate(l_int32, l_int32, l_int32, l_int32);
+BOXA *boxaCreate(l_int32);
+BOX *boxaGetBox(BOXA *, l_int32, l_int32);
+NUMA *numaCreate(l_int32);
+NUMA *numaCreateFromFArray(l_float32 *, l_int32, l_int32);
+BOXA *boxaCombineOverlaps(BOXA *);
+BOXA *boxaClipToBox(BOXA *, BOX *);
+BOX *boxCopy(BOX *);
+BOX *boxClone(BOX *);
+BOX *boxOverlapRegion(BOX *, BOX *);
+BOX *boxAdjustSides(BOX *, BOX *, l_int32, l_int32, l_int32, l_int32);
+l_int32 boxaAddBox(BOXA *, BOX *, l_int32);
+l_int32 numaGetMax(NUMA *, l_float32 *, l_int32 *);
+l_int32 numaGetIValue(NUMA *, l_int32, l_int32 *);
+l_int32 boxaGetCount(BOXA *);
+l_int32 numaGetCount(NUMA *);
+l_int32 boxaWrite(const char *, BOXA *);
+void boxDestroy(BOX **);
+void boxaDestroy(BOXA **);
+void numaDestroy(NUMA **);
+void pixDestroy(PIX **);
+l_int32 pixWritePng(const char *, PIX *, l_float32);
+l_int32 pixWriteMemPng(l_uint8 **, unsigned int *, PIX *, l_float32);
+l_int32 pixGetWidth(PIX *);
+l_int32 pixGetHeight(PIX *);
+l_int32 pixGetDepth(PIX *);
+l_int32 pixGetWpl(PIX *);
+l_int32 pixSetPixel(PIX *, l_int32, l_int32, l_uint32);
+l_uint32 *pixGetData(PIX *);
+PIX *pixCreate(l_int32, l_int32, l_int32);
+PIX *pixConvertTo1(PIX *, l_int32);
+PIX *pixThresholdToBinary(PIX *, l_int32);
+PIX *pixConvertTo32(PIX *);
+PIX *pixDrawBoxaRandom(PIX *, BOXA *, l_int32);
+PIX *pixMultiplyByColor(PIX *, PIX *, BOX *, l_uint32);
+PIX *pixBlendBackgroundToColor(PIX *, PIX *, BOX *, l_uint32, l_float32, l_int32, l_int32);
+l_int32 pixGetRegionsBinary(PIX *, PIX **, PIX **, PIX **, l_int32);
+BOXA *pixSplitIntoBoxa(PIX *, l_int32, l_int32, l_int32, l_int32, l_int32, l_int32);
+PIX *pixReduceRankBinaryCascade(PIX *, l_int32, l_int32, l_int32, l_int32);
 ]]
