ea6eaf71b6a7f7a732df4fc38b188994572b405d
Frans de Jonge <fransdejonge@gmail.com>
2019-03-14 15:01:40 +0100

    [build] Pull in Git submodules with depth 20
    I couldn't include this in #675 due to old Git (mainly in Travis) but it should now be possible.

--- thirdparty/cmake_modules/koreader_thirdparty_git.cmake
+++ thirdparty/cmake_modules/koreader_thirdparty_git.cmake
@@ -8,6 +8,7 @@ endif()
 
 # Default depth
 set(git_clone_depth 50)
+set(git_submodule_depth 20)
 
 set(run 0)
 
@@ -128,12 +129,21 @@ if(error_code)
 endif()
 
 execute_process(
-  COMMAND \"${git_EXECUTABLE}\" submodule update --recursive ${git_submodules}
+  COMMAND \"${git_EXECUTABLE}\" submodule update --depth \${git_submodule_depth} --recursive ${git_submodules}
   WORKING_DIRECTORY \"${work_dir}/${src_name}\"
   RESULT_VARIABLE error_code
 )
 if(error_code)
-  message(FATAL_ERROR \"Failed to update submodules in: '${work_dir}/${src_name}'\")
+  message(STATUS \"Fetching fuller submodule\")
+  execute_process(
+    # some repos are already unshallow at --depth=10, so go deep
+    COMMAND \"${git_EXECUTABLE}\" submodule foreach \"${git_EXECUTABLE} fetch --depth=9999\" --recursive ${git_submodules}
+    WORKING_DIRECTORY \"${work_dir}/${src_name}\"
+    RESULT_VARIABLE error_code
+  )
+  if(error_code)
+    message(FATAL_ERROR \"Failed to update submodules in: '${work_dir}/${src_name}'\")
+  endif()
 endif()
 
 # Complete success, update the script-last-run stamp file:
