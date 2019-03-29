koreader/koreader-base

commit 5f5df5c0091642daed49b09fb1b023c3113b66ed

Faster blitting for BB8 & BBRGB32 when no processing is needed (#878)
* Minor floor/ceil cleanups
* Bump LuaSocket and make sure it doesn't enforce O2 -ggdb3
* Minor BitOpt cleanups (require & cache)
* The eponymous fast blitting for unprocessed same-to-same copy blitting for BB8 & BBRGB32
* fill-based paintRect (and fix its bound checking)
* Unify odd/even checks (MOD -> AND)
* Slightly more elegant fix for #3949
* Fix a stupid c/p mistake from #863 that broke setPixelBlend on >= 16bpp 
* Rejig fb:clear to use bb:fill, as it's already using ffi.fill, but handles xres_virtual properly ;)
* Switch CRe to a 32bpp draw buffer on color screens
* Update SDL2 PixelFormat constants, and use something less braintwisty (no functional changes)
* Update to FBInk v1.12.2 to pickup required fbdepth updates

blitbuffer.c
@@ -19,6 +19,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "blitbuffer.h"
 
 #define MASK_INVERSE 0x02
@@ -143,38 +144,70 @@ static const char*
             break; \
     }
 
-// NOTE: This one needs block scope because it's used inside case labels by BB_fill_rect
-#define FILL_COLOR(bb, COLOR, c, i, j) \
-{ \
-    int rotation = GET_BB_ROTATION(bb); \
-    COLOR *dstptr; \
-    for (j = y; j < y + h; j++) { \
-        for (i = x; i < x + w; i++) { \
-            BB_GET_PIXEL(bb, rotation, COLOR, i, j, &dstptr); \
-            *dstptr = c; \
-        } \
-    } \
-}
-
-void BB_fill_rect(BlitBuffer *bb, int x, int y, int w, int h, Color8A *color) {
+void BB_fill_rect(BlitBuffer *bb, int x, int y, int w, int h, uint8_t v) {
+    int rotation = GET_BB_ROTATION(bb);
+    int rx, ry, rw, rh;
+    // Compute rotated rectangle coordinates & size
+    switch (rotation) {
+        case 0:
+                rx = x;
+                ry = y;
+                rw = w;
+                rh = h;
+                break;
+        case 1:
+                rx = bb->w - (y + h);
+                ry = x;
+                rw = h;
+                rh = w;
+                break;
+        case 2:
+                rx = bb->w - (x + w);
+                ry = bb->h - (y + h);
+                rw = w;
+                rh = h;
+                break;
+        case 3:
+                rx = y;
+                ry = bb->h - (x + w);
+                rw = h;
+                rh = w;
+                break;
+    }
+    // Handle any target pitch properly (i.e., fetch the amount of bytes taken per pixel)...
     int bb_type = GET_BB_TYPE(bb);
-    int i, j;
+    uint8_t bpp = 1;
     switch (bb_type) {
         case TYPE_BB8:
-            FILL_COLOR(bb, Color8, Color8A_To_Color8(color), i, j);
+            bpp = 1;
             break;
         case TYPE_BB8A:
-            FILL_COLOR(bb, Color8A, *color, i, j);
+            bpp = 2;
             break;
         case TYPE_BBRGB16:
-            FILL_COLOR(bb, ColorRGB16, Color8A_To_Color16(color), i, j);
+            bpp = 2;
             break;
         case TYPE_BBRGB24:
-            FILL_COLOR(bb, ColorRGB24, Color8A_To_Color24(color), i, j);
+            bpp = 3;
             break;
         case TYPE_BBRGB32:
-            FILL_COLOR(bb, ColorRGB32, Color8A_To_Color32(color), i, j);
+            bpp = 4;
             break;
+    }
+    if (rx == 0 && rw == bb->w) {
+        // Single step for contiguous scanlines
+        //fprintf(stdout, "%s: Single fill paintRect\n", __FUNCTION__);
+        uint8_t *p = bb->data + bb->pitch*ry;
+        memset(p, v, bpp*bb->phys_w*rh);
+    } else {
+        // Scanline per scanline fill
+        //fprintf(stdout, "%s: Scanline fill paintRect\n", __FUNCTION__);
+        uint8_t *p = bb->data;
+        int j;
+        for (j = ry; j < ry+rh; j++) {
+            p = bb->data + bb->pitch*j + bpp*rx;
+            memset(p, v, bpp*rw);
+        }
     }
 }
 
@@ -261,17 +294,41 @@ void BB_blit_to_BB8(BlitBuffer *src, BlitBuffer *dst,
     switch (sbb_type) {
         case TYPE_BB8:
             {
-                Color8 *srcptr;
-                o_y = offs_y;
-                for (d_y = dest_y; d_y < dest_y + h; d_y++) {
-                    o_x = offs_x;
-                    for (d_x = dest_x; d_x < dest_x + w; d_x++) {
-                        BB_GET_PIXEL(dst, dbb_rotation, Color8, d_x, d_y, &dstptr);
-                        BB_GET_PIXEL(src, sbb_rotation, Color8, o_x, o_y, &srcptr);
-                        *dstptr = *srcptr;
-                        o_x += 1;
+                // We can only do a fast copy for simple same-to-same blitting without any extra processing.
+                // (i.e., setPixel, no rota, no invert).
+                // The cbb codepath ensures setPixel & no invert, so we only check for rotation.
+                if (sbb_rotation == 0 && dbb_rotation == 0) {
+                    if (offs_x == 0 && dest_x == 0 && w == src->phys_w && w == dst->phys_w) {
+                        // Single step for contiguous scanlines (on both sides)
+                        //fprintf(stdout, "%s: full copy blit from BB8 to BB8\n", __FUNCTION__);
+                        // BB8 is 1 byte per pixel
+                        const uint8_t *srcp = src->data + src->pitch*offs_y;
+                        uint8_t *dstp = dst->data + dst->pitch*dest_y;
+                        memcpy(dstp, srcp, w*h);
+                    } else {
+                        // Scanline per scanline copy
+                        //fprintf(stdout, "%s: scanline copy blit from BB8 to BB8\n", __FUNCTION__);
+                        o_y = offs_y;
+                        for (d_y = dest_y; d_y < dest_y+h; d_y++, o_y++) {
+                            // BB8 is 1 byte per pixel
+                            const uint8_t *srcp = src->data + src->pitch*o_y + offs_x;
+                            uint8_t *dstp = dst->data + dst->pitch*d_y + dest_x;
+                            memcpy(dstp, srcp, w);
+                        }
                     }
-                    o_y += 1;
+                } else {
+                    Color8 *srcptr;
+                    o_y = offs_y;
+                    for (d_y = dest_y; d_y < dest_y + h; d_y++) {
+                        o_x = offs_x;
+                        for (d_x = dest_x; d_x < dest_x + w; d_x++) {
+                            BB_GET_PIXEL(dst, dbb_rotation, Color8, d_x, d_y, &dstptr);
+                            BB_GET_PIXEL(src, sbb_rotation, Color8, o_x, o_y, &srcptr);
+                            *dstptr = *srcptr;
+                            o_x += 1;
+                        }
+                        o_y += 1;
+                    }
                 }
             }
             break;
@@ -714,17 +771,41 @@ void BB_blit_to_BB32(BlitBuffer *src, BlitBuffer *dst,
             break;
         case TYPE_BBRGB32:
             {
-                ColorRGB32 *srcptr;
-                o_y = offs_y;
-                for (d_y = dest_y; d_y < dest_y + h; d_y++) {
-                    o_x = offs_x;
-                    for (d_x = dest_x; d_x < dest_x + w; d_x++) {
-                        BB_GET_PIXEL(dst, dbb_rotation, ColorRGB32, d_x, d_y, &dstptr);
-                        BB_GET_PIXEL(src, sbb_rotation, ColorRGB32, o_x, o_y, &srcptr);
-                        *dstptr = *srcptr;
-                        o_x += 1;
+                // We can only do a fast copy for simple same-to-same blitting without any extra processing.
+                // (i.e., setPixel, no rota, no invert).
+                // The cbb codepath ensures setPixel & no invert, so we only check for rotation.
+                if (sbb_rotation == 0 && dbb_rotation == 0) {
+                    if (offs_x == 0 && dest_x == 0 && w == src->phys_w && w == dst->phys_w) {
+                        // Single step for contiguous scanlines (on both sides)
+                        //fprintf(stdout, "%s: full copy blit from BBRGB32 to BBRGB32\n", __FUNCTION__);
+                        // BBRGB32 is 4 bytes per pixel
+                        const uint8_t *srcp = src->data + src->pitch*offs_y;
+                        uint8_t *dstp = dst->data + dst->pitch*dest_y;
+                        memcpy(dstp, srcp, (w << 2)*h);
+                    } else {
+                        // Scanline per scanline copy
+                        //fprintf(stdout, "%s: scanline copy blit from BBRGB32 to BBRGB32\n", __FUNCTION__);
+                        o_y = offs_y;
+                        for (d_y = dest_y; d_y < dest_y+h; d_y++, o_y++) {
+                            // BBRGB32 is 4 bytes per pixel
+                            const uint8_t *srcp = src->data + src->pitch*o_y + (offs_x << 2);
+                            uint8_t *dstp = dst->data + dst->pitch*d_y + (dest_x << 2);
+                            memcpy(dstp, srcp, w << 2);
+                        }
                     }
-                    o_y += 1;
+                } else {
+                    ColorRGB32 *srcptr;
+                    o_y = offs_y;
+                    for (d_y = dest_y; d_y < dest_y + h; d_y++) {
+                        o_x = offs_x;
+                        for (d_x = dest_x; d_x < dest_x + w; d_x++) {
+                            BB_GET_PIXEL(dst, dbb_rotation, ColorRGB32, d_x, d_y, &dstptr);
+                            BB_GET_PIXEL(src, sbb_rotation, ColorRGB32, o_x, o_y, &srcptr);
+                            *dstptr = *srcptr;
+                            o_x += 1;
+                        }
+                        o_y += 1;
+                    }
                 }
             }
             break;
@@ -734,7 +815,7 @@ void BB_blit_to_BB32(BlitBuffer *src, BlitBuffer *dst,
 void BB_blit_to(BlitBuffer *src, BlitBuffer *dst,
         int dest_x, int dest_y, int offs_x, int offs_y, int w, int h) {
     int dbb_type = GET_BB_TYPE(dst);
-    // fprintf(stdout, "%s: blit from type: %s to: %s\n", __FUNCTION__, get_bbtype_name(GET_BB_TYPE(src)), get_bbtype_name(GET_BB_TYPE(dst)));
+    //fprintf(stdout, "%s: blit from type: %s to: %s\n", __FUNCTION__, get_bbtype_name(GET_BB_TYPE(src)), get_bbtype_name(GET_BB_TYPE(dst)));
     switch (dbb_type) {
         case TYPE_BB8:
             BB_blit_to_BB8(src, dst, dest_x, dest_y, offs_x, offs_y, w, h);

ffi/framebuffer_linux.lua
@@ -213,15 +213,15 @@ function framebuffer:init()
     assert(tonumber(ffi.cast("intptr_t", self.data)) ~= C.MAP_FAILED,
            "can not mmap() framebuffer")
     if vinfo.bits_per_pixel == 32 then
-        self.bb = BB.new(vinfo.xres, vinfo.yres, BB.TYPE_BBRGB32, self.data, finfo.line_length)
+        self.bb = BB.new(vinfo.xres, vinfo.yres, BB.TYPE_BBRGB32, self.data, finfo.line_length, vinfo.xres_virtual, vinfo.yres_virtual)
     elseif vinfo.bits_per_pixel == 24 then
-        self.bb = BB.new(vinfo.xres, vinfo.yres, BB.TYPE_BBRGB24, self.data, finfo.line_length)
+        self.bb = BB.new(vinfo.xres, vinfo.yres, BB.TYPE_BBRGB24, self.data, finfo.line_length, vinfo.xres_virtual, vinfo.yres_virtual)
     elseif vinfo.bits_per_pixel == 16 then
-        self.bb = BB.new(vinfo.xres, vinfo.yres, BB.TYPE_BBRGB16, self.data, finfo.line_length)
+        self.bb = BB.new(vinfo.xres, vinfo.yres, BB.TYPE_BBRGB16, self.data, finfo.line_length, vinfo.xres_virtual, vinfo.yres_virtual)
     elseif vinfo.bits_per_pixel == 8 then
-        self.bb = BB.new(vinfo.xres, vinfo.yres, BB.TYPE_BB8, self.data, finfo.line_length)
+        self.bb = BB.new(vinfo.xres, vinfo.yres, BB.TYPE_BB8, self.data, finfo.line_length, vinfo.xres_virtual, vinfo.yres_virtual)
     elseif vinfo.bits_per_pixel == 4 then
-        self.bb = BB.new(vinfo.xres, vinfo.yres, BB.TYPE_BB4, self.data, finfo.line_length)
+        self.bb = BB.new(vinfo.xres, vinfo.yres, BB.TYPE_BB4, self.data, finfo.line_length, vinfo.xres_virtual, vinfo.yres_virtual)
     else
         error("unknown bpp value for the eink driver")
     end
@@ -240,17 +240,6 @@ function framebuffer:init()
     self.bb:fill(BB.COLOR_WHITE)
 
     framebuffer.parent.init(self)
-end
-
--- Used to bypass blitting when we just want a white screen
-function framebuffer:clear(w, h)
-    if self.data and w and h then
-        -- NOTE: We do not rely on self.fb_size, because it's larger than line_length * yres,
-        --       and regions of memory outside of the visible screen *may* be used by other things in the system,
-        --       for performance reasons, and we do not want to screw them over ;).
-        -- NOTE: This should be equivalent to line_length * yres
-        ffi.fill(self.data, w * (self.fb_bpp / 8) * h, 0xFF)
-    end
 end
 
 function framebuffer:close()

thirdparty/fbink/CMakeLists.txt
@@ -53,7 +53,7 @@ endif()
 ko_write_gitclone_script(
     GIT_CLONE_SCRIPT_FILENAME
     https://github.com/NiLuJe/FBInk.git
-    v1.12.1
+    v1.12.2
     ${SOURCE_DIR}
 )
 

thirdparty/freetype2/CMakeLists.txt
@@ -14,7 +14,7 @@ assert_var_defined(LDFLAGS)
 ep_get_source_dir(SOURCE_DIR)
 ep_get_binary_dir(BINARY_DIR)
 
-set(CFG_ENV_VAR "CC=\"${CC}\" CXX=\"${CXX}\" CFLAGS=\"${CFLAGS}\" CXXFLAGS=\"${CXXFLAGS}\" LDFLAGS=\"${LDFLAGS}\" ac_cv_func_mmap_fixed_mapped=\"yes\"")
+set(CFG_ENV_VAR "CC=\"${CC}\" CXX=\"${CXX}\" CFLAGS=\"${CFLAGS}\" CXXFLAGS=\"${CXXFLAGS}\" LDFLAGS=\"${LDFLAGS}\"")
 set(CFG_OPTS "-q --prefix=${BINARY_DIR} --disable-static --enable-shared --with-zlib=no --with-bzip2=no --with-png=no --with-harfbuzz=no --host=\"${CHOST}\"")
 set(CFG_CMD sh -c "${CFG_ENV_VAR} ${SOURCE_DIR}/configure ${CFG_OPTS}")
 

thirdparty/kpvcrlib/CMakeLists.txt
@@ -99,6 +99,8 @@ message("Will build crengine library")
 add_definitions(-DHAVE_PROTOTYPES)
 add_definitions(-DHAVE_UNSIGNED_CHAR)
 add_definitions(-DCR_EMULATE_GETTEXT)
+# c.f., https://github.com/koreader/crengine/pull/275
+add_definitions(-DCR_RENDER_32BPP_RGB_PXFMT)
 add_definitions(-Dmain=xxxmain)
 set (CRENGINE_SOURCES
     ${CRE_DIR}/src/cp_stats.cpp

thirdparty/luasocket/CMakeLists.txt
@@ -20,6 +20,8 @@ set(PATCH_CMD "${PATCH_CMD} && ${ISED} 's|mime\\.core|mime\\.mcore|' src/*")
 set(PATCH_CMD "${PATCH_CMD} && ${ISED} 's|mime_core|mime_mcore|' src/*")
 set(PATCH_CMD "${PATCH_CMD} && ${ISED} 's|SOCKET_CDIR)/core|SOCKET_CDIR)/score|' src/*")
 set(PATCH_CMD "${PATCH_CMD} && ${ISED} 's|MIME_CDIR)/core|MIME_CDIR)/mcore|' src/*")
+# Don't enforce CFLAGS
+set(PATCH_CMD "${PATCH_CMD} && ${ISED} 's|-O2 -ggdb3 ||g' -i src/makefile")
 
 set(BUILD_CMD_STR "${KO_MAKE_RECURSIVE} PLAT=${PLAT} CC=\"${CC}\" LD=\"${CC}\"")
 if(DEFINED MYLDFLAGS)
@@ -33,7 +35,7 @@ set(BUILD_CMD sh -c "${BUILD_CMD_STR} LUAINC=\"${LUAINC}\" -j${PARALLEL_JOBS} al
 ko_write_gitclone_script(
     GIT_CLONE_SCRIPT_FILENAME
     https://github.com/diegonehab/luasocket
-    288219fd6b53ce2e709745c9918aa4c4b7f715c9
+    c89a931cc31f219d51dd32a8a253d5ee3fbd31be
     ${SOURCE_DIR}
 )
 

blitbuffer.h
@@ -49,7 +49,9 @@ typedef struct ColorRGB32 {
 
 typedef struct BlitBuffer {
     int w;
+    int phys_w;
     int h;
+    int phys_h;
     int pitch;
     uint8_t *data;
     uint8_t config;
@@ -57,7 +59,9 @@ typedef struct BlitBuffer {
 
 typedef struct BlitBuffer8 {
     int w;
+    int phys_w;
     int h;
+    int phys_h;
     int pitch;
     Color8 *data;
     uint8_t config;
@@ -65,7 +69,9 @@ typedef struct BlitBuffer8 {
 
 typedef struct BlitBuffer8A {
     int w;
+    int phys_w;
     int h;
+    int phys_h;
     int pitch;
     Color8A *data;
     uint8_t config;
@@ -73,7 +79,9 @@ typedef struct BlitBuffer8A {
 
 typedef struct BlitBufferRGB16 {
     int w;
+    int phys_w;
     int h;
+    int phys_h;
     int pitch;
     ColorRGB16 *data;
     uint8_t config;
@@ -81,7 +89,9 @@ typedef struct BlitBufferRGB16 {
 
 typedef struct BlitBufferRGB24 {
     int w;
+    int phys_w;
     int h;
+    int phys_h;
     int pitch;
     ColorRGB24 *data;
     uint8_t config;
@@ -89,13 +99,15 @@ typedef struct BlitBufferRGB24 {
 
 typedef struct BlitBufferRGB32 {
     int w;
+    int phys_w;
     int h;
+    int phys_h;
     int pitch;
     ColorRGB32 *data;
     uint8_t config;
 } BlitBufferRGB32;
 
-void BB_fill_rect(BlitBuffer *bb, int x, int y, int w, int h, Color8A *color);
+void BB_fill_rect(BlitBuffer *bb, int x, int y, int w, int h, uint8_t v);
 void BB_blend_rect(BlitBuffer *bb, int x, int y, int w, int h, Color8A *color);
 void BB_blit_to_BB8(BlitBuffer *src, BlitBuffer *dst,
                     int dest_x, int dest_y, int offs_x, int offs_y, int w, int h);

commit 714ceb770eb621db758e60a46adc07445629c552

Fix a couple bugs from #878 (#882)
* Fix Android, because the framebuffer can also have a physical stride larger than the pixel width ;).
* Fix RGB32-to-RGB32 fast blitting w/ the Lua blitter, which I broke late in #878 because of a stupid typo.
* Allow disabling the C blitter, to make testing easier.
* Bump FBInk to v1.13.0 (unbreak legacy Kindle handling)
* Possibly unbreak some PB corner-cases (koreader/koreader#4851)

blitbuffer.c
@@ -2069,6 +2069,9 @@ void BB_color_blit_from(BlitBuffer *dst, BlitBuffer *src,
                 for (d_y = dest_y; d_y < dest_y + h; d_y++) {
                     o_x = offs_x;
                     for (d_x = dest_x; d_x < dest_x + w; d_x++) {
+                        // NOTE: GCC *may* throw a -Wmaybe-uninitialized about alpha here,
+                        //       because of the lack of default case  in the SET_ALPHA_FROM_A switch.
+                        //       Not a cause for alarm here :).
                         SET_ALPHA_FROM_A(src, sbb_type, sbb_rotation, o_x, o_y, &alpha);
                         if (alpha == 0) {
                             // NOP

ffi-cdecl/posix_decl.c
@@ -88,3 +88,7 @@ cdecl_func(fputc)
 cdecl_const(FIONREAD)
 cdecl_func(fileno)
 cdecl_func(strerror)
+
+cdecl_func(setenv)
+cdecl_func(unsetenv)
+//cdecl_func(_putenv) // Win32

ffi/blitbuffer.lua
@@ -139,7 +139,12 @@ void *malloc(int size);
 void free(void *ptr);
 ]]
 
-local use_cblitbuffer, cblitbuffer = pcall(ffi.load, 'blitbuffer')
+-- NOTE: Try the C blitter, unless it was disabled by the user
+local no_cbb_flag = os.getenv("KO_NO_CBB")
+local use_cblitbuffer, cblitbuffer
+if not no_cbb_flag or no_cbb_flag == "false" then
+    use_cblitbuffer, cblitbuffer = pcall(ffi.load, 'blitbuffer')
+end
 -- NOTE: This works-around a number of corner-cases which may end up with LuaJIT's optimizer blacklisting this very codepath,
 --       which'd obviously *murder* performance (to the effect of a soft-lock, essentially).
 --       c.f., #4137, #4752, #4782
@@ -684,7 +689,6 @@ function BB_mt.__index:setPixel(x, y, color)
     local px, py = self:getPhysicalCoordinates(x, y)
     -- NOTE: The cbb exemption is because it's assuming you're using an inverted bb copy to handle nightmode (c.f., Android & SDL2),
     --       and setPixel can be used from outside blitFrom, unlike the other setters.
-    -- NOTE: Speaking of the other setters, some of the alpha = 0xFF fastpaths *might* be skipping the invert?
     if not use_cblitbuffer and self:getInverse() == 1 then color = color:invert() end
     self:getPixelP(px, py)[0]:set(color)
 end
@@ -956,7 +960,7 @@ function BBRGB32_mt.__index:blitToRGB32(dest, dest_x, dest_y, offs_x, offs_y, wi
         --print("BBRGB32 to BBRGB32 full copy")
         -- BBRGB32 is 4 bytes per pixel
         local srcp = ffi.cast(uint8pt, self.data) + self.pitch*offs_y
-        local dstp = ffi.cast(uint8pt, self.data) + dest.pitch*dest_y
+        local dstp = ffi.cast(uint8pt, dest.data) + dest.pitch*dest_y
         ffi.copy(dstp, srcp, lshift(width, 2)*height)
     else
         -- Scanline per scanline copy
@@ -965,7 +969,7 @@ function BBRGB32_mt.__index:blitToRGB32(dest, dest_x, dest_y, offs_x, offs_y, wi
         for y = dest_y, dest_y+height-1 do
             -- BBRGB32 is 4 bytes per pixel
             local srcp = ffi.cast(uint8pt, self.data) + self.pitch*o_y + lshift(offs_x, 2)
-            local dstp = ffi.cast(uint8pt, self.data) + dest.pitch*y + lshift(dest_x, 2)
+            local dstp = ffi.cast(uint8pt, dest.data) + dest.pitch*y + lshift(dest_x, 2)
             ffi.copy(dstp, srcp, lshift(width, 2))
             o_y = o_y + 1
         end

ffi/framebuffer_android.lua
@@ -44,9 +44,9 @@ function framebuffer:_updateWindow()
     if buffer[0].format == C.WINDOW_FORMAT_RGBA_8888
     or buffer[0].format == C.WINDOW_FORMAT_RGBX_8888
     then
-        bb = BB.new(buffer[0].width, buffer[0].height, BB.TYPE_BBRGB32, buffer[0].bits, buffer[0].stride*4)
+        bb = BB.new(buffer[0].width, buffer[0].height, BB.TYPE_BBRGB32, buffer[0].bits, buffer[0].stride*4, buffer[0].stride)
     elseif buffer[0].format == C.WINDOW_FORMAT_RGB_565 then
-        bb = BB.new(buffer[0].width, buffer[0].height, BB.TYPE_BBRGB16, buffer[0].bits, buffer[0].stride*2)
+        bb = BB.new(buffer[0].width, buffer[0].height, BB.TYPE_BBRGB16, buffer[0].bits, buffer[0].stride*2, buffer[0].stride)
     else
         android.LOGE("unsupported window format!")
     end

ffi/framebuffer_linux.lua
@@ -133,6 +133,7 @@ function framebuffer:init()
         --       (c.f., mxc_epdc_fb_check_var @ drivers/video/mxc/mxc_epdc_fb.c OR drivers/video/fbdev/mxc/mxc_epdc_v2_fb.c).
         --       On PB, not so much (possibly because they expect you to use InkView).
         --       So, do it ourselves, if need be...
+        local xres_virtual = vinfo.xres_virtual
         if not IS_ALIGNED(vinfo.xres_virtual, 32) then
             -- NOTE: As per Kindle/Kobo kernels, xres_virtual = ALIGN(xres, 32);
             vinfo.xres_virtual = ALIGN(vinfo.xres, 32)
@@ -177,6 +178,12 @@ function framebuffer:init()
                 -- And that means the original line_length should *probably* be honored, too...
                 finfo.line_length = line_length
                 io.write("PB FB: line_length <- ", finfo.line_length, "\n")
+                -- As well as both _virtual dimensions (c.f., #4851)...
+                vinfo.xres_virtual = xres_virtual
+                io.write("PB FB: xres_virtual <- ", vinfo.xres_virtual, "\n")
+                -- We probably don't care about yres as much ax xres, but, eh.
+                vinfo.yres_virtual = yres_virtual
+                io.write("PB FB: yres_virtual <- ", vinfo.yres_virtual, "\n")
             end
         end
     else

ffi/posix_h.lua
@@ -71,4 +71,7 @@ int fputc(int, struct _IO_FILE *);
 static const int FIONREAD = 21531;
 int fileno(struct _IO_FILE *) __attribute__((__nothrow__, __leaf__));
 char *strerror(int) __attribute__((__nothrow__, __leaf__));
+int setenv(const char *, const char *, int) __attribute__((__nothrow__, __leaf__));
+int unsetenv(const char *) __attribute__((__nothrow__, __leaf__));
+int _putenv(const char *);
 ]]

thirdparty/fbink/CMakeLists.txt
@@ -53,7 +53,7 @@ endif()
 ko_write_gitclone_script(
     GIT_CLONE_SCRIPT_FILENAME
     https://github.com/NiLuJe/FBInk.git
-    v1.12.2
+    v1.13.0
     ${SOURCE_DIR}
 )
 