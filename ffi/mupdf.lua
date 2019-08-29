4b9c61d12a426cee012413d516753fb8ac7a3879
Frans de Jonge <fransdejonge@gmail.com>
2019-08-29 21:46:36 +0200

    I'm a bool guy, duh.

--- ffi/mupdf.lua
+++ ffi/mupdf.lua
@@ -173,7 +173,7 @@ end
 
 function document_mt.__index:isDocumentReflowable(width, height, em)
     if self.is_reflowable then return self.is_reflowable end
-    self.is_reflowable = M.fz_is_document_reflowable(context(), self.doc)
+    self.is_reflowable = M.fz_is_document_reflowable(context(), self.doc) == 1
     return self.is_reflowable
 end
 
