0a2bd6c2502dad4df7be9c25fecfe6e8aada90d9
NiLuJe <ninuje@gmail.com>
2020-02-29 18:09:07 +0100

    Use a local variable

--- ffi/SDL2_0.lua
+++ ffi/SDL2_0.lua
@@ -105,13 +105,9 @@ function S.open()
     end
 
     -- set up screen (window)
+    local screen_flags = bit.bor(full_screen and 1 or 0, SDL.SDL_WINDOW_RESIZABLE)
     if SDL_Linked_Version_AtLeast(2, 0, 1) then
-        screen_flags = bit.bor(full_screen and 1 or 0,
-                               SDL.SDL_WINDOW_RESIZABLE,
-                               SDL.SDL_WINDOW_ALLOW_HIGHDPI)
-    else
-        screen_flags = bit.bor(full_screen and 1 or 0,
-                               SDL.SDL_WINDOW_RESIZABLE)
+        screen_flags = bit.bor(screen_flags, SDL.SDL_WINDOW_ALLOW_HIGHDPI)
     end
     S.screen = SDL.SDL_CreateWindow("KOReader",
         tonumber(os.getenv("KOREADER_WINDOW_POS_X")) or SDL.SDL_WINDOWPOS_UNDEFINED,
