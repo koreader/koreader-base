e7cf8c11e11a1aaf9f5fce23c0721992bcc13e22
Jason Benwell <jbenwell@hotmail.com>
2020-04-28 11:24:31 -0400

    Update SDL2_0_h.lua

--- ffi/SDL2_0_h.lua
+++ ffi/SDL2_0_h.lua
@@ -705,6 +705,7 @@ int SDL_PollEvent(union SDL_Event *) __attribute__((visibility("default")));
 Uint32 SDL_GetTicks(void) __attribute__((visibility("default")));
 void SDL_Delay(Uint32) __attribute__((visibility("default")));
 int SDL_GetCurrentDisplayMode(int, SDL_DisplayMode *) __attribute__((visibility("default")));
+void SDL_EnableScreenSaver(void);
 SDL_Window *SDL_CreateWindow(const char *, int, int, int, int, Uint32) __attribute__((visibility("default")));
 SDL_Renderer *SDL_CreateRenderer(SDL_Window *, int, Uint32) __attribute__((visibility("default")));
 int SDL_CreateWindowAndRenderer(int, int, Uint32, SDL_Window **, SDL_Renderer **) __attribute__((visibility("default")));
