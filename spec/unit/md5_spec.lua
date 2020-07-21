f266569916787d7d95fd885a8de41adae399726e
Frans de Jonge <fransdejonge@gmail.com>
2020-07-21 16:50:53 +0200

    [fix, CI] Deprecate MD5 module in favor of sha2
    This will require a few minor changes in front. Fixes the tests in the CircleCI clang build.

--- spec/unit/md5_spec.lua
+++ spec/unit/md5_spec.lua
@@ -1,23 +1,24 @@
 require("ffi_wrapper")
 
 describe("MD5 module", function()
-    local md5
+    local md5, sha2
 
     setup(function()
         md5 = require("ffi/MD5")
+        sha2 = require("ffi/sha2")
     end)
 
     it("should calculate correct MD5 hashes", function()
-        assert.is_equal("d41d8cd98f00b204e9800998ecf8427e", md5.sum(""))
-        assert.is_equal("93b885adfe0da089cdf634904fd59f71", md5.new():sum("\0"))
-        assert.is_equal("1b05aba914a8b12315c7ee52b42f3d35", md5.new():sum("0123456789abcdefX"))
+        assert.is_equal("d41d8cd98f00b204e9800998ecf8427e", sha2.md5(""))
+        assert.is_equal("93b885adfe0da089cdf634904fd59f71", sha2.md5("\0"))
+        assert.is_equal("1b05aba914a8b12315c7ee52b42f3d35", sha2.md5("0123456789abcdefX"))
     end)
 
     it("should calculate MD5 sum by updating", function()
-        local m = md5.new()
-        m:update("0123456789")
-        m:update("abcdefghij")
-        assert.is_equal(md5.sum("0123456789abcdefghij"), m:sum())
+        local update = sha2.md5()
+        update("0123456789")
+        update("abcdefghij")
+        assert.is_equal(sha2.md5("0123456789abcdefghij"), update())
     end)
 
     it("should calculate MD5 sum of a file", function()
