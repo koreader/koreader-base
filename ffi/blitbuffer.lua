71c345f14245b96e4e8da420853ff4a13c1565c3
Ez Diy <ezdiy@outlook.com>
2020-09-30 03:36:20 +0200

    Academic code

--- ffi/blitbuffer.lua
+++ ffi/blitbuffer.lua
@@ -581,7 +581,6 @@ local SHIFT_ROTATED = 2
 local MASK_TYPE = 0x70
 local SHIFT_TYPE = 4
 local MASK_CFA = 0x80
-local SHIFT_CFA = 7
 
 local TYPE_BB4 = 0
 local TYPE_BB8 = 1
@@ -980,54 +979,118 @@ end
 
 function BB_mt.__index:blitDefault(dest, dest_x, dest_y, offs_x, offs_y, width, height, setter, set_param)
     -- slow default variant:
-    local hook, mask, count = debug.gethook()
+    local hook, mask = debug.gethook()
     debug.sethook()
 
     -- RGB(A) -> BB8 CFA
     -- TODO: maybe a real mt method picked up somehow?
     if self:getBpp() >= 24 and dest:isCFA() then
         assert(dest:getType() == TYPE_BB8, "CFA only with BB8 for the time being")
-        local o_y = offs_y
-        for y = dest_y, dest_y+height-1 do
-            -- Skew is the "initial" component drawn for each scanline at y, starting from dest_x. From then on we continue
-            -- painting RGBRGB. Ie for resulting skew 1, we'd paint 8 pixels as: GBRGBRGB
-            -- If the physical Y axis reversed, the order turn into BGRBGR along X, so you need to flip the forward bool.
-            -- If the physical X is inverted, then only flipping the sign should suffice.
-            -- This may need adjusting if display CFA is not rooted as R->G->B order going from (0,0) in both X and Y directions.
-            -- That means flipping +- for either x or y below, and/or add/sub small 1..2 constant if we're going on inverted axis so it doesn't start at R
-            local skew = ((4096 * 3) + dest_x - y) % 3
-            --local skew = ((4096 * 3) - dest_x - y) % 3
-            --local skew = ((4096 * 3) - dest_x + y + 2) % 3
-            --local skew = ((4096 * 3) - dest_x + y - 1) % 3
-            --local skew = ((4096 * 3) + dest_x - y + 1) % 3
-            --local skew = ... something along those lines
-            local forward = true -- skew same component "rightwards" with increasing scanlines (y) if true, otherwise "leftwards" if false
-            --local forward = false
 
-            local mask = lshift(0xff, skew * 8)
-
-            local o_x = offs_x
-            for x = dest_x, dest_x+width-1 do
-
-                -- pick one component off rgb[x%3]
-                local px, py = self:getPhysicalCoordinates(o_x, o_y)
-                local v = band(ffi.cast(uint32pt, self:getPixelP(px, py))[0], mask)
-                -- collapse it to 8 bit value
-                local pixel = bor(v, bor(rshift(v,16),rshift(v,8)))
-                setter(dest, x, y, Color8(pixel), set_param)
-                o_x = o_x + 1
-                -- note that doing mod 3 like this is far cheaper as we avoid exploding the trace into an unstable branch
-                if forward then
-                    -- skew forward, ie on x axis first we show r, then we show g, then b ...
-                    mask = lshift(mask, 8)
-                    mask = band(bor(mask, rshift(mask, 24)), 0xffffff)
-                else
-                    -- skew backward, ie on x axis first we show g, then we show g, then r ...
-                    mask = bor(lshift(band(mask, 0xff), 16), rshift(mask, 8))
-                end
-            end
-            o_y = o_y + 1
+        -- Unimportant, just to abstract things a little
+        local screen_width = self:getWidth()
+        local screen_height = self:getHeight()
+        local source = self:viewport(offs_x, offs_y, width, height)
+        local function putPixel(x,y,value)
+            return setter(dest, x, y, value, set_param)
         end
+        local function select_one_of_rgb(rgb, choice)
+            choice = (3 + choice) % 3
+            return Color8(bit.band(bit.rshift(ffi.cast(uint32pt, rgb)[0], choice*8), 0xff))
+        end
+
+        -- Imagine a minimal 8x8 CFA display, if it has pixel coordinates corresponding to hardware portrait:
+        -- (the arrows indicate "RGB order")
+        --
+        --    x=0 ->  ->    x=7
+        --y=0 R g b R g b R g
+        --    R g b R g b R g
+        --\|/ R g b R g b R g
+        --    g R g b R g b R
+        --\|/ g R g b R g b R
+        --    g R g b R g b R
+        --    b g R g b R g b
+        --y=7 b g R g b R g b
+        --
+        -- However pocketbook is rotated 90 clockwise it's a hardware landscape. So the real pixel coordinates
+        -- if you're *looking at a portrait* can be either (assuming the display is sane):
+        --   y=7   <- <-   y=0
+        --   g R b g R b g R x=0
+        --   g R b g R b g R 
+        --   g R b g R b g R \|/
+        --   b g R b g R b g 
+        --   b g R b g R b g \|/
+        --   b g R b g R b g 
+        --   R b g R b g R b 
+        --   R b g R b g R b x=7
+
+        -- But I'd not count on it. There's potential for insanity like this:
+        --   y=7  <- <-    y=0
+        --   R b g R b g R b x=7
+        --   R b g R b g R b  
+        --   R b g R b g R b 
+        --   b g R b g R b g 
+        --   b g R b g R b g /|\
+        --   b g R b g R b g  | 
+        --   g R b g R b g R 
+        --   g R b g R b g R x=0
+        --
+        -- With no "apparent" RGB anchor whatsoever! That's why we're counting "backwards" using right-to-left/bottom-up setting if needed,
+        -- and possibly adding small "fuzz" factor to account for invisible "starting red" pixel as if the screen was actually 9x9, not 8x8.
+        --
+        -- go from first row (in source image) to last row
+        for y = 0, height-1 do
+            -- from first column to the last one in this row
+            for x = 0, width-1 do
+                -- read a pixel from source image. we don't care where it really comes from as long we get 3 color components.
+                local rgb = source:getPixelP(x, y)
+
+                -- what is important is the on-screen coordinates, as that determines the color filter the pixel will be under
+                local pixel_x = dest_x + x
+                local pixel_y = dest_y + y
+
+                -------------------------------------------------
+                -- right-to-left and/or bottom-up rgb pixel order
+                -------------------------------------------------
+                if true then
+                    -- the row might right-to-level pixel order (probably the case on PB)
+                    pixel_x = screen_width - 1 - pixel_x
+                end
+                if false then
+                    -- or the column RGB can be ordered bottom-up
+                    pixel_y = screen_height - 1 - pixel_y
+                end
+
+                -------------------------------------------------
+                -- fuzz factors. on ideal displays both are 0
+                -------------------------------------------------
+                pixel_x = pixel_x + 0 -- something between -2..2
+                pixel_y = pixel_y + 0 -- something between -8..8
+
+                -- now that we have translated the pixel where it probably is "physically" on the mask.
+                -- the formula is simple "every 3 rows, we get the next color. and every column we can get next color too"
+                local choice = (pixel_x+math.floor(pixel_y/3))%3
+
+                --[[
+                -- Select one channel according to the result.
+                if choice == 0 then
+                    cfa_pixel = rgb.r
+                elseif choice == 1 then
+                    cfa_pixel = rgb.g
+                elseif choice == 2 then
+                    cfa_pixel = rgb.b
+                end
+                The conditions can't be really spelled like that as such code cannot be JITed.
+                Helper rselect_one_of_rgb() function does the equivalent without branching.
+                ]]
+                -- choice=0 for red, choice=1 for green, choice=2 for blue
+                local cfa_pixel = select_one_of_rgb(rgb, choice)
+
+                -- and draw the pixel, hopefully of correctly chosen color
+                putPixel(dest_x+x, dest_y+y, cfa_pixel)
+            end
+        end
+        -- we're finished painting on CFA surface
         debug.sethook(hook, mask)
         return
     end
