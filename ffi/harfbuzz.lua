5f9e9b7eb39fc98cdacba52d081fe2b559fcfb33
ezdiy <ezdiy@outlook.com>
2020-10-21 18:17:40 +0200

    harfbuzz: Don't crash on invalid language tags. (#1223)
    Fixes https://github.com/koreader/koreader/issues/6806

--- ffi/harfbuzz.lua
+++ ffi/harfbuzz.lua
@@ -18,13 +18,17 @@ function hb_face_t:getNames(maxlen)
     local buf = ffi.new("char[?]", maxlen)
     local res = {}
     for i=0, n[0]-1 do
-        local hb_lang = list[i].language
         local name_id = list[i].name_id
-        local got = hb.hb_ot_name_get_utf8(self, name_id, hb_lang, ffi.new("unsigned[1]", maxlen), buf)
-        hb_lang = ffi.string(hb.hb_language_to_string(hb_lang))
-        if got > 0 then
-            res[hb_lang] = res[hb_lang] or {}
-            res[hb_lang][name_id] = ffi.string(buf)
+        local hb_lang = list[i].language
+        local lang = hb.hb_language_to_string(hb_lang)
+        if lang ~= nil then
+            lang = ffi.string(lang)
+            local got = hb.hb_ot_name_get_utf8(self, name_id, hb_lang, ffi.new("unsigned[1]", maxlen), buf)
+            name_id = tonumber(name_id)
+            if got > 0 then
+                res[lang] = res[lang] or {}
+                res[lang][name_id] = ffi.string(buf)
+            end
         end
     end
     return res
@@ -89,7 +93,7 @@ end
 
 -- private
 
--- preprocess the script/language tables to HB range sets
+-- preprocess the script/language tables into HB range sets
 local function make_set(tab)
     local set = hb.hb_set_create()
     local first = 0
