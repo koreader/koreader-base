5ba7967930bb0434582e145e275ff6f97ec749bd
NiLuJe <ninuje@gmail.com>
2020-12-15 20:40:40 +0100

    CI: Update to Clang 11 & Busted 2.0.0
    Also resync GH workflow with front

--- .ci/emulator_install.sh
+++ .ci/emulator_install.sh
@@ -8,7 +8,7 @@ test -d "${HOME}/.luarocks" || {
     mkdir "${HOME}/.luarocks"
     cp /etc/luarocks/config-5.1.lua "${HOME}/.luarocks/config.lua"
     echo "wrap_bin_scripts = false" >>"${HOME}/.luarocks/config.lua"
-    travis_retry luarocks --local install busted 2.0.rc13-0
+    travis_retry luarocks --local install busted 2.0.0-1
     # for verbose_print module
     travis_retry luarocks --local install ansicolors
 }
@@ -18,11 +18,11 @@ eval "$(luarocks path --bin)"
 if [ "$CC" = "clang" ]; then
     wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 
-    sudo tee -a /etc/apt/sources.list.d/clang7.list >/dev/null <<EOF
-deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7 main
-deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7 main
+    sudo tee -a /etc/apt/sources.list.d/clang11.list >/dev/null <<EOF
+deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-11 main
+deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-11 main
 EOF
 
     sudo apt-get update
-    sudo apt-get install -y clang-7
+    sudo apt-get install -y clang-11
 fi
