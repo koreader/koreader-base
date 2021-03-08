3727ef33c92b8a94dc64873c6305726699207ad8
NiLuJe <ninuje@gmail.com>
2021-03-08 01:27:51 +0100

    RTC: Implement HCToSys (#1322)

--- ffi-cdecl/rtc_cdecl.c
+++ ffi-cdecl/rtc_cdecl.c
@@ -1,6 +1,7 @@
 #include <linux/rtc.h>
 #include <linux/ioctl.h>
 #include <time.h>
+#include <sys/time.h>
 
 #include "ffi-cdecl.h"
 
@@ -11,10 +12,14 @@ cdecl_const(RTC_AIE_ON)
 cdecl_const(RTC_AIE_OFF)
 cdecl_const(RTC_WKALM_SET)
 cdecl_const(RTC_WKALM_RD)
+cdecl_const(RTC_RD_TIME)
 
 cdecl_type(time_t)
 cdecl_struct(tm)
+cdecl_struct(timezone)
 cdecl_func(time)
 cdecl_func(gmtime)
 cdecl_func(localtime)
 cdecl_func(mktime)
+cdecl_func(settimeofday)
+cdecl_func(tzset)
