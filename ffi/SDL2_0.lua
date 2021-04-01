998e78355ead997c97fd089b239e130d59e9147c
NiLuJe <ninuje@gmail.com>
2021-04-01 03:19:26 +0200

    Amend that comment ;p.
    We kinda use those for swipes, actually ^^.

--- ffi/SDL2_0.lua
+++ ffi/SDL2_0.lua
@@ -402,7 +402,7 @@ function S.waitForEvent(sec, usec)
             genEmuEvent(C.EV_KEY, 1073741885, 1)
         end
 
-        -- We got an event we don't do anything with (e.g., SDL_MOUSEMOTION)
+        -- We got an event we don't necessarily do anything with (e.g., SDL_MOUSEMOTION)
         if timeout ~= -1 and #inputQueue == 0 then
             -- Back to Input:waitEvent to recompute the timeout
             return false, C.EINTR
