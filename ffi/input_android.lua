94d9b328105339fd7d1d2c24d258f3de6e8abb47
NiLuJe <ninuje@gmail.com>
2021-04-02 00:36:58 +0200

    Android: port to ne waitEvent API

--- ffi/input_android.lua
+++ ffi/input_android.lua
@@ -154,76 +154,76 @@ end
 function input.waitForEvent(sec, usec)
     -- TimeVal's :tomsecs if we were passed one to begin with, otherwise, -1 => block
     local timeout = sec and math.floor(sec * 1000000 + usec + 0.5) / 1000 or -1
-    while true do
-        -- check for queued events
-        if #inputQueue > 0 then
-            -- return oldest FIFO element
-            return true, table.remove(inputQueue, 1)
-        end
-        -- Will point to the the raw fd number
-        local fd     = ffi.new("int[1]")
-        -- Will point to the poll events
-        local events = ffi.new("int[1]")
-        -- Will point to the data passed at addFd/attachLooper time, c.f., the android_poll_source struct definition.
-        -- NOTE: Its id field is mostly redundant, as ALooper already returns the ident.
-        --       And its process function can be used as a weird delayed callback mechanism, but ALooper already has native callback handling :?.
-        --       TL;DR: We don't actually use it here.
-        local source = ffi.new("struct android_poll_source*[1]")
-        local poll_state = android.lib.ALooper_pollAll(timeout, fd, events, ffi.cast("void**", source))
-        if poll_state >= 0 then
-            -- NOTE: Since we actually want to process this in Lua-land (i.e., here), and not in C-land,
-            --       we do *NOT* make use of the weird delayed callback mechanism afforded by the android_poll_source struct
-            --       we pass as the data pointer to ALooper in the glue code when registering a polling source.
-            --       Instead, we do everything here, which is why this may look eerily like the C functions
-            --       process_cmd & process_input in the glue code.
-            --       Sidebar: if you *actually* need to process stuff in C-land ASAP, use ALooper's native callback system.
-            if poll_state == C.LOOPER_ID_MAIN then
-                -- e.g., source[0].process(android.app, source[0]) where process would point to process_cmd
-                local cmd = android.glue.android_app_read_cmd(android.app)
-                android.glue.android_app_pre_exec_cmd(android.app, cmd)
-                commandHandler(cmd, 1)
-                android.glue.android_app_post_exec_cmd(android.app, cmd)
-            elseif poll_state == C.LOOPER_ID_INPUT then
-                -- e.g., source[0].process(android.app, source[0]) where process would point to process_input
-                local event = ffi.new("AInputEvent*[1]")
-                while android.lib.AInputQueue_getEvent(android.app.inputQueue, event) >= 0 do
-                    if android.lib.AInputQueue_preDispatchEvent(android.app.inputQueue, event[0]) == 0 then
-                        local event_type = android.lib.AInputEvent_getType(event[0])
-                        local handled = 1
-                        if event_type == C.AINPUT_EVENT_TYPE_MOTION then
-                            motionEventHandler(event[0])
-                        elseif event_type == C.AINPUT_EVENT_TYPE_KEY then
-                            handled = keyEventHandler(event[0])
-                        end
-                        android.lib.AInputQueue_finishEvent(android.app.inputQueue, event[0], handled)
-                    end
-                end
-            elseif poll_state == C.LOOPER_ID_USER then
-                local message = ffi.new("unsigned char [4]")
-                C.read(fd[0], message, 4)
-                if message[0] == C.AEVENT_POWER_CONNECTED then
-                    commandHandler(C.AEVENT_POWER_CONNECTED, 0)
-                elseif message[0] == C.AEVENT_POWER_DISCONNECTED then
-                    commandHandler(C.AEVENT_POWER_DISCONNECTED, 0)
-                end
-            end
-            if android.app.destroyRequested ~= 0 then
-                android.LOGI("Engine thread destroy requested!")
-                -- Do nothing, if this is set, we've already pushed an APP_CMD_DESTROY event that'll get handled in front.
-            end
-        elseif poll_state == C.ALOOPER_POLL_TIMEOUT then
-            return false, C.ETIME
-        elseif poll_state == C.ALOOPER_POLL_ERROR then
-            android.LOGE("Encountered a polling error!")
-            return
-        end
-        -- NOTE: We never set callbacks, and we never call wake, so no need to check for ALOOPER_POLL_CALLBACK & ALOOPER_POLL_WAKE
 
-        -- poll returned early with something we don't do anything with
-        if timeout ~= -1 and #inputQueue == 0 then
-            -- Back to Input:waitEvent to recompute the timeout
-            return false, C.EINTR
+    -- Reset the queue
+    inputQueue = {}
+
+    -- Will point to the the raw fd number
+    local fd     = ffi.new("int[1]")
+    -- Will point to the poll events
+    local events = ffi.new("int[1]")
+    -- Will point to the data passed at addFd/attachLooper time, c.f., the android_poll_source struct definition.
+    -- NOTE: Its id field is mostly redundant, as ALooper already returns the ident.
+    --       And its process function can be used as a weird delayed callback mechanism, but ALooper already has native callback handling :?.
+    --       TL;DR: We don't actually use it here.
+    local source = ffi.new("struct android_poll_source*[1]")
+    local poll_state = android.lib.ALooper_pollAll(timeout, fd, events, ffi.cast("void**", source))
+    if poll_state >= 0 then
+        -- NOTE: Since we actually want to process this in Lua-land (i.e., here), and not in C-land,
+        --       we do *NOT* make use of the weird delayed callback mechanism afforded by the android_poll_source struct
+        --       we pass as the data pointer to ALooper in the glue code when registering a polling source.
+        --       Instead, we do everything here, which is why this may look eerily like the C functions
+        --       process_cmd & process_input in the glue code.
+        --       Sidebar: if you *actually* need to process stuff in C-land ASAP, use ALooper's native callback system.
+        if poll_state == C.LOOPER_ID_MAIN then
+            -- e.g., source[0].process(android.app, source[0]) where process would point to process_cmd
+            local cmd = android.glue.android_app_read_cmd(android.app)
+            android.glue.android_app_pre_exec_cmd(android.app, cmd)
+            commandHandler(cmd, 1)
+            android.glue.android_app_post_exec_cmd(android.app, cmd)
+        elseif poll_state == C.LOOPER_ID_INPUT then
+            -- e.g., source[0].process(android.app, source[0]) where process would point to process_input
+            local event = ffi.new("AInputEvent*[1]")
+            while android.lib.AInputQueue_getEvent(android.app.inputQueue, event) >= 0 do
+                if android.lib.AInputQueue_preDispatchEvent(android.app.inputQueue, event[0]) == 0 then
+                    local event_type = android.lib.AInputEvent_getType(event[0])
+                    local handled = 1
+                    if event_type == C.AINPUT_EVENT_TYPE_MOTION then
+                        motionEventHandler(event[0])
+                    elseif event_type == C.AINPUT_EVENT_TYPE_KEY then
+                        handled = keyEventHandler(event[0])
+                    end
+                    android.lib.AInputQueue_finishEvent(android.app.inputQueue, event[0], handled)
+                end
+            end
+        elseif poll_state == C.LOOPER_ID_USER then
+            local message = ffi.new("unsigned char [4]")
+            C.read(fd[0], message, 4)
+            if message[0] == C.AEVENT_POWER_CONNECTED then
+                commandHandler(C.AEVENT_POWER_CONNECTED, 0)
+            elseif message[0] == C.AEVENT_POWER_DISCONNECTED then
+                commandHandler(C.AEVENT_POWER_DISCONNECTED, 0)
+            end
         end
+        if android.app.destroyRequested ~= 0 then
+            android.LOGI("Engine thread destroy requested!")
+            -- Do nothing, if this is set, we've already pushed an APP_CMD_DESTROY event that'll get handled in front.
+        end
+    elseif poll_state == C.ALOOPER_POLL_TIMEOUT then
+        return false, C.ETIME
+    elseif poll_state == C.ALOOPER_POLL_ERROR then
+        android.LOGE("Encountered a polling error!")
+        return
+    end
+    -- NOTE: We never set callbacks, and we never call wake, so no need to check for ALOOPER_POLL_CALLBACK & ALOOPER_POLL_WAKE
+
+    if #inputQueue > 0 then
+        -- We generated some actionable events
+        return true, inputQueue
+    else
+        -- poll returned early, but without an event we actually use.
+        -- Back to Input:waitEvent to recompute the timeout
+        return false, C.EINTR
     end
 end
 
