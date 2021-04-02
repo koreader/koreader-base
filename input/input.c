4352c5269442ae7677a021dd4551c341f3bbdbab
NiLuJe <ninuje@gmail.com>
2021-04-02 17:43:08 +0200

    Tone down the queue size
    We're looping anyway, so the size isn't terribly important.

--- input/input.c
+++ input/input.c
@@ -322,10 +322,10 @@ static int waitForInput(lua_State* L)
             size_t j = 0U;    // Index of ev_array's tail
             for (;;) {
                 // NOTE: This should be more than enough ;).
-                //       FWIW, libevdev would default to 256 on most of our target devices,
+                //       FWIW, this matches libevdev's default nn most of our target devices,
                 //       because they don't support ABS_MT_SLOT.
                 //       c.f., https://gitlab.freedesktop.org/libevdev/libevdev/-/blob/8d70f449892c6f7659e07bb0f06b8347677bb7d8/libevdev/libevdev.c#L66-101
-                struct input_event input_queue[512U];  // 8K on 32-bit, 12K on 64-bit
+                struct input_event input_queue[256U];  // 4K on 32-bit, 6K on 64-bit
                 ssize_t            len = read(inputfds[i], &input_queue, sizeof(input_queue));
 
                 if (len < 0) {
