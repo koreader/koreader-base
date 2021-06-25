72f1bb671dc8b765e9ab17f0291b3d1d8b5a71dd
zwim <martin.zwicknagl@kirchbichl.net>
2021-06-25 10:17:57 +0200

    Add syntax check for regular expressions

--- cre.cpp
+++ cre.cpp
@@ -3318,6 +3318,14 @@ static int regularizeRegisteredFontsWeights(lua_State *L) {
 	return 0;
 }
 
+static int checkRegex(lua_State *L) {
+	CreDocument *doc		= (CreDocument*) luaL_checkudata(L, 1, "credocument");
+	const char *l_pattern   = luaL_checkstring(L, 2);
+	lString32 pattern		= lString32(l_pattern);
+    lua_pushinteger(L, checkRegex(pattern));
+    return 1;
+}
+
 // ported from Android UI kpvcrlib/crengine/android/jni/docview.cpp
 
 static int findText(lua_State *L) {
@@ -3650,6 +3658,7 @@ static const struct luaL_Reg credocument_meth[] = {
     {"setUserHyphenationDict", setUserHyphenationDict},
     {"getHyphenationForWord", getHyphenationForWord},
     {"getLowercasedWord", getLowercasedWord},
+    {"checkRegex", checkRegex},
     {"readDefaults", readDefaults},
     {"saveDefaults", saveDefaults},
     {"close", closeDocument},
