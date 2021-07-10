3d1e83c974fbf32e9969b8150485792c0b9220bf
zwim <martin.zwicknagl@kirchbichl.net>
2021-07-10 19:41:36 +0200

    Use utf8proc_NFKC_Casefold

--- ffi/utf8proc_h.lua
+++ ffi/utf8proc_h.lua
@@ -8,4 +8,6 @@ int32_t utf8proc_tolower(int32_t c);
 utf8proc_ssize_t utf8proc_iterate(const uint8_t *str, utf8proc_ssize_t strlen, int32_t *codepoint_ref);
 
 utf8proc_ssize_t utf8proc_encode_char(int32_t codepoint, uint8_t *dst);
+
+uint8_t* utf8proc_NFKC_Casefold(const uint8_t * str);
 ]]
