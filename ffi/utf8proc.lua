a44a69d9cd0c74600fa28a3ba994b1cade95315e
zwim <martin.zwicknagl@kirchbichl.net>
2021-07-10 19:41:36 +0200

    Use utf8proc_NFKC_Casefold

--- ffi/utf8proc.lua
+++ ffi/utf8proc.lua
@@ -21,41 +21,7 @@ end
 local Utf8Proc = {}
 
 function Utf8Proc.lowercase(str)
-    local str8_len = str:len()
-
-    local str8 = ffi.new("uint8_t[" .. str8_len .."]")
-    local str32 = ffi.new("int32_t[" .. str8_len .. "]")
-
-    -- copy string contents to uint8_t array
-    for i = 1,str8_len do
-        str8[i-1] = str:byte(i)
-    end
-
-    -- generate array of codepoints
-    local str32_len = 0
-    local pos8 = 0
-    while pos8<str8_len do
-        pos8 = pos8 + libutf8proc.utf8proc_iterate(str8 + pos8, str8_len-pos8, str32 + str32_len)
-        str32_len = str32_len + 1
-    end
-
-    -- tolower
-    for i = 0,str32_len-1 do
-        str32[i] = libutf8proc.utf8proc_tolower(str32[i])
-    end
-
-    -- encode back to utf8
-    pos8 = 0
-    for i = 0,3 do
-        pos8 = pos8 + libutf8proc.utf8proc_encode_char(str32[i], str8 + pos8);
-    end
-
-    local retval = ""
-    for i = 0,str8_len-1 do
-        retval = retval .. string.char(str8[i])
-    end
-
-    return retval
+    return ffi.string(libutf8proc.utf8proc_NFKC_Casefold(str))
 end
 
 return Utf8Proc
