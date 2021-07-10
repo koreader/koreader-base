8111dee00476b53e302e12753667bc542b357d0a
zwim <martin.zwicknagl@kirchbichl.net>
2021-07-10 17:10:19 +0200

    lint

--- ffi/utf8proc.lua
+++ ffi/utf8proc.lua
@@ -5,13 +5,11 @@ This is a LuaJIT FFI wrapper for utf8proc.
 ]]
 
 local ffi = require("ffi")
-local C = ffi.C
-local BB = require("ffi/blitbuffer")
 
 require("ffi/posix_h")
 require("ffi/utf8proc_h")
 
-local turbojpeg
+local libutf8proc
 if ffi.os == "Windows" then
     libutf8proc = ffi.load("libs/libutf8proc.dll")
 elseif ffi.os == "OSX" then
