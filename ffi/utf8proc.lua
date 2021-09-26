583f745120bd1671e11b13a7108b3714d13d356f
zwim <martin.zwicknagl@kirchbichl.net>
2021-09-26 19:18:28 +0200

    Add strlen for utf8 coded strings

--- ffi/utf8proc.lua
+++ ffi/utf8proc.lua
@@ -30,4 +30,16 @@ function Utf8Proc.lowercase(str)
     return folded_str
 end
 
+function Utf8Proc.strlen(str)
+    local str_buf = ffi.cast("const utf8proc_uint8_t *", str)
+    local buffer = ffi.new("utf8proc_int32_t[1]")
+    local retval = 0
+    local pos = 0
+    while str_buf[pos] ~= 0 do
+        pos = pos + libutf8proc.utf8proc_iterate(str_buf + pos, -1 , buffer)
+        retval = retval + 1
+    end
+    return retval
+end
+
 return Utf8Proc
