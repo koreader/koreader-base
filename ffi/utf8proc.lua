eb1285d2e3e20ecf886695a474cae11e556c3437
zwim <36999612+zwim@users.noreply.github.com>
2021-12-07 17:43:25 +0100

    utf8proc: add count() to count codepoints (#1414)

--- ffi/utf8proc.lua
+++ ffi/utf8proc.lua
@@ -37,4 +37,22 @@ function Utf8Proc.normalize_NFC(str)
     return normalized_str
 end
 
+function Utf8Proc.count(str)
+    local str_p = ffi.cast("const utf8proc_uint8_t *", str)
+    local codepoint = ffi.new("utf8proc_int32_t[1]")
+    local count = 0
+    local pos = 0
+    local str_len = #str -- may contain NUL
+    while pos < str_len do
+        local bytes = libutf8proc.utf8proc_iterate(str_p + pos, -1, codepoint)
+        if bytes > 0 then
+            count = count + 1
+            pos = pos + bytes
+        else
+            return count, false
+        end
+    end
+    return count, true
+end
+
 return Utf8Proc
