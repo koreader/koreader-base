a05938e2520725bf39865ae2da34e8ec8f420f32
NiLuJe <ninuje@gmail.com>
2022-02-21 01:00:07 +0100

    Refresh Kindle FFI stuff

--- ffi/mxcfb_kindle_h.lua
+++ ffi/mxcfb_kindle_h.lua
@@ -56,62 +56,62 @@ enum mxcfb_dithering_mode {
   EPDC_FLAG_USE_DITHERING_MAX = 5,
 };
 struct mxcfb_rect {
-  unsigned int top;
-  unsigned int left;
-  unsigned int width;
-  unsigned int height;
+  uint32_t top;
+  uint32_t left;
+  uint32_t width;
+  uint32_t height;
 };
 struct mxcfb_alt_buffer_data {
-  unsigned int phys_addr;
-  unsigned int width;
-  unsigned int height;
+  uint32_t phys_addr;
+  uint32_t width;
+  uint32_t height;
   struct mxcfb_rect alt_update_region;
 };
 struct mxcfb_update_data {
   struct mxcfb_rect update_region;
-  unsigned int waveform_mode;
-  unsigned int update_mode;
-  unsigned int update_marker;
-  unsigned int hist_bw_waveform_mode;
-  unsigned int hist_gray_waveform_mode;
+  uint32_t waveform_mode;
+  uint32_t update_mode;
+  uint32_t update_marker;
+  uint32_t hist_bw_waveform_mode;
+  uint32_t hist_gray_waveform_mode;
   int temp;
   unsigned int flags;
   struct mxcfb_alt_buffer_data alt_buffer_data;
 };
 struct mxcfb_update_data_zelda {
   struct mxcfb_rect update_region;
-  unsigned int waveform_mode;
-  unsigned int update_mode;
-  unsigned int update_marker;
+  uint32_t waveform_mode;
+  uint32_t update_mode;
+  uint32_t update_marker;
   int temp;
   unsigned int flags;
   int dither_mode;
   int quant_bit;
   struct mxcfb_alt_buffer_data alt_buffer_data;
-  unsigned int hist_bw_waveform_mode;
-  unsigned int hist_gray_waveform_mode;
-  unsigned int ts_pxp;
-  unsigned int ts_epdc;
+  uint32_t hist_bw_waveform_mode;
+  uint32_t hist_gray_waveform_mode;
+  uint32_t ts_pxp;
+  uint32_t ts_epdc;
 };
 struct mxcfb_update_data_rex {
   struct mxcfb_rect update_region;
-  unsigned int waveform_mode;
-  unsigned int update_mode;
-  unsigned int update_marker;
+  uint32_t waveform_mode;
+  uint32_t update_mode;
+  uint32_t update_marker;
   int temp;
   unsigned int flags;
   int dither_mode;
   int quant_bit;
   struct mxcfb_alt_buffer_data alt_buffer_data;
-  unsigned int hist_bw_waveform_mode;
-  unsigned int hist_gray_waveform_mode;
+  uint32_t hist_bw_waveform_mode;
+  uint32_t hist_gray_waveform_mode;
 };
 static const int MXCFB_SEND_UPDATE = 1078478382;
 static const int MXCFB_SEND_UPDATE_ZELDA = 1079526958;
 static const int MXCFB_SEND_UPDATE_REX = 1079002670;
 struct mxcfb_update_marker_data {
-  unsigned int update_marker;
-  unsigned int collision_test;
+  uint32_t update_marker;
+  uint32_t collision_test;
 };
 static const int MXCFB_WAIT_FOR_UPDATE_COMPLETE = 3221767727;
 static const int MXCFB_WAIT_FOR_UPDATE_COMPLETE_PEARL = 1074021935;
@@ -125,4 +125,71 @@ struct mxcfb_nightmode_ctrl {
   int current_level;
 };
 static const int MXCFB_SET_NIGHTMODE = 2147763786;
+static const int MTK_EPDC_FLAG_USE_DITHERING_Y4 = 16384;
+static const int MTK_EPDC_FLAG_USE_REGAL = 32768;
+static const int MTK_EPDC_FLAG_ENABLE_SWIPE = 65536;
+static const int MTK_WAVEFORM_MODE_INIT = 0;
+static const int MTK_WAVEFORM_MODE_DU = 1;
+static const int MTK_WAVEFORM_MODE_GC16 = 2;
+static const int MTK_WAVEFORM_MODE_GC16_FAST = 2;
+static const int MTK_WAVEFORM_MODE_GL16 = 3;
+static const int MTK_WAVEFORM_MODE_GL16_FAST = 3;
+static const int MTK_WAVEFORM_MODE_GL4 = 3;
+static const int MTK_WAVEFORM_MODE_GL16_INV = 3;
+static const int MTK_WAVEFORM_MODE_GLR16 = 4;
+static const int MTK_WAVEFORM_MODE_REAGL = 4;
+static const int MTK_WAVEFORM_MODE_GLD16 = 5;
+static const int MTK_WAVEFORM_MODE_REAGLD = 5;
+static const int MTK_WAVEFORM_MODE_A2 = 6;
+static const int MTK_WAVEFORM_MODE_DU4 = 7;
+static const int MTK_WAVEFORM_MODE_LAST = 7;
+static const int MTK_WAVEFORM_MODE_GCK16 = 8;
+static const int MTK_WAVEFORM_MODE_GLKW16 = 9;
+static const int MTK_WAVEFORM_MODE_GC16_PARTIAL = 10;
+static const int MTK_WAVEFORM_MODE_GCK16_PARTIAL = 11;
+static const int MTK_WAVEFORM_MODE_DUNM = 12;
+static const int MTK_WAVEFORM_MODE_P2SW = 13;
+enum MTK_SWIPE_DIRECTION_ENUM {
+  MTK_SWIPE_DOWN = 0,
+  MTK_SWIPE_UP = 1,
+  MTK_SWIPE_LEFT = 2,
+  MTK_SWIPE_RIGHT = 3,
+  MTK_SWIPE_MAX = 4,
+};
+struct mxcfb_swipe_data {
+  uint32_t direction;
+  uint32_t steps;
+};
+struct mxcfb_update_data_mtk {
+  struct mxcfb_rect update_region;
+  uint32_t waveform_mode;
+  uint32_t update_mode;
+  uint32_t update_marker;
+  int temp;
+  unsigned int flags;
+  int dither_mode;
+  int quant_bit;
+  struct mxcfb_alt_buffer_data alt_buffer_data;
+  struct mxcfb_swipe_data swipe_data;
+  uint32_t hist_bw_waveform_mode;
+  uint32_t hist_gray_waveform_mode;
+  uint32_t ts_pxp;
+  uint32_t ts_epdc;
+};
+struct mxcfb_halftone_data {
+  struct mxcfb_rect region[2];
+  int halftone_mode;
+};
+typedef union {
+  uint32_t flag;
+  uint32_t markers[64];
+} mxcfb_markers_data;
+static const int UPDATE_FLAGS_FAST_MODE = -2147483648;
+static const int UPDATE_FLAGS_MODE_FAST_FLAG = 1;
+static const int MXCFB_SEND_UPDATE_MTK = 1080051246;
+static const int MXCFB_SET_NIGHTMODE_MTK = 2148550218;
+static const int MXCFB_SET_HALFTONE_MTK = 1076119115;
+static const int MXCFB_WAIT_FOR_ANY_UPDATE_COMPLETE_MTK = 3221505591;
+static const int MXCFB_SET_UPDATE_FLAGS_MTK = 1074021947;
+static const int MXCFB_GET_UPDATE_FLAGS_MTK = 3221505596;
 ]]
