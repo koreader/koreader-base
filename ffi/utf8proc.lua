96dbb5dfa8300bf919f97ba4b72c08eceaba837e
zwim <martin.zwicknagl@kirchbichl.net>
2022-03-06 17:06:49 +0100

    a dumb lowercase

--- ffi/utf8proc.lua
+++ ffi/utf8proc.lua
@@ -23,11 +23,43 @@ end
 
 local Utf8Proc = {}
 
+-- with normalization
 function Utf8Proc.lowercase(str)
     local folded_strz = libutf8proc.utf8proc_NFKC_Casefold(str)
     local folded_str = ffi.string(folded_strz)
     C.free(folded_strz)
     return folded_str
+end
+
+-- no normalization here
+function Utf8Proc.lowercase_dumb(str)
+    local lowercased = ""
+    local tmp_str = (" "):rep(10)
+    local tmp_p = ffi.cast("utf8proc_uint8_t *", tmp_str)
+    local str_p = ffi.cast("const utf8proc_uint8_t *", str)
+    local codepoint = ffi.new("utf8proc_int32_t[1]")
+    local count = 0
+    local pos = 0
+    local str_len = #str -- may contain NUL
+    while pos < str_len do
+        -- get codepoint
+        local bytes = libutf8proc.utf8proc_iterate(str_p + pos, -1, codepoint)
+        -- lowercase codepoint
+        local lower_cp = libutf8proc.utf8proc_tolower(codepoint[0])
+        -- encode lowercased codepoint and get length of new char*
+        local lower_len = libutf8proc.utf8proc_encode_char(lower_cp, tmp_p)
+        tmp_p[lower_len] = 0
+        -- append
+        lowercased = lowercased .. ffi.string(tmp_p)
+
+        if bytes > 0 then
+            count = count + 1
+            pos = pos + bytes
+        else
+            return lowercased, false
+        end
+    end
+    return lowercased, true
 end
 
 function Utf8Proc.normalize_NFC(str)
