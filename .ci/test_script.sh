7a963ff84656bee280053c650c842030d911d95e
Frans de Jonge <fransdejonge@gmail.com>
2022-04-06 17:49:28 +0200

    Purge Github (#1465)
    This reverts commit ab2fa6f3aedb3e9a799a10df39b3bd5346de3f14.
    
    Fixes https://github.com/koreader/koreader-base/pull/1462#issuecomment-1070807378

--- .ci/test_script.sh
+++ .ci/test_script.sh
@@ -10,6 +10,7 @@ if [ "$TARGET" = "android" ]; then
         exit 1
     fi
 elif [ "$EMULATE_READER" = "1" ]; then
+    cp build/*/luajit "${HOME}/.luarocks/bin"
     # install tesseract trained language data for testing OCR functionality
     travis_retry wget https://src.fedoraproject.org/repo/pkgs/tesseract/tesseract-ocr-3.02.eng.tar.gz/3562250fe6f4e76229a329166b8ae853/tesseract-ocr-3.02.eng.tar.gz
     tar zxf tesseract-ocr-3.02.eng.tar.gz
