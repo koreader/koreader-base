f7599555fdd6f7f0ef3f5c7347646b4a358d0405
poire-z <poire-z@users.noreply.github.com>
2022-09-10 21:44:13 +0200

    Add thirdparty/lunasvg, patched and extended
    To be used by crengine for better SVG support than
    with NanoSVG.
    Will use the help of crengine to draw text and images.
    Build from upstream.
    Add patch to upstream code, which should hopefully
    be stable.
    Add our own files in a xtended/ subdir, so we can
    follow their history in here.

---
+++ thirdparty/lunasvg/xtended/xlunasvg.h
@@ -0,0 +1,64 @@
+#ifndef XLUNASVG_H
+#define XLUNASVG_H
+
+#include <functional>
+
+namespace lunasvg {
+
+typedef struct external_context_t external_context_t;
+
+// Font specification, given to external text to glyph SVG paths converter
+typedef struct {
+    const char * family = nullptr;
+    double size = 16;
+    short weight = 400;
+    bool italic = false;
+    unsigned int features = 0;
+    const char * lang = nullptr;
+} external_font_spec_t;
+
+// Prototype of callback (defined in xtspanelement.cpp) to receive SVG paths for each glyph
+typedef std::function<void(const char * path_d, double advance_x, double advance_y, bool can_adjust_from_previous, unsigned int base_char)> tspan_path_callback_t;
+
+// Prototype of external helper function called to convert text to glyph SVG paths
+typedef void (*external_text_to_paths_func_t)( external_context_t * xcontext, const char * text,
+                                external_font_spec_t * font_spec, tspan_path_callback_t callback );
+
+// Prototype of external helper function called to decode an image (url, "data:image...") and draw it onto the provided bitmap
+typedef bool (*external_draw_image_func_t)( external_context_t * xcontext, const char * url,
+                                const unsigned char * bitmap, int width, int height, double & original_aspect_ratio) ;
+
+// External context struct that can be provided to loadFromData() to get text and image support,
+// and give additional context about the SVG image and its target document/page/canvas.
+struct external_context_t {
+    // Pointer to any external object, so helper funcs can find and use it if needed
+    void * external_object = nullptr;
+
+    // Helper function to convert text to glyph SVG paths
+    external_text_to_paths_func_t text_to_paths_helper = nullptr;
+
+    // Helper function to decode an image (url, "data:image...") and draw it onto the provided bitmap
+    external_draw_image_func_t draw_image_helper = nullptr;
+
+    // Allow overridding SVG original size, so external engine does not need to rescale it and can get the best quality
+    int target_width = -1;
+    int target_height = -1;
+
+};
+
+} //namespace lunasvg
+
+// Avoid "error: 'to_string' is not a member of 'std'" on Android
+#ifdef __ANDROID__
+#include <string>
+#include <sstream>
+namespace std {
+    template <typename T> std::string to_string( const T& n ) {
+        std::ostringstream ss;
+        ss << n ;
+        return ss.str() ;
+    }
+}
+#endif
+
+#endif // XLUNASVG_H
---
+++ thirdparty/lunasvg/xtended/xtspanelement.h
@@ -0,0 +1,61 @@
+#ifndef TSPANELEMENT_H
+#define TSPANELEMENT_H
+
+#include "geometryelement.h"
+#include "layoutcontext.h"
+
+namespace lunasvg {
+
+enum class TextAnchor
+{
+    Start,
+    Middle,
+    End,
+};
+
+enum class LengthAdjust
+{
+    None,
+    Spacing,
+    SpacingAndGlyphs,
+};
+
+// Text drawing state, passed from <text> to/by all sub-<tspans>
+typedef struct {
+    double cursor_x;
+    double cursor_y;
+    double current_start_x;
+    double current_start_y;
+    double current_end_x;
+    double current_end_y;
+    bool current_started;
+    bool last_was_space;
+    bool is_pre;
+    bool is_vertical_rl;
+    TextAnchor text_anchor;
+    TextAnchor current_text_anchor;
+    LengthAdjust current_length_adjust;
+    double current_adjust_text_length;
+    std::unique_ptr<LayoutGroup> current_group;
+} text_state_t;
+
+class TSpanElement : public GeometryElement
+{
+public:
+    // Limited support for <textPath>, that we make just a special kind of <tspan> with TSpanElement(ElementId::TextPath)
+    TSpanElement(ElementId id=ElementId::TSpan);
+
+    static void addCurrentGroup(LayoutGroup* parent, text_state_t &text_state);
+
+    void layoutText(LayoutContext* context, LayoutGroup* parent, text_state_t &text_state) const;
+
+    void layout(LayoutContext* context, LayoutContainer* current) const override; // not used
+
+    Path path() const { return Path{}; } // not used
+
+    std::unique_ptr<Node> clone() const;
+};
+
+} // namespace lunasvg
+
+#endif // TSPANELEMENT_H
