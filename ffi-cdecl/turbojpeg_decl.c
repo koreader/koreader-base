5bfb67c310b7de6efe8bef961428b3ecc2f7c7d2
NiLuJe <ninuje@gmail.com>
2022-09-25 23:55:24 +0200

    Update to libjpeg-turbo 2.1.4

--- ffi-cdecl/turbojpeg_decl.c
+++ ffi-cdecl/turbojpeg_decl.c
@@ -5,13 +5,14 @@
 cdecl_type(tjhandle)
 
 cdecl_enum(TJPF)
+cdecl_enum(TJSAMP)
 
 cdecl_func(tjDestroy)
 cdecl_func(tjInitDecompress)
 cdecl_func(tjDecompressHeader2)
 cdecl_func(tjDecompressHeader3)
 cdecl_func(tjDecompress2)
-cdecl_func(tjDecompressToYUV)
+cdecl_func(tjDecompressToYUV2)
 cdecl_func(tjInitCompress)
 cdecl_func(tjFree)
 cdecl_func(tjCompress2)
