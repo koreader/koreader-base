d6aae57ffc9a1dcee7134fc8265a61075bd47ac7
NiLuJe <ninuje@gmail.com>
2022-10-03 19:21:02 +0200

    We don't build w/ Lua 5.2 compat, so keep using the real pairs, as I don't recall if next is JIT'ed

--- ffi/serpent.lua
+++ ffi/serpent.lua
@@ -4,7 +4,6 @@ local c, d = "Paul Kulchenko", "Lua serializer and pretty printer"
 local snum = {[tostring(1/0)]='1/0 --[[math.huge]]',[tostring(-1/0)]='-1/0 --[[-math.huge]]',[tostring(0/0)]='0/0'}
 local badtype = {thread = true, userdata = true, cdata = true}
 local getmetatable = debug and debug.getmetatable or getmetatable
-local pairs = function(t) return next, t end -- avoid using __pairs in Lua 5.2+
 local keyword, globals, G = {}, {}, (_G or _ENV)
 for _,k in ipairs({'and', 'break', 'do', 'else', 'elseif', 'end', 'false',
   'for', 'function', 'goto', 'if', 'in', 'local', 'nil', 'not', 'or', 'repeat',
