4a140f9d54851db1fd0e2f0e025f653289b5e4cf
Robert-Jan de Dreu <160743+rjd22@users.noreply.github.com>
2022-12-05 07:36:57 +0100

    Put back libkoreader that should not be removed

--- ffi/input.lua
+++ ffi/input.lua
@@ -8,4 +8,6 @@ elseif util.isAndroid() then
     return require("ffi/input_android")
 elseif util.isPocketbook() then
     return require("ffi/input_pocketbook")
+else
+    return require("libs/libkoreader-input")
 end
