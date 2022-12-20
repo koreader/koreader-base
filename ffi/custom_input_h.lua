a0c4d1214929cbd2691990fc017b678ad3322f02
NiLuJe <ninuje@gmail.com>
2022-12-20 04:40:08 +0100

    Use a custom EV_MSC code instead of a brand new type to save a branch in front

--- ffi/custom_input_h.lua
+++ ffi/custom_input_h.lua
@@ -1,16 +1,16 @@
 local ffi = require("ffi")
 
 -- Custom values we use for ev.type, ev.code & ev.value in Input and Device HAL implementations
--- NOTE: EV_SDL == 'S', EV_GYRO == 'G'
+-- NOTE: EV_SDL == 'S'
+--       MSC_GYRO == 'G'
 
 -- In particular, orientation constants, usable by Device implementations for platform-specific gyro translations.
 -- (matches framebuffer constants, which matches Linux <input/fb.h> FB_ROTATE_* constants).
 -- (i.e., this is how the device is *physically* oriented).
 ffi.cdef[[
-static const int EV_GYRO = 71;
 static const int EV_SDL = 83;
 
-static const int GYRO_HANDLED = 0;
+static const int MSC_GYRO = 71;
 
 static const int DEVICE_ORIENTATION_UPRIGHT = 0;
 static const int DEVICE_ORIENTATION_CLOCKWISE = 1;
