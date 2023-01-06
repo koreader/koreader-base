8b7a8a98c86bf31f9ba987e6a207ba52759f5b01
Piotrek Marciniak <piotrek@ptrm.eu>
2023-01-06 14:24:28 +0100

    Unhardcode Apple M1 brew paths for SDL2

--- ffi/SDL2_0.lua
+++ ffi/SDL2_0.lua
@@ -25,8 +25,7 @@ local SDL = util.ffiLoadCandidates{
     -- this unfortunately needs to be written in full due to the . in the name
     "libSDL2-2.0.so",
     "libSDL2-2.0.so.0",
-    "/usr/local/opt/lib/libSDL2.dylib",
-    "/opt/homebrew/lib/libSDL2.dylib",
+    util.KO_DYLD_PREFIX .. "/lib/libSDL2.dylib",
 }
 
 -- Some features (like SDL_GameControllerRumble) may require a minimum version
