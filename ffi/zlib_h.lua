e2aa2da4750ec8bb0cffbaee19d4972c394e5fbb
Benoit Pierre <benoit.pierre@gmail.com>
2023-06-11 19:19:02 +0200

    ffi/zlib: fix cdecl

--- ffi/zlib_h.lua
+++ ffi/zlib_h.lua
@@ -3,5 +3,6 @@ local ffi = require("ffi")
 ffi.cdef[[
 long unsigned int compressBound(long unsigned int);
 int compress2(unsigned char *, long unsigned int *, const unsigned char *, long unsigned int, int);
+long unsigned int crc32(long unsigned int, const unsigned char *, unsigned int);
 int uncompress(unsigned char *, long unsigned int *, const unsigned char *, long unsigned int);
 ]]
