koreader/koreader-base

commit 0f59e6a5af63b3a5827306cbcb7c67e05fbefe62

SDL: distinguish between window size and screen size
Pointer input and window resize events use window coordinates. Fixes
koreader#10420.

ffi/SDL2_0.lua
@@ -112,10 +112,10 @@ function S.open(w, h, x, y)
         if SDL.SDL_GetCurrentDisplayMode(0, mode) ~= 0 then
             error("SDL cannot get current display mode.")
         end
-        S.w, S.h = mode.w, mode.h
+        S.win_w, S.win_h = mode.w, mode.h
     else
-        S.w = tonumber(os.getenv("EMULATE_READER_W")) or w or 600
-        S.h = tonumber(os.getenv("EMULATE_READER_H")) or h or 800
+        S.win_w = tonumber(os.getenv("EMULATE_READER_W")) or w or 600
+        S.win_h = tonumber(os.getenv("EMULATE_READER_H")) or h or 800
     end
 
     -- Disable to work around an SDL issue in 2.0.22.
@@ -132,7 +132,7 @@ function S.open(w, h, x, y)
     S.screen = SDL.SDL_CreateWindow("KOReader",
         tonumber(os.getenv("KOREADER_WINDOW_POS_X")) or x or SDL.SDL_WINDOWPOS_UNDEFINED,
         tonumber(os.getenv("KOREADER_WINDOW_POS_Y")) or y or SDL.SDL_WINDOWPOS_UNDEFINED,
-        S.w, S.h,
+        S.win_w, S.win_h,
         bit.bor(full_screen and 1 or 0, SDL.SDL_WINDOW_RESIZABLE, SDL.SDL_WINDOW_ALLOW_HIGHDPI)
     )
 
@@ -147,6 +147,9 @@ function S.open(w, h, x, y)
         -- tl;dr Use Windows or X11 if you value your eyes and sanity. Or *gulp* Android. And Sailfish of course.
         S.w = tonumber(output_w[0])
         S.h = tonumber(output_h[0])
+    else
+        S.w = S.win_w
+        S.h = S.win_h
     end
     S.texture = S.createTexture()
 
@@ -359,6 +362,10 @@ function S.waitForEvent(sec, usec)
         return false, C.ETIME
     end
 
+    -- Used to scale coordinates of pointer events if ouput scaling is used
+    local scale_x = S.w / S.win_w
+    local scale_y = S.h / S.win_h
+
     -- if we got an event, examine it here and generate events for koreader
     if event.type == SDL.SDL_KEYDOWN then
         genEmuEvent(C.EV_KEY, event.key.keysym.sym, 1)
@@ -377,11 +384,11 @@ function S.waitForEvent(sec, usec)
         else
             if pointers[0] and pointers[0].down then
                 slot = 0 -- left mouse button down
-                genTouchMoveEvent(event, slot, event.motion.x, event.motion.y)
+                genTouchMoveEvent(event, slot, event.motion.x * scale_x, event.motion.y * scale_y)
             end
             if pointers[1] and pointers[1].down then
                 slot = 1 -- right mouse button down
-                genTouchMoveEvent(event, slot, event.motion.x, event.motion.y)
+                genTouchMoveEvent(event, slot, event.motion.x * scale_x, event.motion.y * scale_y)
             end
         end
     elseif event.type == SDL.SDL_MOUSEBUTTONUP and event.button.which ~= SDL_TOUCH_MOUSEID
@@ -396,8 +403,8 @@ function S.waitForEvent(sec, usec)
             slot = 0
         end
 
-        local x = is_finger and event.tfinger.x * S.w or event.button.x
-        local y = is_finger and event.tfinger.y * S.h or event.button.y
+        local x = is_finger and event.tfinger.x * S.w or event.button.x * scale_x
+        local y = is_finger and event.tfinger.y * S.h or event.button.y * scale_y
         genTouchUpEvent(event, slot, x, y)
         if is_finger then
             pointers[tonumber(event.tfinger.fingerId)] = nil
@@ -421,8 +428,8 @@ function S.waitForEvent(sec, usec)
         else -- SDL_BUTTON_LEFT
             slot = 0
         end
-        local x = is_finger and event.tfinger.x * S.w or event.button.x
-        local y = is_finger and event.tfinger.y * S.h or event.button.y
+        local x = is_finger and event.tfinger.x * S.w or event.button.x * scale_x
+        local y = is_finger and event.tfinger.y * S.h or event.button.y * scale_y
         setPointerDownState(slot, true)
         genTouchDownEvent(event, slot, x, y)
     elseif event.type == SDL.SDL_MULTIGESTURE then

ffi/framebuffer_SDL2_0.lua
@@ -10,7 +10,7 @@ local framebuffer = {
 
 function framebuffer:init()
     if not self.dummy then
-        SDL.open(self.w, self.h, self.x, self.y)
+        SDL.open(self.win_w, self.win_h, self.x, self.y)
         self:_newBB()
     else
         self.bb = BB.new(600, 800)
@@ -27,11 +27,14 @@ function framebuffer:init()
 end
 
 function framebuffer:resize(w, h)
-    w = w or SDL.w
-    h = h or SDL.h
     local output_w = ffi.new("int[1]", 0)
     local output_h = ffi.new("int[1]", 0)
-    if SDL.SDL.SDL_GetRendererOutputSize(SDL.renderer, output_w, output_h) == 0 and tonumber(output_w[0]) ~= w then
+
+    SDL.SDL.SDL_GetWindowSize(SDL.screen, output_w, output_h)
+    SDL.win_w = w or tonumber(output_w[0])
+    SDL.win_h = h or tonumber(output_h[0])
+
+    if SDL.SDL.SDL_GetRendererOutputSize(SDL.renderer, output_w, output_h) == 0 then
         -- This is a workaround to obtain a simulacrum of real pixels in scenarios that marketing likes to refer to as "HiDPI".
         -- The utterly deranged idea is to render things at 2x or 3x, so it can subsequently be scaled down in order to assure everything will always be at least a little bit blurry unless you happen to use exactly 2x or 3x, instead of the traditional methods of just rendering at the desired DPI that have worked perfectly fine in Windows and X11 for decades.
         -- Contrary to claims by the blind that macOS is sharp, it's not.
@@ -39,6 +42,9 @@ function framebuffer:resize(w, h)
         -- tl;dr Use Windows or X11 if you value your eyes and sanity. Or *gulp* Android. And Sailfish of course.
         w = tonumber(output_w[0])
         h = tonumber(output_h[0])
+    else
+        w = SDL.win_w
+        h = SDL.win_h
     end
 
     if not self.dummy then

commit e465170259fa5ef787b35008b0f26c1967c52ad9

SDL: Unbreak window state save & restore (#1627)
Fix https://github.com/koreader/koreader/issues/10502, regression since #1610

Well, only the size is a regression (plus, it was sortof an ffi/framebuffer API break, even if SDL is quirky in that fb.h / fb.w might not match fb.bb's dimensions because HiDPI shenanigans).

The position one is a bit of a mystery, we get an initial move event no matter what, for some reason, and without the explicit set, it's at the top left despite passing the right stuff to CreateWindow o_O.

Also dropped a duplicate openGameController call, as we handle it via an SDL_CONTROLLERDEVICEADDED event already.

ffi-cdecl/SDL2_0_decl.c
@@ -142,6 +142,8 @@ cdecl_func(SDL_CreateTexture)
 cdecl_func(SDL_UpdateTexture)
 cdecl_func(SDL_DestroyTexture)
 cdecl_func(SDL_SetWindowTitle)
+cdecl_func(SDL_GetWindowPosition)
+cdecl_func(SDL_SetWindowPosition)
 
 cdecl_type(SDL_blit)
 cdecl_func(SDL_CreateRGBSurface)

ffi/SDL2_0.lua
@@ -60,6 +60,7 @@ local SDL_TOUCH_MOUSEID = ffi.cast('uint32_t', -1);
 
 local S = {
     w = 0, h = 0,
+    win_w = 0, win_h = 0,
     screen = nil,
     renderer = nil,
     texture = nil,
@@ -129,12 +130,16 @@ function S.open(w, h, x, y)
     end
 
     -- set up screen (window)
+    local pos_x = tonumber(os.getenv("KOREADER_WINDOW_POS_X")) or x or SDL.SDL_WINDOWPOS_UNDEFINED
+    local pos_y = tonumber(os.getenv("KOREADER_WINDOW_POS_Y")) or y or SDL.SDL_WINDOWPOS_UNDEFINED
     S.screen = SDL.SDL_CreateWindow("KOReader",
-        tonumber(os.getenv("KOREADER_WINDOW_POS_X")) or x or SDL.SDL_WINDOWPOS_UNDEFINED,
-        tonumber(os.getenv("KOREADER_WINDOW_POS_Y")) or y or SDL.SDL_WINDOWPOS_UNDEFINED,
+        pos_x, pos_y,
         S.win_w, S.win_h,
-        bit.bor(full_screen and 1 or 0, SDL.SDL_WINDOW_RESIZABLE, SDL.SDL_WINDOW_ALLOW_HIGHDPI)
+        bit.bor(full_screen and SDL.SDL_WINDOW_FULLSCREEN or 0, SDL.SDL_WINDOW_RESIZABLE, SDL.SDL_WINDOW_ALLOW_HIGHDPI)
     )
+    -- For some mysterious reason, CreateWindow doesn't give a damn about the initial position, and will enforce top-left (we get an SDL_WINDOWEVENT_MOVED), so, force its hand...
+    -- What's even more curious is that we still only get a single SDL_WINDOWEVENT_MOVED on startup, except that way it's at the requested coordinates...
+    SDL.SDL_SetWindowPosition(S.screen, pos_x, pos_y)
 
     S.renderer = SDL.SDL_CreateRenderer(S.screen, -1, 0)
     local output_w = ffi.new("int[1]", 0)
@@ -152,8 +157,6 @@ function S.open(w, h, x, y)
         S.h = S.win_h
     end
     S.texture = S.createTexture()
-
-    openGameController()
 end
 
 function S:startTextInput()

ffi/SDL2_0_h.lua
@@ -805,6 +805,8 @@ SDL_Texture *SDL_CreateTexture(SDL_Renderer *, Uint32, int, int, int) __attribut
 int SDL_UpdateTexture(SDL_Texture *, const SDL_Rect *, const void *, int) __attribute__((visibility("default")));
 void SDL_DestroyTexture(SDL_Texture *) __attribute__((visibility("default")));
 void SDL_SetWindowTitle(SDL_Window *, const char *) __attribute__((visibility("default")));
+void SDL_GetWindowPosition(SDL_Window * window, int *x, int *y) __attribute__((visibility("default")));
+void SDL_SetWindowPosition(SDL_Window * window, int x, int y) __attribute__((visibility("default")));
 typedef int (*SDL_blit)(struct SDL_Surface *, SDL_Rect *, struct SDL_Surface *, SDL_Rect *);
 SDL_Surface *SDL_CreateRGBSurface(Uint32, int, int, int, Uint32, Uint32, Uint32, Uint32) __attribute__((visibility("default")));
 SDL_Surface *SDL_CreateRGBSurfaceFrom(void *, int, int, int, int, Uint32, Uint32, Uint32, Uint32) __attribute__((visibility("default")));

ffi/framebuffer_SDL2_0.lua
@@ -10,7 +10,10 @@ local framebuffer = {
 
 function framebuffer:init()
     if not self.dummy then
-        SDL.open(self.win_w, self.win_h, self.x, self.y)
+        -- Input dimensions are *window* sizes
+        SDL.open(self.w, self.h, self.x, self.y)
+        -- Open might fudge them a bit on the emu, so, resync
+        self.w, self.h = SDL.win_w, SDL.win_h
         self:_newBB()
     else
         self.bb = BB.new(600, 800)