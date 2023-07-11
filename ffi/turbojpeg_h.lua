af22471009041807ccabb39f56970c24bc8bb9bc
NiLuJe <ninuje@gmail.com>
2023-07-11 17:56:04 +0200

    Update to the TJ3 API

--- ffi/turbojpeg_h.lua
+++ ffi/turbojpeg_h.lua
@@ -1,7 +1,21 @@
 local ffi = require("ffi")
 
 ffi.cdef[[
-typedef void *tjhandle;
+enum TJINIT {
+  TJINIT_COMPRESS = 0,
+  TJINIT_DECOMPRESS = 1,
+  TJINIT_TRANSFORM = 2,
+};
+enum TJSAMP {
+  TJSAMP_444 = 0,
+  TJSAMP_422 = 1,
+  TJSAMP_420 = 2,
+  TJSAMP_GRAY = 3,
+  TJSAMP_440 = 4,
+  TJSAMP_411 = 5,
+  TJSAMP_441 = 6,
+  TJSAMP_UNKNOWN = -1,
+};
 enum TJPF {
   TJPF_RGB = 0,
   TJPF_BGR = 1,
@@ -17,24 +31,39 @@ enum TJPF {
   TJPF_CMYK = 11,
   TJPF_UNKNOWN = -1,
 };
-enum TJSAMP {
-  TJSAMP_444 = 0,
-  TJSAMP_422 = 1,
-  TJSAMP_420 = 2,
-  TJSAMP_GRAY = 3,
-  TJSAMP_440 = 4,
-  TJSAMP_411 = 5,
-  TJSAMP_441 = 6,
-  TJSAMP_UNKNOWN = -1,
+enum TJPARAM {
+  TJPARAM_STOPONWARNING = 0,
+  TJPARAM_BOTTOMUP = 1,
+  TJPARAM_NOREALLOC = 2,
+  TJPARAM_QUALITY = 3,
+  TJPARAM_SUBSAMP = 4,
+  TJPARAM_JPEGWIDTH = 5,
+  TJPARAM_JPEGHEIGHT = 6,
+  TJPARAM_PRECISION = 7,
+  TJPARAM_COLORSPACE = 8,
+  TJPARAM_FASTUPSAMPLE = 9,
+  TJPARAM_FASTDCT = 10,
+  TJPARAM_OPTIMIZE = 11,
+  TJPARAM_PROGRESSIVE = 12,
+  TJPARAM_SCANLIMIT = 13,
+  TJPARAM_ARITHMETIC = 14,
+  TJPARAM_LOSSLESS = 15,
+  TJPARAM_LOSSLESSPSV = 16,
+  TJPARAM_LOSSLESSPT = 17,
+  TJPARAM_RESTARTBLOCKS = 18,
+  TJPARAM_RESTARTROWS = 19,
+  TJPARAM_XDENSITY = 20,
+  TJPARAM_YDENSITY = 21,
+  TJPARAM_DENSITYUNITS = 22,
 };
-int tjDestroy(tjhandle);
-tjhandle tjInitDecompress(void);
-int tjDecompressHeader2(tjhandle, unsigned char *, long unsigned int, int *, int *, int *);
-int tjDecompressHeader3(tjhandle, const unsigned char *, long unsigned int, int *, int *, int *, int *);
-int tjDecompress2(tjhandle, const unsigned char *, long unsigned int, unsigned char *, int, int, int, int, int);
-int tjDecompressToYUV2(tjhandle, const unsigned char *, long unsigned int, unsigned char *, int, int, int, int);
-tjhandle tjInitCompress(void);
-void tjFree(unsigned char *);
-int tjCompress2(tjhandle, const unsigned char *, int, int, int, int, unsigned char **, long unsigned int *, int, int, int);
-int tjSaveImage(const char *, unsigned char *, int, int, int, int, int);
+typedef void *tjhandle;
+tjhandle tj3Init(int);
+int tj3Set(tjhandle, int, int);
+int tj3Get(tjhandle, int);
+int tj3Compress8(tjhandle, const unsigned char *, int, int, int, int, unsigned char **, size_t *);
+int tj3DecompressHeader(tjhandle, const unsigned char *, size_t);
+int tj3Decompress8(tjhandle, const unsigned char *, size_t, unsigned char *, int, int);
+void tj3Destroy(tjhandle);
+int tj3SaveImage8(tjhandle, const char *, const unsigned char *, int, int, int, int);
+void tj3Free(void *);
 ]]
