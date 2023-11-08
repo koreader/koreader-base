66d388a5df210594f98e05f09ba0902403dce48b
Wim de With <wf@dewith.io>
2023-11-08 23:59:36 +0100

    Add ffi definitions for libcrypto
    PBKDF2 with HMAC-SHA1 is used by WPA to derive the PSK. We currently use
    wpa_passphrase to calculate this PSK. Since we already bundle OpenSSL,
    and OpenSSL has a function for PBKDF2 with HMAC-SHA1, we can replace
    wpa_passphrase with a direct function call.

---
+++ ffi-cdecl/crypto_decl.c
@@ -0,0 +1,5 @@
+#include <openssl/evp.h>
+
+#include "ffi-cdecl.h"
+
+cdecl_func(PKCS5_PBKDF2_HMAC_SHA1)
---
+++ ffi/crypto_h.lua
@@ -0,0 +1,7 @@
+local ffi = require("ffi")
+
+ffi.cdef[[
+int PKCS5_PBKDF2_HMAC_SHA1(const char *pass, int passlen,
+                           const unsigned char *salt, int saltlen, int iter,
+                           int keylen, unsigned char *out);
+]]
