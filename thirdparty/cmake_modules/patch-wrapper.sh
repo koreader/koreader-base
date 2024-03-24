371660c6d35f1a91d5de7ee5c3143441d7e498d7
Benoit Pierre <benoit.pierre@gmail.com>
2024-03-24 22:29:27 +0000

    tweak `patch-wrapper.sh` (#1746)
    To support multiple patches in argument. Better than ignoring
    extra arguments after the first (e.g. in case of missing
    `COMMAND` in an external project patch command list).

--- thirdparty/cmake_modules/patch-wrapper.sh
+++ thirdparty/cmake_modules/patch-wrapper.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
+set -e
+
 if [ $# -eq 0 ]; then
-    echo "Patch file required as argument."
+    echo "Patch file(s) required as argument(s)."
     exit 1
 fi
 
-PATCH_FILE="$1"
-echo "* Applying ${PATCH_FILE} . . ."
-
-# Reverse patch will succeed if the patch is already applied.
-# In case of failure, it means we should try to apply the patch.
-if ! patch -R -p1 -N --dry-run <"${PATCH_FILE}" >/dev/null 2>&1; then
-    # Now patch for real.
-    patch -p1 -N <"${PATCH_FILE}"
-    exit $?
-fi
+for patch in "$@"; do
+    echo "* Applying ${patch} . . ."
+    # Reverse patch will succeed if the patch is already applied.
+    # In case of failure, it means we should try to apply the patch.
+    if ! patch -R -p1 -N --dry-run --input="${patch}" >/dev/null 2>&1; then
+        # Now patch for real.
+        patch -p1 -N --input="${patch}"
+    fi
+done
