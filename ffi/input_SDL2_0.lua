5bb141241a7173561db4e83cff0109d3b96c4f1f
NiLuJe <ninuje@gmail.com>
2024-05-14 18:35:47 +0200

    Stray paren

--- ffi/input_SDL2_0.lua
+++ ffi/input_SDL2_0.lua
@@ -8,7 +8,7 @@ return {
     fakeTapInput = function() end,
     -- NOP:
     close = function() end,
-    -- NOP: Input & Output are handled as one, and we call SDL_Quit in fb:close already)
+    -- NOP: Input & Output are handled as one, and we call SDL_Quit in fb:close already
     closeAll = function() end,
     -- Tell front that we're a custom imp with no concept of paths/fd
     is_ffi = true,
