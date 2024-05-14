17332673ea39fd543fd6fe46e8168e64b7ce5b55
NiLuJe <ninuje@gmail.com>
2024-05-14 18:36:36 +0200

    Less redundant comments

--- ffi/input_SDL2_0.lua
+++ ffi/input_SDL2_0.lua
@@ -4,11 +4,10 @@ local SDL = require("ffi/SDL2_0")
 return {
     open = SDL.open,
     waitForEvent = SDL.waitForEvent,
-    -- NOP:
+    -- NOPs:
     fakeTapInput = function() end,
-    -- NOP:
     close = function() end,
-    -- NOP: Input & Output are handled as one, and we call SDL_Quit in fb:close already
+    -- Input & Output are handled as one, and we call SDL_Quit in fb:close already
     closeAll = function() end,
     -- Tell front that we're a custom imp with no concept of paths/fd
     is_ffi = true,
