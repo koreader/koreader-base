8da7153302a0c942df157a98dd2e7861829b1648
Benoit Pierre <benoit.pierre@gmail.com>
2024-05-28 20:57:55 +0200

    giflib: update to 5.2.1

--- thirdparty/giflib/build.cmake
+++ thirdparty/giflib/build.cmake
@@ -2,9 +2,9 @@ cmake_minimum_required(VERSION 3.14)
 project(giflib LANGUAGES C)
 
 add_library(gif SHARED
-    lib/dgif_lib.c lib/egif_lib.c
-    lib/gifalloc.c lib/gif_err.c lib/gif_font.c lib/gif_hash.c
-    lib/openbsd-reallocarray.c lib/quantize.c)
+    dgif_lib.c egif_lib.c
+    gifalloc.c gif_err.c gif_font.c gif_hash.c
+    openbsd-reallocarray.c quantize.c)
 set_target_properties(gif PROPERTIES SOVERSION 7)
 if(DEFINED ANDROID_STL)
     target_compile_definitions(gif PRIVATE -DS_IREAD=S_IRUSR -DS_IWRITE=S_IWUSR)
