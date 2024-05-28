ef0e446bffe5a51996700ea22abb7d00c924c74c
Benoit Pierre <benoit.pierre@gmail.com>
2024-05-28 20:55:09 +0200

    ci/circle: check libraries for missing dependencies

--- .ci/test_script.sh
+++ .ci/test_script.sh
@@ -4,17 +4,25 @@ CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=/dev/null
 source "${CI_DIR}/common.sh"
 
-if [ "$TARGET" = "android" ]; then
-    if [ -f build/luajit ]; then
-        echo "ERROR: android build should not include luajit binary."
-        exit 1
+exit_code=0
+
+echo -e "${ANSI_GREEN}Libcheck results${ANSI_RESET}"
+make libcheck || exit_code=1
+
+if [ "$EMULATE_READER" = "1" ]; then
+    echo -e "\n${ANSI_GREEN}Testsuite${ANSI_RESET}"
+    # cp build/luajit "${HOME}/.luarocks/bin/"
+    (
+        # install test data
+        travis_retry make test-data
+        # finally make test
+        travis_retry make --assume-old=all test
+    ) || exit_code=1
+else
+    if [ "$TARGET" = "android" ] && [ -f build/luajit ]; then
+        echo -e "\n${ANSI_RED}ERROR${ANSI_RESET}: android build should not include luajit binary."
+        exit_code=1
     fi
-elif [ "$EMULATE_READER" = "1" ]; then
-    cp build/luajit "${HOME}/.luarocks/bin/"
-    # install test data
-    travis_retry make test-data
-    # finally make test
-    travis_retry make --assume-old=all test
 fi
 
-echo "test passed."
+exit ${exit_code}
