koreader/koreader-base

commit a6e112a5ee06e6939203accbd0736134af8939cd

mupdf: update to 1.24.2

ffi-cdecl/mupdf_decl.c
@@ -9,13 +9,10 @@ cdecl_const(FZ_STEXT_BLOCK_TEXT)
 
 /* math */
 cdecl_type(fz_point)
-cdecl_struct(fz_point_s)
+cdecl_type(fz_quad)
 cdecl_type(fz_rect)
-cdecl_struct(fz_rect_s)
 cdecl_type(fz_irect)
-cdecl_struct(fz_irect_s)
 cdecl_type(fz_matrix)
-cdecl_struct(fz_matrix_s)
 cdecl_var(fz_identity)
 cdecl_var(fz_empty_rect)
 
@@ -48,8 +45,11 @@ cdecl_func(fz_runetochar)
 
 /* document */
 cdecl_type(fz_stream)
+cdecl_type(fz_location)
 cdecl_type(fz_outline)
-cdecl_struct(fz_outline_s)
+cdecl_struct(fz_outline)
+cdecl_type(fz_cookie)
+cdecl_type(fz_separations)
 cdecl_type(fz_page)
 cdecl_type(fz_document)
 cdecl_type(fz_device)
@@ -66,15 +66,19 @@ cdecl_func(fz_lookup_metadata)
 
 /* page */
 cdecl_func(mupdf_load_page)
-cdecl_func(fz_bound_page)
+cdecl_func(mupdf_fz_bound_page)
 cdecl_func(fz_drop_page)
 
 /* links */
 cdecl_type(fz_link)
-cdecl_struct(fz_link_s)
+cdecl_struct(fz_link)
 cdecl_func(mupdf_load_links)
-cdecl_func(fz_resolve_link)
+cdecl_func(mupdf_fz_resolve_link)
 cdecl_func(fz_drop_link)
+
+/* location */
+cdecl_func(mupdf_fz_page_number_from_location)
+cdecl_func(mupdf_fz_location_from_page_number)
 
 /* outline */
 cdecl_func(mupdf_load_outline)
@@ -86,20 +90,20 @@ cdecl_func(mupdf_open_memory)
 
 /* structured text */
 cdecl_type(fz_stext_char)
-cdecl_struct(fz_stext_char_s)
+cdecl_struct(fz_stext_char)
 
 cdecl_type(fz_stext_line)
-cdecl_struct(fz_stext_line_s)
+cdecl_struct(fz_stext_line)
 
 cdecl_type(fz_stext_block)
-cdecl_struct(fz_stext_block_s)
+cdecl_struct(fz_stext_block)
 cdecl_type(fz_stext_options)
-cdecl_struct(fz_stext_options_s)
 cdecl_type(fz_stext_page)
-cdecl_struct(fz_stext_page_s)
 cdecl_func(mupdf_new_stext_page_from_page)
 cdecl_func(fz_drop_stext_page)
 
+cdecl_type(fz_color_params)
+cdecl_type(fz_default_colorspaces)
 cdecl_func(fz_default_color_params)
 
 /* pixmaps */
@@ -135,12 +139,12 @@ cdecl_type(pdf_page)
 cdecl_type(pdf_document)
 
 /* annotations */
-cdecl_func(pdf_specifics)
 cdecl_func(mupdf_pdf_create_annot)
 cdecl_func(mupdf_pdf_delete_annot)
 cdecl_func(mupdf_pdf_set_annot_quad_points)
 cdecl_func(mupdf_pdf_set_annot_contents)
-cdecl_func(mupdf_pdf_set_markup_appearance)
+cdecl_func(mupdf_pdf_set_annot_color)
+cdecl_func(mupdf_pdf_set_annot_opacity)
 cdecl_func(mupdf_pdf_first_annot)
 cdecl_func(mupdf_pdf_next_annot)
 cdecl_func(mupdf_pdf_annot_quad_point_count)
@@ -148,7 +152,6 @@ cdecl_func(mupdf_pdf_annot_quad_point)
 
 /* saving documents */
 cdecl_type(pdf_write_options)
-cdecl_struct(pdf_write_options_s)
 cdecl_func(mupdf_pdf_save_document)
 
 /* the following is for our own wrapper lib: */
@@ -158,10 +161,11 @@ cdecl_func(mupdf_error_code)
 cdecl_func(mupdf_error_message)
 
 /* geometry */
-cdecl_func(fz_scale)
-cdecl_func(fz_translate)
-cdecl_func(fz_pre_rotate)
-cdecl_func(fz_pre_translate)
-cdecl_func(fz_transform_rect)
-cdecl_func(fz_round_rect)
-cdecl_func(fz_union_rect)
+cdecl_func(mupdf_fz_scale)
+cdecl_func(mupdf_fz_translate)
+cdecl_func(mupdf_fz_pre_rotate)
+cdecl_func(mupdf_fz_pre_translate)
+cdecl_func(mupdf_fz_transform_rect)
+cdecl_func(mupdf_fz_round_rect)
+cdecl_func(mupdf_fz_union_rect)
+cdecl_func(mupdf_fz_rect_from_quad)

ffi/mupdf.lua
@@ -41,7 +41,7 @@ local mupdf = {
 }
 -- this cannot get adapted by the cdecl file because it is a
 -- string constant. Must match the actual mupdf API:
-local FZ_VERSION = "1.13.0"
+local FZ_VERSION = "1.24.2"
 
 local document_mt = { __index = {} }
 local page_mt = { __index = {} }
@@ -221,7 +221,7 @@ end
 local function toc_walker(toc, outline, depth)
     while outline ~= nil do
         table.insert(toc, {
-            page = outline.page + 1,
+            page = outline.page.page + 1,
             title = ffi.string(outline.title),
             depth = depth,
         })
@@ -335,7 +335,6 @@ function document_mt.__index:writeDocument(filename)
     opts[0].do_ascii = 0
     opts[0].do_garbage = 0
     opts[0].do_linear = 0
-    opts[0].continue_on_error = 1
     local ok = W.mupdf_pdf_save_document(context(), ffi.cast("pdf_document*", self.doc), filename, opts)
     if ok == nil then merror("could not write document") end
 end
@@ -370,11 +369,11 @@ function page_mt.__index:getSize(draw_context)
     local bounds = ffi.new("fz_rect")
     local ctm = ffi.new("fz_matrix")
 
-    M.fz_scale(ctm, draw_context.zoom, draw_context.zoom)
-    M.fz_pre_rotate(ctm, draw_context.rotate)
+    W.mupdf_fz_scale(ctm, draw_context.zoom, draw_context.zoom)
+    W.mupdf_fz_pre_rotate(ctm, draw_context.rotate)
 
-    M.fz_bound_page(context(), self.page, bounds)
-    M.fz_transform_rect(bounds, ctm)
+    W.mupdf_fz_bound_page(context(), self.page, bounds)
+    W.mupdf_fz_transform_rect(bounds, ctm)
 
     -- NOTE: fz_bound_page returns an internal representation computed @ 72dpi...
     --       It is often superbly mysterious, even for images,
@@ -529,8 +528,10 @@ function page_mt.__index:getPageText()
                                 break
                             end
                             textlen = textlen + M.fz_runetochar(textbuf + textlen, ch.c)
-                            M.fz_union_rect(word_bbox, ch.bbox)
-                            M.fz_union_rect(line_bbox, ch.bbox)
+                            local char_bbox = ffi.new("fz_rect")
+                            W.mupdf_fz_rect_from_quad(char_bbox, ch.quad)
+                            W.mupdf_fz_union_rect(word_bbox, char_bbox)
+                            W.mupdf_fz_union_rect(line_bbox, char_bbox)
                             if ch.c >= 0x4e00 and ch.c <= 0x9FFF or -- CJK Unified Ideographs
                                 ch.c >= 0x2000 and ch.c <= 0x206F or -- General Punctuation
                                 ch.c >= 0x3000 and ch.c <= 0x303F or -- CJK Symbols and Punctuation
@@ -598,11 +599,13 @@ function page_mt.__index:getPageLinks()
             x1 = link.rect.x1, y1 = link.rect.y1,
         }
         local pos = ffi.new("float[2]")
-        local page = M.fz_resolve_link(context(), link.doc, link.uri, pos, pos+1)
-        if page >= 0 then
-            data.page = page -- FIXME page+1?
-        else
+        local location = ffi.new("fz_location")
+        W.mupdf_fz_resolve_link(context(), self.doc.doc, link.uri, pos, pos+1, location)
+        -- `fz_resolve_link` return a location of (-1, -1) for external links.
+        if location.chapter == -1 and location.page == -1 then
             data.uri = ffi.string(link.uri)
+        else
+            data.page = W.mupdf_fz_page_number_from_location(context(), self.doc.doc, location)
         end
         data.pos = {
             x = pos[0], y = pos[1],
@@ -646,9 +649,9 @@ TODO: make this the used interface
 function page_mt.__index:draw_new(draw_context, width, height, offset_x, offset_y)
     local ctm = ffi.new("fz_matrix")
 
-    M.fz_scale(ctm, draw_context.zoom, draw_context.zoom)
-    M.fz_pre_rotate(ctm, draw_context.rotate)
-    M.fz_pre_translate(ctm, draw_context.offset_x, draw_context.offset_y)
+    W.mupdf_fz_scale(ctm, draw_context.zoom, draw_context.zoom)
+    W.mupdf_fz_pre_rotate(ctm, draw_context.rotate)
+    W.mupdf_fz_pre_translate(ctm, draw_context.offset_x, draw_context.offset_y)
 
     local bbox = ffi.new("fz_irect", offset_x, offset_y, offset_x + width, offset_y + height)
 
@@ -681,8 +684,6 @@ mupdf.LINE_THICKNESS = 0.05
 function page_mt.__index:addMarkupAnnotation(points, n, type)
     local color = ffi.new("float[3]")
     local alpha = 1.0
-    local line_height = 0.5
-    local line_thickness = 1.0
     if type == M.PDF_ANNOT_HIGHLIGHT then
         color[0] = 1.0
         color[1] = 1.0
@@ -692,34 +693,30 @@ function page_mt.__index:addMarkupAnnotation(points, n, type)
         color[0] = 0.0
         color[1] = 0.0
         color[2] = 1.0
-        line_thickness = mupdf.LINE_THICKNESS
-        line_height = mupdf.UNDERLINE_HEIGHT
     elseif type == M.PDF_ANNOT_STRIKE_OUT then
         color[0] = 1.0
         color[1] = 0.0
         color[2] = 0.0
-        line_thickness = mupdf.LINE_THICKNESS
-        line_height = mupdf.STRIKE_HEIGHT
     else
         return
     end
-
-    local doc = M.pdf_specifics(context(), self.doc.doc)
-    if doc == nil then merror("could not get pdf_specifics") end
 
     local annot = W.mupdf_pdf_create_annot(context(), ffi.cast("pdf_page*", self.page), type)
     if annot == nil then merror("could not create annotation") end
 
     local ok = W.mupdf_pdf_set_annot_quad_points(context(), annot, n, points)
-    if ok == nil then merror("could not set markup annot quadpoints") end
+    if ok == nil then merror("could not set annotation quadpoints") end
 
-    ok = W.mupdf_pdf_set_markup_appearance(context(), doc, annot, color, alpha, line_thickness, line_height)
-    if ok == nil then merror("could not set markup appearance") end
+    ok = W.mupdf_pdf_set_annot_color(context(), annot, 3, color)
+    if ok == nil then merror("could not set annotation color") end
+
+    ok = W.mupdf_pdf_set_annot_opacity(context(), annot, alpha)
+    if ok == nil then merror("could not set annotation opacity") end
 
     -- Fetch back MuPDF's stored coordinates of all quadpoints, as they may have been modified/rounded
     -- (we need the exact ones that were saved if we want to be able to find them for deletion/update)
     for i = 0, n-1 do
-        W.mupdf_pdf_annot_quad_point(context(), annot, i, points+i*8)
+        W.mupdf_pdf_annot_quad_point(context(), annot, i, points+i)
     end
 end
 
@@ -729,24 +726,25 @@ function page_mt.__index:deleteMarkupAnnotation(annot)
 end
 
 function page_mt.__index:getMarkupAnnotation(points, n)
-    local doc = M.pdf_specifics(context(), self.doc.doc)
-    if doc == nil then merror("could not get pdf_specifics") end
-
     local annot = W.mupdf_pdf_first_annot(context(), ffi.cast("pdf_page*", self.page))
     while annot ~= nil do
         local n2 = W.mupdf_pdf_annot_quad_point_count(context(), annot)
         if n == n2 then
-            local quadpoint = ffi.new("float[?]", 8)
+            local quadpoint = ffi.new("fz_quad[1]")
             local match = true
             for i = 0, n-1 do
                 W.mupdf_pdf_annot_quad_point(context(), annot, i, quadpoint)
-                for k = 0, 7 do
-                    if points[i*8 + k] ~= quadpoint[k] then
-                        match = false
-                        break
-                    end
+                if (points[i].ul.x ~= quadpoint[0].ul.x or
+                    points[i].ul.y ~= quadpoint[0].ul.y or
+                    points[i].ur.x ~= quadpoint[0].ur.x or
+                    points[i].ur.y ~= quadpoint[0].ur.y or
+                    points[i].ll.x ~= quadpoint[0].ll.x or
+                    points[i].ll.y ~= quadpoint[0].ll.y or
+                    points[i].lr.x ~= quadpoint[0].lr.x or
+                    points[i].lr.y ~= quadpoint[0].lr.y) then
+                    match = false
+                    break
                 end
-                if not match then break end
             end
             if match then return annot end
         end
@@ -756,8 +754,6 @@ function page_mt.__index:getMarkupAnnotation(points, n)
 end
 
 function page_mt.__index:updateMarkupAnnotation(annot, contents)
-    local doc = M.pdf_specifics(context(), self.doc.doc)
-    if doc == nil then merror("could not get pdf_specifics") end
     local ok = W.mupdf_pdf_set_annot_contents(context(), annot, contents)
     if ok == nil then merror("could not update markup annot contents") end
 end
@@ -996,9 +992,9 @@ local function render_for_kopt(bmp, page, scale, bounds)
 
     local bbox = ffi.new("fz_irect")
     local ctm = ffi.new("fz_matrix")
-    M.fz_scale(ctm, scale, scale)
-    M.fz_transform_rect(bounds, ctm)
-    M.fz_round_rect(bbox, bounds)
+    W.mupdf_fz_scale(ctm, scale, scale)
+    W.mupdf_fz_transform_rect(bounds, ctm)
+    W.mupdf_fz_round_rect(bbox, bounds)
 
     local colorspace = mupdf.color and M.fz_device_rgb(context())
         or M.fz_device_gray(context())
@@ -1057,7 +1053,7 @@ function page_mt.__index:toBmp(bmp, dpi, color)
     mupdf.color = color and true or false
 
     local bounds = ffi.new("fz_rect")
-    M.fz_bound_page(context(), self.page, bounds)
+    W.mupdf_fz_bound_page(context(), self.page, bounds)
 
     render_for_kopt(bmp, self, dpi/72, bounds)
 

ffi/mupdf_h.lua
@@ -2,70 +2,100 @@ local ffi = require("ffi")
 
 ffi.cdef[[
 static const int FZ_STEXT_BLOCK_TEXT = 0;
-typedef struct fz_point_s fz_point;
-struct fz_point_s {
+typedef struct {
   float x;
   float y;
-};
-typedef struct fz_rect_s fz_rect;
-struct fz_rect_s {
+} fz_point;
+typedef struct {
+  fz_point ul;
+  fz_point ur;
+  fz_point ll;
+  fz_point lr;
+} fz_quad;
+typedef struct {
   float x0;
   float y0;
   float x1;
   float y1;
-};
-typedef struct fz_irect_s fz_irect;
-struct fz_irect_s {
+} fz_rect;
+typedef struct {
   int x0;
   int y0;
   int x1;
   int y1;
-};
-typedef struct fz_matrix_s fz_matrix;
-struct fz_matrix_s {
+} fz_irect;
+typedef struct {
   float a;
   float b;
   float c;
   float d;
   float e;
   float f;
-};
+} fz_matrix;
 extern const fz_matrix fz_identity;
 extern const fz_rect fz_empty_rect;
-typedef struct fz_context_s fz_context;
-typedef struct fz_font_s fz_font;
+typedef struct fz_context fz_context;
+typedef struct fz_font fz_font;
 void fz_install_external_font_funcs(fz_context *);
-typedef struct fz_buffer_s fz_buffer;
+typedef struct {
+  int refs;
+  unsigned char *data;
+  size_t cap;
+  size_t len;
+  int unused_bits;
+  int shared;
+} fz_buffer;
 fz_buffer *mupdf_new_buffer_from_shared_data(fz_context *, const unsigned char *, size_t);
 void *mupdf_drop_buffer(fz_context *, fz_buffer *);
-typedef struct fz_alloc_context_s fz_alloc_context;
-typedef struct fz_colorspace_s fz_colorspace;
-typedef struct fz_locks_context_s fz_locks_context;
+typedef struct {
+  void *user;
+  void *(*malloc)(void *, size_t);
+  void *(*realloc)(void *, void *, size_t);
+  void (*free)(void *, void *);
+} fz_alloc_context;
+typedef struct fz_colorspace fz_colorspace;
+typedef struct {
+  void *user;
+  void (*lock)(void *, int);
+  void (*unlock)(void *, int);
+} fz_locks_context;
 fz_context *fz_new_context_imp(const fz_alloc_context *, const fz_locks_context *, size_t, const char *);
 void fz_drop_context(fz_context *);
 void fz_register_document_handlers(fz_context *);
-typedef struct fz_image_s fz_image;
-typedef struct fz_pixmap_s fz_pixmap;
+typedef struct fz_image fz_image;
+typedef struct fz_pixmap fz_pixmap;
 fz_image *mupdf_new_image_from_buffer(fz_context *, fz_buffer *);
 fz_pixmap *mupdf_get_pixmap_from_image(fz_context *, fz_image *, const fz_irect *, fz_matrix *, int *, int *);
 void fz_drop_image(fz_context *, fz_image *);
 int fz_runetochar(char *, int);
-typedef struct fz_stream_s fz_stream;
-typedef struct fz_outline_s fz_outline;
-struct fz_outline_s {
+typedef struct fz_stream fz_stream;
+typedef struct {
+  int chapter;
+  int page;
+} fz_location;
+typedef struct fz_outline fz_outline;
+struct fz_outline {
   int refs;
   char *title;
   char *uri;
-  int page;
+  fz_location page;
   float x;
   float y;
-  fz_outline *next;
-  fz_outline *down;
+  struct fz_outline *next;
+  struct fz_outline *down;
   int is_open;
 };
-typedef struct fz_page_s fz_page;
-typedef struct fz_document_s fz_document;
-typedef struct fz_device_s fz_device;
+typedef struct {
+  int abort;
+  int progress;
+  size_t progress_max;
+  int errors;
+  int incomplete;
+} fz_cookie;
+typedef struct fz_separations fz_separations;
+typedef struct fz_page fz_page;
+typedef struct fz_document fz_document;
+typedef struct fz_device fz_device;
 fz_document *mupdf_open_document(fz_context *, const char *);
 fz_document *mupdf_open_document_with_stream(fz_context *, const char *, fz_stream *);
 int fz_is_document_reflowable(fz_context *, fz_document *);
@@ -76,34 +106,40 @@ int mupdf_count_pages(fz_context *, fz_document *);
 void *mupdf_layout_document(fz_context *, fz_document *, float, float, float);
 int fz_lookup_metadata(fz_context *, fz_document *, const char *, char *, int);
 fz_page *mupdf_load_page(fz_context *, fz_document *, int);
-fz_rect *fz_bound_page(fz_context *, fz_page *, fz_rect *);
+fz_rect *mupdf_fz_bound_page(fz_context *, fz_page *, fz_rect *);
 void fz_drop_page(fz_context *, fz_page *);
-typedef struct fz_link_s fz_link;
-struct fz_link_s {
+typedef struct fz_link fz_link;
+struct fz_link {
   int refs;
-  fz_link *next;
+  struct fz_link *next;
   fz_rect rect;
-  void *doc;
   char *uri;
+  void (*set_rect_fn)(fz_context *, fz_link *, fz_rect);
+  void (*set_uri_fn)(fz_context *, fz_link *, const char *);
+  void (*drop)(fz_context *, fz_link *);
 };
 fz_link *mupdf_load_links(fz_context *, fz_page *);
-int fz_resolve_link(fz_context *, fz_document *, const char *, float *, float *);
+fz_location *mupdf_fz_resolve_link(fz_context *, fz_document *, const char *, float *, float *, fz_location *);
 void fz_drop_link(fz_context *, fz_link *);
+int mupdf_fz_page_number_from_location(fz_context *, fz_document *, fz_location *);
+void *mupdf_fz_location_from_page_number(fz_context *, fz_document *, fz_location *, int);
 fz_outline *mupdf_load_outline(fz_context *, fz_document *);
 void fz_drop_outline(fz_context *, fz_outline *);
 void *mupdf_drop_stream(fz_context *, fz_stream *);
 fz_stream *mupdf_open_memory(fz_context *, const unsigned char *, size_t);
-typedef struct fz_stext_char_s fz_stext_char;
-struct fz_stext_char_s {
+typedef struct fz_stext_char fz_stext_char;
+struct fz_stext_char {
   int c;
+  int bidi;
+  int color;
   fz_point origin;
-  fz_rect bbox;
+  fz_quad quad;
   float size;
   fz_font *font;
   fz_stext_char *next;
 };
-typedef struct fz_stext_line_s fz_stext_line;
-struct fz_stext_line_s {
+typedef struct fz_stext_line fz_stext_line;
+struct fz_stext_line {
   int wmode;
   fz_point dir;
   fz_rect bbox;
@@ -112,8 +148,8 @@ struct fz_stext_line_s {
   fz_stext_line *prev;
   fz_stext_line *next;
 };
-typedef struct fz_stext_block_s fz_stext_block;
-struct fz_stext_block_s {
+typedef struct fz_stext_block fz_stext_block;
+struct fz_stext_block {
   int type;
   fz_rect bbox;
   union {
@@ -129,39 +165,51 @@ struct fz_stext_block_s {
   fz_stext_block *prev;
   fz_stext_block *next;
 };
-typedef struct fz_stext_options_s fz_stext_options;
-struct fz_stext_options_s {
+typedef struct {
   int flags;
-};
-typedef struct fz_stext_page_s fz_stext_page;
-struct fz_stext_page_s {
-  struct fz_pool_s *pool;
+  float scale;
+} fz_stext_options;
+typedef struct {
+  struct fz_pool *pool;
   fz_rect mediabox;
   fz_stext_block *first_block;
   fz_stext_block *last_block;
-};
+} fz_stext_page;
 fz_stext_page *mupdf_new_stext_page_from_page(fz_context *, fz_page *, const fz_stext_options *);
 void fz_drop_stext_page(fz_context *, fz_stext_page *);
-const struct fz_color_params_s *fz_default_color_params(fz_context *);
-fz_pixmap *fz_new_pixmap(fz_context *, fz_colorspace *, int, int, struct fz_separations_s *, int);
-fz_pixmap *mupdf_new_pixmap_with_bbox(fz_context *, fz_colorspace *, const fz_irect *, struct fz_separations_s *, int);
-fz_pixmap *mupdf_new_pixmap_with_data(fz_context *, fz_colorspace *, int, int, struct fz_separations_s *, int, int, unsigned char *);
-fz_pixmap *mupdf_new_pixmap_with_bbox_and_data(fz_context *, fz_colorspace *, const fz_irect *, struct fz_separations_s *, int, unsigned char *);
-fz_pixmap *fz_convert_pixmap(fz_context *, fz_pixmap *, fz_colorspace *, fz_colorspace *, struct fz_default_colorspaces_s *, const struct fz_color_params_s *, int);
+typedef struct {
+  uint8_t ri;
+  uint8_t bp;
+  uint8_t op;
+  uint8_t opm;
+} fz_color_params;
+typedef struct {
+  int refs;
+  fz_colorspace *gray;
+  fz_colorspace *rgb;
+  fz_colorspace *cmyk;
+  fz_colorspace *oi;
+} fz_default_colorspaces;
+extern const fz_color_params fz_default_color_params;
+fz_pixmap *fz_new_pixmap(fz_context *, fz_colorspace *, int, int, fz_separations *, int);
+fz_pixmap *mupdf_new_pixmap_with_bbox(fz_context *, fz_colorspace *, const fz_irect *, fz_separations *, int);
+fz_pixmap *mupdf_new_pixmap_with_data(fz_context *, fz_colorspace *, int, int, fz_separations *, int, int, unsigned char *);
+fz_pixmap *mupdf_new_pixmap_with_bbox_and_data(fz_context *, fz_colorspace *, const fz_irect *, fz_separations *, int, unsigned char *);
+fz_pixmap *fz_convert_pixmap(fz_context *, const fz_pixmap *, fz_colorspace *, fz_colorspace *, fz_default_colorspaces *, fz_color_params, int);
 void fz_drop_pixmap(fz_context *, fz_pixmap *);
 void fz_clear_pixmap_with_value(fz_context *, fz_pixmap *, int);
 void fz_gamma_pixmap(fz_context *, fz_pixmap *, float);
-fz_pixmap *fz_scale_pixmap(fz_context *, fz_pixmap *, float, float, float, float, fz_irect *);
-int fz_pixmap_width(fz_context *, fz_pixmap *);
-int fz_pixmap_height(fz_context *, fz_pixmap *);
-int fz_pixmap_components(fz_context *, fz_pixmap *);
-unsigned char *fz_pixmap_samples(fz_context *, fz_pixmap *);
+fz_pixmap *fz_scale_pixmap(fz_context *, fz_pixmap *, float, float, float, float, const fz_irect *);
+int fz_pixmap_width(fz_context *, const fz_pixmap *);
+int fz_pixmap_height(fz_context *, const fz_pixmap *);
+int fz_pixmap_components(fz_context *, const fz_pixmap *);
+unsigned char *fz_pixmap_samples(fz_context *, const fz_pixmap *);
 fz_colorspace *fz_device_gray(fz_context *);
 fz_colorspace *fz_device_rgb(fz_context *);
 fz_colorspace *fz_device_bgr(fz_context *);
 fz_device *mupdf_new_draw_device(fz_context *, const fz_matrix *, fz_pixmap *);
 fz_device *mupdf_new_bbox_device(fz_context *, fz_rect *);
-void *mupdf_run_page(fz_context *, fz_page *, fz_device *, const fz_matrix *, struct fz_cookie_s *);
+void *mupdf_run_page(fz_context *, fz_page *, fz_device *, const fz_matrix *, fz_cookie *);
 void fz_close_device(fz_context *, fz_device *);
 void fz_drop_device(fz_context *, fz_device *);
 enum pdf_annot_type {
@@ -177,36 +225,38 @@ enum pdf_annot_type {
   PDF_ANNOT_UNDERLINE = 9,
   PDF_ANNOT_SQUIGGLY = 10,
   PDF_ANNOT_STRIKE_OUT = 11,
-  PDF_ANNOT_STAMP = 12,
-  PDF_ANNOT_CARET = 13,
-  PDF_ANNOT_INK = 14,
-  PDF_ANNOT_POPUP = 15,
-  PDF_ANNOT_FILE_ATTACHMENT = 16,
-  PDF_ANNOT_SOUND = 17,
-  PDF_ANNOT_MOVIE = 18,
-  PDF_ANNOT_WIDGET = 19,
-  PDF_ANNOT_SCREEN = 20,
-  PDF_ANNOT_PRINTER_MARK = 21,
-  PDF_ANNOT_TRAP_NET = 22,
-  PDF_ANNOT_WATERMARK = 23,
-  PDF_ANNOT_3D = 24,
+  PDF_ANNOT_REDACT = 12,
+  PDF_ANNOT_STAMP = 13,
+  PDF_ANNOT_CARET = 14,
+  PDF_ANNOT_INK = 15,
+  PDF_ANNOT_POPUP = 16,
+  PDF_ANNOT_FILE_ATTACHMENT = 17,
+  PDF_ANNOT_SOUND = 18,
+  PDF_ANNOT_MOVIE = 19,
+  PDF_ANNOT_RICH_MEDIA = 20,
+  PDF_ANNOT_WIDGET = 21,
+  PDF_ANNOT_SCREEN = 22,
+  PDF_ANNOT_PRINTER_MARK = 23,
+  PDF_ANNOT_TRAP_NET = 24,
+  PDF_ANNOT_WATERMARK = 25,
+  PDF_ANNOT_3D = 26,
+  PDF_ANNOT_PROJECTION = 27,
   PDF_ANNOT_UNKNOWN = -1,
 };
-typedef struct pdf_annot_s pdf_annot;
-typedef struct pdf_page_s pdf_page;
-typedef struct pdf_document_s pdf_document;
-pdf_document *pdf_specifics(fz_context *, fz_document *);
+typedef struct pdf_annot pdf_annot;
+typedef struct pdf_page pdf_page;
+typedef struct pdf_document pdf_document;
 pdf_annot *mupdf_pdf_create_annot(fz_context *, pdf_page *, enum pdf_annot_type);
 void *mupdf_pdf_delete_annot(fz_context *, pdf_page *, pdf_annot *);
-void *mupdf_pdf_set_annot_quad_points(fz_context *, pdf_annot *, int, const float *);
+void *mupdf_pdf_set_annot_quad_points(fz_context *, pdf_annot *, int, const fz_quad *);
 void *mupdf_pdf_set_annot_contents(fz_context *, pdf_annot *, const char *);
-void *mupdf_pdf_set_markup_appearance(fz_context *, pdf_document *, pdf_annot *, float *, float, float, float);
+void *mupdf_pdf_set_annot_color(fz_context *, pdf_annot *, int, const float *);
+void *mupdf_pdf_set_annot_opacity(fz_context *, pdf_annot *, float);
 pdf_annot *mupdf_pdf_first_annot(fz_context *, pdf_page *);
 pdf_annot *mupdf_pdf_next_annot(fz_context *, pdf_annot *);
 int mupdf_pdf_annot_quad_point_count(fz_context *, pdf_annot *);
-void *mupdf_pdf_annot_quad_point(fz_context *, pdf_annot *, int, float *);
-typedef struct pdf_write_options_s pdf_write_options;
-struct pdf_write_options_s {
+void *mupdf_pdf_annot_quad_point(fz_context *, pdf_annot *, int, fz_quad *);
+typedef struct {
   int do_incremental;
   int do_pretty;
   int do_ascii;
@@ -218,19 +268,28 @@ struct pdf_write_options_s {
   int do_linear;
   int do_clean;
   int do_sanitize;
-  int continue_on_error;
-  int *errors;
-};
+  int do_appearance;
+  int do_encrypt;
+  int dont_regenerate_id;
+  int permissions;
+  char opwd_utf8[128];
+  char upwd_utf8[128];
+  int do_snapshot;
+  int do_preserve_metadata;
+  int do_use_objstms;
+  int compression_effort;
+} pdf_write_options;
 void *mupdf_pdf_save_document(fz_context *, pdf_document *, const char *, pdf_write_options *);
 fz_alloc_context *mupdf_get_my_alloc_context();
 int mupdf_get_cache_size();
 int mupdf_error_code(fz_context *);
 char *mupdf_error_message(fz_context *);
-fz_matrix *fz_scale(fz_matrix *, float, float);
-fz_matrix *fz_translate(fz_matrix *, float, float);
-fz_matrix *fz_pre_rotate(fz_matrix *, float);
-fz_matrix *fz_pre_translate(fz_matrix *, float, float);
-fz_rect *fz_transform_rect(fz_rect *restrict, const fz_matrix *restrict);
-fz_irect *fz_round_rect(fz_irect *restrict, const fz_rect *restrict);
-fz_rect *fz_union_rect(fz_rect *restrict, const fz_rect *restrict);
+fz_matrix *mupdf_fz_scale(fz_matrix *, float, float);
+fz_matrix *mupdf_fz_translate(fz_matrix *, float, float);
+fz_matrix *mupdf_fz_pre_rotate(fz_matrix *, float);
+fz_matrix *mupdf_fz_pre_translate(fz_matrix *, float, float);
+fz_rect *mupdf_fz_transform_rect(fz_rect *, const fz_matrix *);
+fz_irect *mupdf_fz_round_rect(fz_irect *, const fz_rect *);
+fz_rect *mupdf_fz_union_rect(fz_rect *, const fz_rect *);
+fz_rect *mupdf_fz_rect_from_quad(fz_rect *, const fz_quad *);
 ]]

spec/unit/mupdf_spec.lua
@@ -73,11 +73,10 @@ describe("mupdf module", function()
     describe("PDF document API", function()
         local doc1, doc2, doc3
         local ffi = require("ffi")
-        local annotation_quadpoints = ffi.new("float[8]", {
-                 70,  930,
-                510,  930,
-                510,  970,
-                 70,  970 })
+        local annotation_quadpoints = ffi.new("fz_quad[1]", {{
+            { 70,  930, 510,  930 },
+            { 510,  970, 70,  970 }
+        }})
         setup(function()
             doc1 = M.openDocument(sample_pdf)
             assert.is_not_nil(doc1)
@@ -179,6 +178,7 @@ describe("mupdf module", function()
             local page
             local dc
             setup(function()
+                doc3:authenticatePassword("test")
                 page = doc3:openPage(2)
                 dc = require("ffi/drawcontext").new()
             end)
@@ -194,7 +194,7 @@ describe("mupdf module", function()
                 assert.equals(math.floor(bbox[1]*1000), 56145)
                 assert.equals(math.floor(bbox[2]*1000), 69233)
                 assert.equals(math.floor(bbox[3]*1000), 144790)
-                assert.equals(math.floor(bbox[4]*1000), 106089)
+                assert.equals(math.floor(bbox[4]*1000), 103669)
             end)
             it("should get page text", function()
                 local text = page:getPageText()

commit 14a4eef65a347805df648cdd34e86ec047b237ee

MuPDF: Unbreak RGB->BGR swap
fz_default_color_params is no longer a function, but a plain constant.

Regression since #1805

Re: https://github.com/koreader/koreader/issues/11952

ffi-cdecl/mupdf_decl.c
@@ -104,7 +104,7 @@ cdecl_func(fz_drop_stext_page)
 
 cdecl_type(fz_color_params)
 cdecl_type(fz_default_colorspaces)
-cdecl_func(fz_default_color_params)
+cdecl_const(fz_default_color_params)
 
 /* pixmaps */
 cdecl_func(fz_new_pixmap) // compat

ffi/mupdf.lua
@@ -807,7 +807,7 @@ function mupdf.renderImage(data, size, width, height)
     -- Handle RGB->BGR conversion for Kobos when needed
     local bb
     if mupdf.bgr and ncomp >= 3 then
-        local bgr_pixmap = M.fz_convert_pixmap(context(), pixmap, M.fz_device_bgr(context()), nil, nil, M.fz_default_color_params(context()), (ncomp == 4 and 1 or 0))
+        local bgr_pixmap = M.fz_convert_pixmap(context(), pixmap, M.fz_device_bgr(context()), nil, nil, M.fz_default_color_params, (ncomp == 4 and 1 or 0))
         M.fz_drop_pixmap(context(), pixmap)
 
         local p = M.fz_pixmap_samples(context(), bgr_pixmap)