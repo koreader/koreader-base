fc9c8aab47c9ceb3adc63fb6eb9ccac28c632f2a
Benoit Pierre <benoit.pierre@gmail.com>
2024-06-11 22:14:16 +0200

    utils: lint

--- utils/bindeps.sh
+++ utils/bindeps.sh
@@ -12,25 +12,25 @@ fi
 
 os="$(uname -s)"
 case "${os}" in
-Darwin)
-    binary_mime_rx='application/x-mach-binary'
-    bindeps() {
-        otool -L "$1" | grep -E $'^\t'
-        otool -l "$1" | sed -n -e '/^ *cmd LC_RPATH$/!d' -e ':_loop' -e 'n;/^ *path /!b_loop' -e 'p'
-    }
-    find_exe_opts=(-perm -300)
-    ;;
-Linux)
-    binary_mime_rx='application/x-(executable|pie-executable|sharedlib)'
-    bindeps() {
-        readelf -d "$1" | grep -wE '^readelf|NEEDED|RPATH|SONAME'
-    }
-    find_exe_opts=(-executable)
-    ;;
-*)
-    echo "unsupported operating system: ${os}" 1>&2
-    exit 1
-    ;;
+    Darwin)
+        binary_mime_rx='application/x-mach-binary'
+        bindeps() {
+            otool -L "$1" | grep -E $'^\t'
+            otool -l "$1" | sed -n -e '/^ *cmd LC_RPATH$/!d' -e ':_loop' -e 'n;/^ *path /!b_loop' -e 'p'
+        }
+        find_exe_opts=(-perm -300)
+        ;;
+    Linux)
+        binary_mime_rx='application/x-(executable|pie-executable|sharedlib)'
+        bindeps() {
+            readelf -d "$1" | grep -wE '^readelf|NEEDED|RPATH|SONAME'
+        }
+        find_exe_opts=(-executable)
+        ;;
+    *)
+        echo "unsupported operating system: ${os}" 1>&2
+        exit 1
+        ;;
 esac
 
 find "$@" -type f "${find_exe_opts[@]}" \
