df29afc2c568f4b0935db40a2feda2d736458052
Benoit Pierre <benoit.pierre@gmail.com>
2024-06-11 22:14:15 +0200

    ci: improve lint script
    - do run luacheck even if there are shellcheck issues
    - always run luacheck directly: so a luacheck install for
      another LUA version or using a shell script launcher
      works (the docker image wrapper already uses luajit)
    - don't disable colors in luacheck output

--- .ci/lint_script.sh
+++ .ci/lint_script.sh
@@ -4,11 +4,11 @@ CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=/dev/null
 source "${CI_DIR}/common.sh"
 
-"${CI_DIR}/helper_shellchecks.sh"
+exit_code=0
+
+"${CI_DIR}/helper_shellchecks.sh" || exit_code=1
 
 echo -e "\n${ANSI_GREEN}Luacheck results${ANSI_RESET}"
-if ! command -v luajit; then
-    luacheck --no-color -q ffi spec
-else
-    luajit "$(command -v luacheck)" --no-color -q ffi spec
-fi
+luacheck -q ffi spec || exit_code=1
+
+exit ${exit_code}
