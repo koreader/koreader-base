8d613ad40f3ffdca755d89c594c38ae169c7ebb2
Benoit Pierre <benoit.pierre@gmail.com>
2024-06-11 22:14:15 +0200

    ci: use new shellcheck helper

--- .ci/helper_shellchecks.sh
+++ .ci/helper_shellchecks.sh
@@ -7,21 +7,4 @@ source "${CI_DIR}/common.sh"
 # shellcheck disable=2016
 mapfile -t shellscript_locations < <({ git grep -lE '^#!(/usr)?/bin/(env )?(bash|sh)' && git submodule --quiet foreach '[ "$path" = "thirdparty/kpvcrlib/crengine" ] && git grep -lE "^#!(/usr)?/bin/(env )?(bash|sh)" | grep .ci | sed "s|^|$path/|" || git grep -lE "^#!(/usr)?/bin/(env )?(bash|sh)" | sed "s|^|$path/|"' && git ls-files ./*.sh; } | sort | uniq)
 
-SHELLSCRIPT_ERROR=0
-
-for shellscript in "${shellscript_locations[@]}"; do
-    echo -e "${ANSI_GREEN}Running shellcheck on ${shellscript}"
-    shellcheck "${shellscript}" || SHELLSCRIPT_ERROR=1
-    echo -e "${ANSI_GREEN}Running shfmt on ${shellscript}"
-    if ! shfmt -i 4 "${shellscript}" >/dev/null 2>&1; then
-        echo -e "${ANSI_RED}Warning: ${shellscript} contains the following problem:"
-        shfmt -i 4 "${shellscript}" || SHELLSCRIPT_ERROR=1
-        continue
-    fi
-    if [ "$(cat "${shellscript}")" != "$(shfmt -i 4 "${shellscript}")" ]; then
-        echo -e "${ANSI_RED}Warning: ${shellscript} does not abide by coding style, diff for expected style:"
-        shfmt -i 4 "${shellscript}" | diff "${shellscript}" - || SHELLSCRIPT_ERROR=1
-    fi
-done
-
-exit "${SHELLSCRIPT_ERROR}"
+./utils/shellcheck.sh "${shellscript_locations[@]}"
