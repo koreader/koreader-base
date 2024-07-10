1720023ddd09160e38d12f421f3a6c67f5976a7b
Benoit Pierre <benoit.pierre@gmail.com>
2024-07-10 21:06:40 +0200

    cmake: rewrite external projects support
    Don't use CMake's `ExternalProject` support, this allow for consolidating some steps:
    - download: clone / download to `thirdparty/project/build/source` / `thirdparty/project/build/downloads`
    - prepare: (re-)create source directory, apply patches (overlay, files, command).
    - configure: (re-)create build directory (or clean up source directory when not building out-of-tree), configure project
    - build: self-explanatory
    - install: self-explanatory
    
    Merging the extract / checkout creation with applying patches into one
    prepare step ensure the later are always applied starting from a pristine
    source tree: interrupting the step will not result in a broken (partially
    patched) tree.
    
    Steps are triggered on the following:
    - download: parameters changed
    - prepare: download step ran, patch overlay contents changed, patch list and/or contents changed, patch command changed
    - configure: prepare step ran, command line changed, koreader/toolchain file contents changed, direct dependency change
      (e.g. a zlib's version change — archive MD5 or Git revision — trigger a re-build of libpng)
    - build: configure step ran, command line changed, source file timestamp changed, direct dependency install step ran
    - install: build step ran, command line changed

---
+++ thirdparty/cmake_modules/koreader_external_project.cmake
@@ -0,0 +1,322 @@
+find_package(Git REQUIRED)
+
+# Transform paths in depfiles to be absolute.
+if(POLICY CMP0116)
+    cmake_policy(SET CMP0116 NEW)
+endif()
+
+# Crappy macOS command line utilities strike again…
+set(PRINTF_QS "%q")
+if(DARWIN)
+    set(PRINTF_QS "'%s'")
+endif()
+set(KOENV ${CMAKE_BINARY_DIR}/koenv.sh)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/koenv.sh ${KOENV} @ONLY)
+file(MD5 ${CMAKE_TOOLCHAIN_FILE} CMAKE_TOOLCHAIN_FILE_MD5)
+
+function(external_project_step NAME)
+
+    # Arguments.
+    cmake_parse_arguments(
+        PARSE_ARGV 1
+        # Prefix.
+        ""
+        # Options.
+        "ALWAYS;ONLY_ONCE;USES_TERMINAL"
+        # One value keywords.
+        "DEPFILE;WORKING_DIRECTORY"
+        # Multi-value keywords.
+        "AFTER;BEFORE;COMMAND;COMMENT;DEPENDS;OUTPUTS;TRIGGERS"
+    )
+    if(_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "unparsed arguments: ${_UNPARSED_ARGUMENTS}")
+    endif()
+
+    get_property(PROJECT_STEPS TARGET ${PROJECT_NAME} PROPERTY STEPS)
+    if(NAME IN_LIST PROJECT_STEPS)
+        message(FATAL_ERROR "step already defined: ${NAME}")
+    endif()
+
+    set(SPEC)
+    set(STAMP)
+    if(DEFINED _COMMAND)
+        set(LOGFILE)
+        set(STAMP ${CMAKE_CURRENT_BINARY_DIR}/stamp/${NAME})
+        if(NOT "$ENV{CI}" STREQUAL "")
+            # CI: no need for logging, and don't use the terminal.
+            set(_USES_TERMINAL)
+        elseif(_USES_TERMINAL)
+            set(_USES_TERMINAL USES_TERMINAL)
+        else()
+            set(_USES_TERMINAL)
+            set(LOGFILE ${CMAKE_CURRENT_BINARY_DIR}/log/${NAME})
+        endif()
+        set(ONLY_ONCE 0)
+        if(_ONLY_ONCE)
+            set(ONLY_ONCE 1)
+        endif()
+        string(REGEX REPLACE [[(^|\\;)(COMMAND\\;)+]] [[\1COMMAND\\;run\\;]] _COMMAND "${_COMMAND}")
+        set(SPEC
+            COMMENT "${_COMMENT}"
+            COMMAND : "$<JOIN:$<TARGET_PROPERTY:${PROJECT_NAME}-${NAME},TRIGGERS>, >"
+            COMMAND set --  ${PROJECT_NAME} ${_COMMENT} ${ONLY_ONCE} ${STAMP} ${LOGFILE}
+            COMMAND . ${KOENV}
+            COMMAND ${_COMMAND}
+            DEPENDS $<TARGET_PROPERTY:${PROJECT_NAME}-${NAME},DEPENDS>
+            VERBATIM
+            ${_USES_TERMINAL}
+            WORKING_DIRECTORY ${_WORKING_DIRECTORY}
+        )
+        if(DEFINED _DEPFILE)
+            list(APPEND SPEC DEPFILE ${_DEPFILE})
+        endif()
+    else()
+        if(_ONLY_ONCE OR DEFINED _TRIGGERS)
+            message(FATAL_ERROR "unsupported: with ONLY_ONCE / TRIGGERS and no COMMAND")
+        endif()
+    endif()
+    if(_ALWAYS OR NOT SPEC)
+        add_custom_target(${PROJECT_NAME}-${NAME} ${SPEC} BYPRODUCTS ${_OUTPUTS} ${STAMP})
+    else()
+        add_custom_command(${SPEC} OUTPUT ${_OUTPUTS} ${STAMP})
+        add_custom_target(${PROJECT_NAME}-${NAME} DEPENDS ${STAMP})
+    endif()
+
+    # Update properties.
+    set_property(TARGET ${PROJECT_NAME}-${NAME} APPEND PROPERTY TRIGGERS "${_TRIGGERS}")
+    set_property(TARGET ${PROJECT_NAME}-${NAME} APPEND PROPERTY DEPENDS "${_DEPENDS}")
+    set_property(TARGET ${PROJECT_NAME}-${NAME} PROPERTY STAMP ${STAMP})
+    set_property(TARGET ${PROJECT_NAME} APPEND PROPERTY STEPS ${NAME})
+    set_property(TARGET ${PROJECT_NAME} APPEND PROPERTY BYPRODUCTS ${STAMP})
+    add_dependencies(${PROJECT_NAME} ${PROJECT_NAME}-${NAME})
+
+    # Inter-steps dependencies.
+    set(INTER_STEPS_DEPS)
+    foreach(STEP IN LISTS _AFTER)
+        if(NOT STEP IN_LIST PROJECT_STEPS)
+            message(FATAL_ERROR "invalid AFTER step: ${STEP}")
+        endif()
+        list(APPEND INTER_STEPS_DEPS ${NAME} ${STEP})
+    endforeach()
+    foreach(STEP IN LISTS _BEFORE)
+        if(NOT STEP IN_LIST PROJECT_STEPS)
+            message(FATAL_ERROR "invalid BEFORE step: ${STEP}")
+        endif()
+        list(APPEND INTER_STEPS_DEPS ${STEP} ${NAME})
+    endforeach()
+    while(INTER_STEPS_DEPS)
+        list(POP_FRONT INTER_STEPS_DEPS STEP1 STEP2)
+        add_dependencies(${PROJECT_NAME}-${STEP1} ${PROJECT_NAME}-${STEP2})
+        get_property(STAMP2 TARGET ${PROJECT_NAME}-${STEP2} PROPERTY STAMP)
+        set_property(TARGET ${PROJECT_NAME}-${STEP1} APPEND PROPERTY DEPENDS ${STAMP2})
+    endwhile()
+
+endfunction()
+
+function(external_project)
+
+    # Arguments.
+    cmake_parse_arguments(
+        PARSE_ARGV 0
+        # Prefix.
+        ""
+        # Options.
+        "BUILD_ALWAYS;PROTECTED"
+        # One value keywords.
+        "PATCH_OVERLAY;SOURCE_SUBDIR"
+        # Multi-value keywords.
+        "BUILD_COMMAND;BUILD_DEPENDS;CMAKE_ARGS;CONFIGURE_COMMAND;DOWNLOAD;INSTALL_COMMAND;PATCH_COMMAND;PATCH_FILES"
+    )
+    if(_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "unparsed arguments: ${_UNPARSED_ARGUMENTS}")
+    endif()
+
+    set(OUT_OF_TREE_BUILD TRUE)
+    if(BINARY_DIR STREQUAL SOURCE_DIR)
+        set(OUT_OF_TREE_BUILD FALSE)
+    endif()
+    set(ONLY_ONCE)
+    if(_PROTECTED)
+        set(ONLY_ONCE ONLY_ONCE)
+    endif()
+
+    # Main target.
+    add_custom_target(${PROJECT_NAME} ALL)
+
+    # Download.
+    set(CMD)
+    set(DOWNLOAD_METHOD)
+    set(DOWNLOAD_URL_FILE)
+    set(DOWNLOAD_GIT_REPO_ARGS)
+    set(PROJECT_HASH)
+    if(DEFINED _DOWNLOAD)
+        list(LENGTH _DOWNLOAD LEN)
+        list(POP_FRONT _DOWNLOAD DOWNLOAD_METHOD)
+        if(LEN LESS "3" OR NOT (DOWNLOAD_METHOD STREQUAL "GIT" OR DOWNLOAD_METHOD STREQUAL "URL"))
+            message(FATAL_ERROR "invalid DOWNLOAD arguments: ${_DOWNLOAD}")
+        endif()
+        if(DOWNLOAD_METHOD STREQUAL "GIT")
+            # DOWNLOAD: <revision> <remote> <submodules>*
+            list(POP_FRONT _DOWNLOAD GIT_REVISION GIT_REMOTE)
+            set(GIT_SUBMODULES ${_DOWNLOAD})
+            set(GIT_CLONE ${DOWNLOAD_DIR}/source)
+            list(APPEND CMD COMMAND clone_git_repo ${GIT_CLONE} ${GIT_REMOTE} ${GIT_REVISION} ${GIT_SUBMODULES})
+            set(PROJECT_HASH ${GIT_REVISION})
+        elseif(DOWNLOAD_METHOD STREQUAL "URL")
+            # DOWNLOAD: <md5> <url>+
+            list(GET _DOWNLOAD 0 DOWNLOAD_MD5)
+            list(GET _DOWNLOAD 1 DOWNLOAD_URL_FILE)
+            get_filename_component(DOWNLOAD_URL_FILE ${DOWNLOAD_URL_FILE} NAME)
+            set(DOWNLOAD_URL_FILE ${DOWNLOAD_DIR}/${DOWNLOAD_URL_FILE})
+            list(APPEND CMD COMMAND download_archive ${DOWNLOAD_URL_FILE} ${_DOWNLOAD})
+            set(PROJECT_HASH ${DOWNLOAD_MD5})
+        endif()
+    endif()
+    external_project_step(
+        download
+        COMMENT "Downloading '${PROJECT_NAME}'"
+        COMMAND "${CMD}"
+    )
+    set_property(TARGET ${PROJECT_NAME} PROPERTY HASH ${PROJECT_HASH})
+
+    # Prepare.
+    set(CMD)
+    set(TRIGGERS)
+    # Create source tree.
+    if(DOWNLOAD_METHOD STREQUAL "GIT")
+        list(APPEND CMD COMMAND checkout_git_repo ${SOURCE_DIR} ${GIT_CLONE} ${GIT_REVISION})
+    elseif(DOWNLOAD_METHOD STREQUAL "URL")
+        list(APPEND CMD COMMAND extract_archive ${SOURCE_DIR} ${DOWNLOAD_URL_FILE})
+    endif()
+    # Patch source tree:
+    # - overlay directory
+    # - patch files
+    # - command
+    if(DEFINED _PATCH_OVERLAY OR DEFINED _PATCH_FILES OR DEFINED _PATCH_COMMAND)
+        if(NOT DEFINED _DOWNLOAD)
+            message(FATAL_ERROR "unsupported: with PATCH_FILES and/or PATCH_COMMAND but no DOWNLOAD")
+        endif()
+        list(APPEND CMD COMMAND cd ${SOURCE_DIR})
+    endif()
+    if(DEFINED _PATCH_OVERLAY)
+        get_filename_component(OVERLAY_PATH ${_PATCH_OVERLAY} ABSOLUTE BASE_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+        file(GLOB_RECURSE OVERLAY_FILES ${OVERLAY_PATH}/*)
+        set(OVERLAY_MANIFEST)
+        foreach(F IN LISTS _BEFORE)
+            file(MD5 ${F} MD5)
+            list(APPEND OVERLAY_MANIFEST ${MD5})
+            file(MD5 ${F} MD5)
+        endforeach()
+        string(MD5 MD5 "${OVERLAY_MANIFEST}")
+        list(APPEND TRIGGERS overlay:${MD5})
+        list(APPEND CMD COMMAND ${CMAKE_COMMAND} -E copy_directory ${OVERLAY_PATH} .)
+    endif()
+    if(DEFINED _PATCH_FILES)
+        list(APPEND CMD COMMAND apply_patches)
+        foreach(PATCH IN LISTS _PATCH_FILES)
+            get_filename_component(PATCH_PATH ${PATCH} ABSOLUTE BASE_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+            set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${PATCH_PATH})
+            list(APPEND CMD ${PATCH_PATH})
+            file(MD5 ${PATCH_PATH} MD5)
+            list(APPEND TRIGGERS ${PATCH}:${MD5})
+        endforeach()
+    endif()
+    if(DEFINED _PATCH_COMMAND)
+        list(APPEND CMD COMMAND ${_PATCH_COMMAND})
+    endif()
+    # Generate source list.
+    set(SOURCE_LIST)
+    if(NOT OUT_OF_TREE_BUILD OR NOT _BUILD_ALWAYS)
+        set(SOURCE_LIST "${CMAKE_CURRENT_BINARY_DIR}/source.list")
+        list(APPEND CMD COMMAND list_tree ${SOURCE_DIR} ${SOURCE_LIST})
+    endif()
+    # And build step depfile.
+    set(BUILD_D)
+    if(NOT _BUILD_ALWAYS)
+        set(BUILD_D "${CMAKE_CURRENT_BINARY_DIR}/build.d")
+        list(APPEND CMD COMMAND generate_depfile ${BUILD_D} ${CMAKE_CURRENT_BINARY_DIR}/stamp/build ${SOURCE_LIST})
+    endif()
+    external_project_step(
+        prepare AFTER download
+        COMMENT "Preparing '${PROJECT_NAME}'"
+        COMMAND "${CMD}"
+        TRIGGERS ${TRIGGERS}
+        OUTPUTS ${SOURCE_LIST}
+        ${ONLY_ONCE}
+    )
+
+    # (Configure) dependencies.
+    external_project_step(deps COMMENT "Completed dependencies for '${PROJECT_NAME}'")
+
+    # Configure.
+    set(CMD)
+    if(DEFINED _CMAKE_ARGS OR DEFINED _CONFIGURE_COMMAND)
+        if(DEFINED _CMAKE_ARGS AND DEFINED _CONFIGURE_COMMAND)
+            message(FATAL_ERROR "unsupported: with both CMAKE_ARGS and CONFIGURE_COMMAND")
+        endif()
+        if(DEFINED _CMAKE_ARGS)
+            list(APPEND CMD COMMAND ${CMAKE_COMMAND} -G Ninja
+                -S ${SOURCE_DIR}/${_SOURCE_SUBDIR}
+                -B ${BINARY_DIR} ${_CMAKE_ARGS}
+            )
+        elseif(DEFINED _CONFIGURE_COMMAND)
+            list(APPEND CMD COMMAND cd ${BINARY_DIR})
+            list(APPEND CMD COMMAND ${_CONFIGURE_COMMAND})
+        endif()
+    endif()
+    if(CMD OR _BUILD_COMMAND)
+        # (Re-)configure from a clean slate.
+        if(OUT_OF_TREE_BUILD)
+            list(PREPEND CMD COMMAND rm -rf ${BINARY_DIR} COMMAND mkdir -p ${BINARY_DIR})
+        else()
+            list(PREPEND CMD COMMAND clean_tree ${SOURCE_DIR} ${SOURCE_LIST})
+        endif()
+    endif()
+    external_project_step(
+        configure AFTER deps prepare
+        COMMENT "Configuring '${PROJECT_NAME}'"
+        COMMAND "${CMD}"
+        # Trigger a rebuild on change to compilation flags.
+        TRIGGERS toolchain:${CMAKE_TOOLCHAIN_FILE_MD5}
+        ${ONLY_ONCE}
+    )
+
+    # Build.
+    set(ALWAYS)
+    set(DEPFILE)
+    if(_BUILD_ALWAYS)
+        set(ALWAYS ALWAYS)
+    elseif(BUILD_D)
+        set(DEPFILE DEPFILE ${BUILD_D})
+    endif()
+    if(DEFINED _BUILD_DEPENDS)
+        file(GLOB _BUILD_DEPENDS LIST_DIRECTORIES false ${_BUILD_DEPENDS})
+    endif()
+    external_project_step(
+        build AFTER configure
+        COMMENT "Building '${PROJECT_NAME}'"
+        COMMAND "${_BUILD_COMMAND}"
+        DEPENDS ${_BUILD_DEPENDS}
+        WORKING_DIRECTORY ${BINARY_DIR}
+        USES_TERMINAL
+        ${ALWAYS}
+        ${DEPFILE}
+    )
+
+    # Install.
+    external_project_step(
+        install AFTER build
+        COMMENT "Installing '${PROJECT_NAME}'"
+        COMMAND "${_INSTALL_COMMAND}"
+        WORKING_DIRECTORY ${BINARY_DIR}
+        ${ALWAYS}
+    )
+
+    # Clean target.
+    add_custom_target(
+        ${PROJECT_NAME}-clean
+        COMMENT "Cleaning '${PROJECT_NAME}'"
+        COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}
+    )
+
+endfunction()
--- thirdparty/cmake_modules/koreader_thirdparty_common.cmake
+++ thirdparty/cmake_modules/koreader_thirdparty_common.cmake
@@ -10,10 +10,6 @@ else()
     set(STRIP_CMD ${CMAKE_STRIP} --strip-unneeded)
 endif()
 
-set(KO_MAKE_RECURSIVE ${CMAKE_CURRENT_LIST_DIR}/komake.sh ${MAKE})
-set(KO_NINJA_RECURSIVE ${CMAKE_CURRENT_LIST_DIR}/koninja.sh ${NINJA})
-set(KO_PATCH ${CMAKE_CURRENT_LIST_DIR}/patch-wrapper.sh)
-
 macro(assert_var_defined varName)
     if(NOT DEFINED ${varName})
         message(FATAL_ERROR "${varName} variable not defined!")
@@ -126,18 +122,21 @@ function(append_tree_install_commands CMD_LIST SRC DST)
 endfunction()
 
 function(target_exports TARGET)
-    cmake_parse_arguments("" "" "WRITE_TO_FILE" "CDECLS" ${ARGN})
+    cmake_parse_arguments("" "" "FILELIST_VAR;WRITE_TO_FILE" "CDECLS" ${ARGN})
     if(NOT _CDECLS)
         message(FATAL_ERROR "missing CDECLS argument")
     endif()
-    set(CDECLS_FILES)
+    set(FILELIST)
     foreach(F IN LISTS _CDECLS)
         set(F ${BASE_DIR}/ffi-cdecl/${F}.c)
         if(NOT EXISTS ${F})
             message(FATAL_ERROR "no such FFI cdecl file: ${F}")
         endif()
-        list(APPEND CDECLS_FILES ${F})
+        list(APPEND FILELIST ${F})
     endforeach()
+    if(DEFINED _FILELIST_VAR)
+        set(${_FILELIST_VAR} ${FILELIST} PARENT_SCOPE)
+    endif()
     if(DEFINED _WRITE_TO_FILE)
         set(FNAME ${_WRITE_TO_FILE})
     else()
@@ -145,9 +144,9 @@ function(target_exports TARGET)
     endif()
     set(LINES
         "add_custom_command("
-        "    COMMAND ${BASE_DIR}/utils/gen_linker_exports.sh ${LD} ${TARGET}.link_args ${TARGET}.link_exports ${CDECLS_FILES}"
+        "    COMMAND ${BASE_DIR}/utils/gen_linker_exports.sh ${LD} ${TARGET}.link_args ${TARGET}.link_exports ${FILELIST}"
         "    OUTPUT ${TARGET}.link_args ${TARGET}.link_exports"
-        "    DEPENDS ${BASE_DIR}/utils/gen_linker_exports.sh ${CDECLS_FILES}"
+        "    DEPENDS ${BASE_DIR}/utils/gen_linker_exports.sh ${FILELIST}"
         "    VERBATIM"
         ")"
         "add_custom_target(${TARGET}-link-deps DEPENDS ${TARGET}.link_args ${TARGET}.link_exports)"
@@ -161,199 +160,3 @@ function(target_exports TARGET)
         include(${FNAME})
     endif()
 endfunction()
-
-# Improved external projects support.
-include(ExternalProject)
-# `ExternalProject` step targets fully adopt their steps.
-if(POLICY CMP0114)
-    cmake_policy(SET CMP0114 NEW)
-endif()
-# Transform paths in depfiles to be absolute.
-if(POLICY CMP0116)
-    cmake_policy(SET CMP0116 NEW)
-endif()
-# When extracting an archive, preserve the timestamps (old behavior):
-# new behavior set the timestamps to the time of extraction instead,
-# which can break some builds (e.g. gettext: autotools fails to
-# re-generate `configure` because `configure.ac` is missing).
-if(POLICY CMP0135)
-    cmake_policy(SET CMP0135 OLD)
-endif()
-# Have `ExternalProject_Add()` create a dedicated target for each step.
-set_property(DIRECTORY PROPERTY EP_STEP_TARGETS download patch configure build install)
-# Don't log steps' output on CI builds.
-if(DEFINED ENV{CI})
-    set(LOG_EXTERNAL_PROJECT_STEPS FALSE)
-else()
-    set(LOG_EXTERNAL_PROJECT_STEPS TRUE)
-endif()
-# lint_cmake: -readability/wonkycase
-function(external_project)
-    cmake_parse_arguments(
-        # Prefix.
-        ""
-        # Options.
-        "BUILD_ALWAYS"
-        # One value keywords.
-        "URL_MD5;SOURCE_SUBDIR"
-        # Multi-value keywords.
-        "BUILD_COMMAND;CMAKE_ARGS;CONFIGURE_COMMAND;DOWNLOAD_COMMAND;INSTALL_COMMAND;PATCH_COMMAND;URL"
-        ${ARGN}
-    )
-    if(_UNPARSED_ARGUMENTS)
-        message(FATAL_ERROR "unparsed arguments: ${_UNPARSED_ARGUMENTS}")
-    endif()
-    # Project name.
-    list(APPEND PARAMS ${PROJECT_NAME})
-    # Options.
-    if(_BUILD_ALWAYS)
-        list(APPEND PARAMS BUILD_ALWAYS TRUE)
-    endif()
-    # Dowload parameters.
-    if(DEFINED _URL)
-        list(APPEND PARAMS DOWNLOAD_DIR ${DOWNLOAD_DIR})
-        list(APPEND PARAMS URL ${_URL})
-        list(APPEND PARAMS URL_MD5 ${_URL_MD5})
-    else()
-        list(APPEND PARAMS DOWNLOAD_DIR .)
-        if(DEFINED _DOWNLOAD_COMMAND)
-            list(APPEND PARAMS DOWNLOAD_COMMAND ${_DOWNLOAD_COMMAND})
-        endif()
-    endif()
-    list(APPEND PARAMS LOG_DOWNLOAD ${LOG_EXTERNAL_PROJECT_STEPS})
-    # Source dir.
-    list(APPEND PARAMS SOURCE_DIR ${SOURCE_DIR})
-    if(DEFINED _SOURCE_SUBDIR)
-        list(APPEND PARAMS SOURCE_SUBDIR ${_SOURCE_SUBDIR})
-    endif()
-    # Build dir.
-    # NOTE: don't use `BUILD_IN_SOURCE` when doing an in-source
-    # build as for some reason CMake will still create an empty
-    # build directory with the standard naming scheme.
-    list(APPEND PARAMS BINARY_DIR ${BINARY_DIR})
-    # Install dir.
-    list(APPEND PARAMS INSTALL_DIR ${STAGING_DIR})
-    # Other directories.
-    list(APPEND PARAMS PREFIX .)
-    list(APPEND PARAMS LOG_DIR log)
-    list(APPEND PARAMS TMP_DIR tmp)
-    list(APPEND PARAMS STAMP_DIR stamp)
-    # CMake arguments, patch / configure / install: we need to manually
-    # escape special characters ourselves because CMake does not take
-    # care of that when creating its intermediate script to run each
-    # command (because logging is enabled).
-    foreach(VAR CMAKE_ARGS PATCH_COMMAND CONFIGURE_COMMAND INSTALL_COMMAND)
-        if(DEFINED _${VAR})
-            list(APPEND PARAMS ${VAR})
-            foreach(ARG ${_${VAR}})
-                if(ARG STREQUAL "")
-                    message(FATAL_ERROR "empty argument in ${VAR} command!")
-                endif()
-                if(LOG_EXTERNAL_PROJECT_STEPS)
-                    string(REPLACE "\\" "\\\\" ARG ${ARG})
-                    string(REPLACE "\"" "\\\"" ARG ${ARG})
-                    string(REPLACE "\$" "\\\$" ARG ${ARG})
-                    # Keep generator expressions…
-                    string(REPLACE "\\\$<" "\$<" ARG ${ARG})
-                    # But not $<SEMICOLON>…
-                    string(REPLACE "\$<SEMICOLON>" "\\\$<SEMICOLON>" ARG ${ARG})
-                endif()
-                list(APPEND PARAMS ${ARG})
-            endforeach()
-        endif()
-    endforeach()
-    # Patch command.
-    list(APPEND PARAMS LOG_PATCH ${LOG_EXTERNAL_PROJECT_STEPS})
-    # Configure command.
-    if(DEFINED _CMAKE_ARGS OR DEFINED _CONFIGURE_COMMAND)
-        # NOTE: don't try to log an "empty" configure command…
-        list(APPEND PARAMS LOG_CONFIGURE ${LOG_EXTERNAL_PROJECT_STEPS})
-    else()
-        list(APPEND PARAMS CONFIGURE_COMMAND COMMAND)
-    endif()
-    # Build command.
-    if(DEFINED _BUILD_COMMAND)
-        list(APPEND PARAMS BUILD_COMMAND ${_BUILD_COMMAND})
-    else()
-        list(APPEND PARAMS BUILD_COMMAND COMMAND)
-    endif()
-    if(DEFINED ENV{CI})
-        # Improve parallelization on CI builds.
-        list(APPEND PARAMS USES_TERMINAL_BUILD FALSE)
-    else()
-        list(APPEND PARAMS USES_TERMINAL_BUILD TRUE)
-    endif()
-    # Install command.
-    if(DEFINED _INSTALL_COMMAND)
-        # NOTE: same as above, only work for a non-empty command.
-        list(APPEND PARAMS LOG_INSTALL ${LOG_EXTERNAL_PROJECT_STEPS})
-    else()
-        list(APPEND PARAMS INSTALL_COMMAND COMMAND)
-    endif()
-    # By-products.
-    set(BYPRODUCTS ${CMAKE_CURRENT_BINARY_DIR}/stamp/${PROJECT_NAME}-done)
-    set_property(GLOBAL PROPERTY ${PROJECT_NAME}_BYPRODUCTS ${BYPRODUCTS})
-    # Show a logged step output on failure.
-    list(APPEND PARAMS LOG_OUTPUT_ON_FAILURE TRUE)
-    # Merge stdout & stderr output when logging.
-    list(APPEND PARAMS LOG_MERGED_STDOUTERR TRUE)
-    # Add clean target.
-    add_custom_target(${PROJECT_NAME}-clean
-        # Remove build directory and by-products.
-        COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}
-        # And trigger reconfigure the build the next time.
-        COMMAND touch ${CMAKE_CURRENT_SOURCE_DIR}/CMakeLists.txt
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    )
-    # message(STATUS "ExternalProject_Add(${PARAMS})")
-    ExternalProject_Add(${PARAMS})
-    # Build dependencies.
-    set(BUILD_DEPS ${${PROJECT_NAME}_BUILD_DEPENDS})
-    ExternalProject_Add_Step(${PROJECT_NAME} deps
-        COMMENT "Completed dependencies for '${PROJECT_NAME}'"
-        DEPENDERS configure
-        DEPENDS ${BUILD_DEPS}
-    )
-    ExternalProject_Add_StepTargets(${PROJECT_NAME} deps)
-    if(NOT _BUILD_ALWAYS)
-        set(SOURCE_D ${CMAKE_CURRENT_BINARY_DIR}/source.d)
-        list(APPEND BUILD_DEPS ${SOURCE_D})
-        add_custom_command(
-            COMMENT "Generating source list for '${PROJECT_NAME}'"
-            COMMAND ${THIRDPARTY_DIR}/cmake_modules/mk-source-deps.sh ${SOURCE_D} ${SOURCE_DIR}
-            DEPENDS ${PROJECT_NAME}-configure
-            DEPFILE ${SOURCE_D}
-            OUTPUT ${SOURCE_D}
-            VERBATIM
-        )
-    endif()
-    ExternalProject_Add_Step(${PROJECT_NAME} build-deps
-        COMMENT "Completed build dependencies for '${PROJECT_NAME}'"
-        DEPENDERS build
-        DEPENDS ${BUILD_DEPS}
-    )
-endfunction()
-function(external_project_step STEP_NAME)
-    cmake_parse_arguments("" "USES_TERMINAL" "" "DEPENDEES" ${ARGN})
-    set(PARAMS ${PROJECT_NAME} ${STEP_NAME})
-    if(_USE_TERMINAL)
-        list(APPEND PARAMS USES_TERMINAL TRUE)
-    else()
-        list(APPEND PARAMS
-            USES_TERMINAL FALSE
-            LOG ${LOG_EXTERNAL_PROJECT_STEPS}
-        )
-    endif()
-    list(APPEND PARAMS
-        DEPENDEES mkdir ${_DEPENDEES}
-        WORKING_DIRECTORY ${BINARY_DIR}
-    )
-    if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.19")
-        list(APPEND PARAMS INDEPENDENT TRUE)
-    endif()
-    list(APPEND PARAMS ${_UNPARSED_ARGUMENTS})
-    # message(STATUS "ExternalProject_Add_Step(${PARAMS})")
-    ExternalProject_Add_Step(${PARAMS})
-    ExternalProject_Add_StepTargets(${PROJECT_NAME} ${STEP_NAME})
-endfunction()
-# lint_cmake: +readability/wonkycase
