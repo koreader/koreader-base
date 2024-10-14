17d0b3b4faaaf20d05a60bed1d5f89b1cdce07dc
Benoit Pierre <benoit.pierre@gmail.com>
2024-10-14 17:43:04 +0200

    dropbear: update to 2024.85

---
+++ thirdparty/dropbear/localoptions.h.cmake
@@ -0,0 +1,15 @@
+#cmakedefine DEBUG_TRACE @DEBUG_TRACE@
+#cmakedefine DROPBEAR_DEFPORT "@DROPBEAR_DEFPORT@"
+#cmakedefine01 DROPBEAR_SMALL_CODE
+#cmakedefine01 DROPBEAR_X11FWD
+#cmakedefine01 INETD_MODE
+#cmakedefine01 LOG_COMMANDS
+// Paths.
+#cmakedefine DBSCP_PATH       "@DBSCP_PATH@"
+#cmakedefine DEFAULT_PATH     "@DEFAULT_PATH@"
+#cmakedefine DROPBEAR_PIDFILE "@DROPBEAR_PIDFILE@"
+#cmakedefine SFTPSERVER_PATH  "@SFTPSERVER_PATH@"
+// Keys.
+#cmakedefine DSS_PRIV_FILENAME   "@DSS_PRIV_FILENAME@"
+#cmakedefine RSA_PRIV_FILENAME   "@RSA_PRIV_FILENAME@"
+#cmakedefine ECDSA_PRIV_FILENAME "@ECDSA_PRIV_FILENAME@"
