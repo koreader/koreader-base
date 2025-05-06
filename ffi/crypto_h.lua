392292cc6b9d227327ead2da5b06ef4747042365
TnS-hun <TnS-hun@users.noreply.github.com>
2025-05-06 21:42:09 +0200

    ffi/crypto: AES ECB decryption, PKCS#7 unpadding (#2070)
    This commit makes it possible to decrypt buffers encrypted with AES using the ECB mode, and unpad buffers using the PKCS#7 algorithm.
    
    It uses OpenSSL's EVP cipher routines, which are a high-level interface to certain symmetric ciphers. So it should be easy to add support for other ciphers beside AES if needed.

--- ffi/crypto_h.lua
+++ ffi/crypto_h.lua
@@ -4,4 +4,14 @@ local ffi = require("ffi")
 
 ffi.cdef[[
 int PKCS5_PBKDF2_HMAC_SHA1(const char *, int, const unsigned char *, int, int, int, unsigned char *);
+struct evp_cipher_ctx_st *EVP_CIPHER_CTX_new(void);
+void EVP_CIPHER_CTX_free(struct evp_cipher_ctx_st *);
+int EVP_CIPHER_CTX_set_padding(struct evp_cipher_ctx_st *, int);
+int EVP_CIPHER_block_size(const struct evp_cipher_st *);
+int EVP_DecryptInit_ex(struct evp_cipher_ctx_st *, const struct evp_cipher_st *, struct engine_st *, const unsigned char *, const unsigned char *);
+int EVP_DecryptUpdate(struct evp_cipher_ctx_st *, unsigned char *, int *, const unsigned char *, int);
+int EVP_DecryptFinal_ex(struct evp_cipher_ctx_st *, unsigned char *, int *);
+const struct evp_cipher_st *EVP_aes_128_ecb(void);
+const struct evp_cipher_st *EVP_aes_192_ecb(void);
+const struct evp_cipher_st *EVP_aes_256_ecb(void);
 ]]
