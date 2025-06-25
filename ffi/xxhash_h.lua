61696e013facd869a011ff2b9a397ac92b9855f5
Benoit Pierre <benoit.pierre@gmail.com>
2025-06-25 22:21:14 +0200

    ffi/xxhash: add cdecls

---
+++ ffi/xxhash_h.lua
@@ -0,0 +1,19 @@
+local ffi = require("ffi")
+
+ffi.cdef[[
+
+typedef struct XXH3_state_s XXH3_state_t;
+typedef uint64_t XXH64_hash_t;
+
+typedef enum {
+    XXH_OK = 0,
+    XXH_ERROR = 1,
+} XXH_errorcode;
+
+XXH3_state_t *XXH3_createState(void);
+XXH_errorcode XXH3_freeState(XXH3_state_t *);
+XXH_errorcode XXH3_64bits_reset(XXH3_state_t *);
+XXH_errorcode XXH3_64bits_update(XXH3_state_t *, const void *, size_t);
+XXH64_hash_t XXH3_64bits_digest(const XXH3_state_t *);
+
+]]
