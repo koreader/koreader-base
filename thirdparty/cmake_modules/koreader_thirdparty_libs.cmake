9753b82ce2167959551a0ef5e6f45c4a9b9348b0
Benoit Pierre <benoit.pierre@gmail.com>
2025-07-20 15:24:10 +0200

    unpack: add tool
    A minimal libarchive based archive extractor with support for
    TAR / ZIP archives, and LZMA / GZip / ZSTD / XZ compression.

--- thirdparty/cmake_modules/koreader_thirdparty_libs.cmake
+++ thirdparty/cmake_modules/koreader_thirdparty_libs.cmake
@@ -90,6 +90,7 @@ declare_dependency(harfbuzz::harfbuzz INCLUDES freetype2 harfbuzz MONOLIBTIC har
 
 # libarchive
 declare_dependency(libarchive::libarchive MONOLIBTIC archive)
+declare_dependency(libarchive::libarchive_static STATIC archive)
 
 # leptonica
 declare_dependency(leptonica::leptonica INCLUDES leptonica MONOLIBTIC leptonica)
@@ -238,8 +239,13 @@ declare_dependency(utf8proc::utf8proc MONOLIBTIC utf8proc)
 # xxhash
 declare_dependency(xxhash::xxhash MONOLIBTIC xxhash)
 
+# xz
+declare_dependency(xz::lzma_static STATIC lzma)
+
 # zlib
 declare_dependency(zlib::z MONOLIBTIC z)
+declare_dependency(zlib::z_static STATIC z)
 
 # zstd
 declare_dependency(zstd::zstd MONOLIBTIC zstd)
+declare_dependency(zstd::zstd_static STATIC zstd)
