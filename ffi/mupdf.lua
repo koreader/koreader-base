24ba62948acf20354b8296fc09d280e7482e4da2
Benoit Pierre <benoit.pierre@gmail.com>
2025-08-22 17:09:56 +0200

    mupdf: update to 1.26.6
    https://mupdf.com/releases/history

--- ffi/mupdf.lua
+++ ffi/mupdf.lua
@@ -34,7 +34,7 @@ local mupdf = {
 }
 -- this cannot get adapted by the cdecl file because it is a
 -- string constant. Must match the actual mupdf API:
-local FZ_VERSION = "1.26.3"
+local FZ_VERSION = "1.26.6"
 
 local document_mt = { __index = {} }
 local page_mt = { __index = {} }
