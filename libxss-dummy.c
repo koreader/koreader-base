56d32c658ecc7d077e9663610a3e17c02a71de77
Benoit Pierre <benoit.pierre@gmail.com>
2025-12-20 19:21:09 +0100

    xss-dummy: fix implementation
    Do return something when applicable.

--- libxss-dummy.c
+++ libxss-dummy.c
@@ -4,16 +4,16 @@
 #include <X11/Xlib.h>
 typedef unsigned long ulong;
 typedef unsigned int uint;
-typedef struct { Window window; int state; int kind; ulong til_or_since; ulong idle; ulong eventMask; } XScreenSaverInfo;
-Bool XScreenSaverQueryExtension(Display *dpy, int *event_base_return, int *error_base_return){}
-Status XScreenSaverQueryVersion(Display *dpy, int *major_version_return, int *minor_version_return){}
-XScreenSaverInfo *XScreenSaverAllocInfo(void){}
-Status XScreenSaverQueryInfo(Display *dpy, Drawable drawable, XScreenSaverInfo *saver_info){}
-void XScreenSaverSelectInput(register Display *dpy, Drawable drawable,  ulong mask){}
+typedef struct XScreenSaverInfo XScreenSaverInfo;
+Bool XScreenSaverQueryExtension(Display *dpy, int *event_base_return, int *error_base_return) { return False; }
+Status XScreenSaverQueryVersion(Display *dpy, int *major_version_return, int *minor_version_return) { return False; }
+XScreenSaverInfo *XScreenSaverAllocInfo(void) { return NULL; }
+Status XScreenSaverQueryInfo(Display *dpy, Drawable drawable, XScreenSaverInfo *saver_info) { return 0; }
+void XScreenSaverSelectInput(register Display *dpy, Drawable drawable,  ulong mask) {}
 void XScreenSaverSetAttributes(Display *dpy, Drawable drawable, int x, int y, uint width, uint height,
-  uint border_width, int depth, uint class, Visual *visual, ulong valuemask, XSetWindowAttributes *attributes){}
-void XScreenSaverUnsetAttributes(register Display *dpy, Drawable drawable){}
-Status XScreenSaverRegister(Display *dpy, int screen, XID xid, Atom type){}
-Status XScreenSaverUnregister(Display *dpy, int screen){}
-Status XScreenSaverGetRegistered(Display *dpy, int screen, XID *xid, Atom *type){}
-void XScreenSaverSuspend(Display *dpy, Bool suspend){}
+  uint border_width, int depth, uint class, Visual *visual, ulong valuemask, XSetWindowAttributes *attributes) {}
+void XScreenSaverUnsetAttributes(register Display *dpy, Drawable drawable) {}
+Status XScreenSaverRegister(Display *dpy, int screen, XID xid, Atom type) { return 0; }
+Status XScreenSaverUnregister(Display *dpy, int screen) { return 0; }
+Status XScreenSaverGetRegistered(Display *dpy, int screen, XID *xid, Atom *type) { return 0; }
+void XScreenSaverSuspend(Display *dpy, Bool suspend) {}
