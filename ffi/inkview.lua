5f32c7a1a85396e9ee329a7f54e31cae0df90e86
Benoit Pierre <benoit.pierre@gmail.com>
2026-03-22 20:15:18 +0100

    pocketbook: print inkview software version at load

--- ffi/inkview.lua
+++ ffi/inkview.lua
@@ -7,6 +7,7 @@ char *GetSoftwareVersion();
 
 -- format is $model.$major.$minor.$build, like "U743g.6.8.4143"
 local software_version = ffi.string(inkview.GetSoftwareVersion())
+print(string.format("PocketBook software version: %s", software_version))
 local version_major, version_minor = software_version:match("([1-9][0-9]*)[.]([1-9][0-9]*)[.][1-9][0-9]*$")
 if not version_major or not version_minor then
     error("could not parse PocketBook software version: "..software_version)
