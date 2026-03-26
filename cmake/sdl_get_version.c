2bf72218ad9c4e8fd9ddf826e62632fa2f64a70b
Benoit Pierre <benoit.pierre@gmail.com>
2026-03-26 18:14:35 +0100

    sdl: rework library detection / version check
    - for the version check executable, instead of dynamically linked with the library
      found, use dlopen (so there's no issue when there's a discrepancy between the
      compiler / linker and the dynamic linker search paths)
    - explicitly search `$LD_LIBRARY_PATH` on Linux, the Homebrew prefix on macOS

--- cmake/sdl_get_version.c
+++ cmake/sdl_get_version.c
@@ -1,9 +1,25 @@
+#include <dlfcn.h>
 #include <stdio.h>
 
 int SDL_GetVersion(void);
 
-int main(void) {
-    int version = SDL_GetVersion();
+int main(int argc, char *argv[]) {
+    int (*get_version)();
+    int version;
+    void *lib;
+    if (argc != 2)
+        return 1;
+    lib = dlopen(argv[1], RTLD_LAZY | RTLD_LOCAL);
+    if (!lib) {
+        fprintf(stderr, "dlopen(%s): %s\n", argv[1], dlerror());
+        return 2;
+    }
+    get_version = dlsym(lib, "SDL_GetVersion");
+    if (!get_version) {
+        fprintf(stderr, "dsym(%s): %s\n", "SDL_GetVersion", dlerror());
+        return 2;
+    }
+    version = get_version();
     printf("%d.%d.%d", version / 1000000, version / 1000 % 1000, version % 1000);
     return 0;
 }
