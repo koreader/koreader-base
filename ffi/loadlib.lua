38973edb9a793166dc8ae30e7acf6dc22505bace
Benoit Pierre <benoit.pierre@gmail.com>
2026-05-17 18:31:16 +0200

    ffi/loadlib: support overriding log function
    To reduce chattiness in tools.

--- ffi/loadlib.lua
+++ ffi/loadlib.lua
@@ -18,7 +18,7 @@ ABI compatibility).
 
 local ffi = require("ffi")
 local android = ffi.os == "Linux" and os.getenv("IS_ANDROID") and require("android")
-local log = android and android.LOGI or print
+local log = ffi.log or android and android.LOGI or print
 
 local monolibtic = {
     path = (android and android.nativeLibraryDir or "libs") .. "/libkoreader-monolibtic." .. (ffi.os == "OSX" and "dylib" or "so"),
