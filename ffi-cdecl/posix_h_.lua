baa64b11255aa2c463f76cde2388f787c2474466
Benoit Pierre <benoit.pierre@gmail.com>
2026-06-28 18:34:19 +0200

    ffi/posix_h: fix regression on older glibc (#2418)
    We still need to load librt on old glibc (< 2.17) versions for `clock_gettime` & friends.

--- ffi-cdecl/posix_h_.lua
+++ ffi-cdecl/posix_h_.lua
@@ -14,3 +14,11 @@ local platform = ({{ {1} }})[platform_str]
 if not platform then
     error("unsupported platform: " .. platform_str)
 end
+
+-- clock_gettime & friends require librt on old glibc (< 2.17) versions...
+if ffi.os == "Linux" then
+    -- Load it in the global namespace to make it easier on callers...
+    -- NOTE: There's no librt.so symlink, so, specify the SOVER, but not the full path,
+    --       in order to let the dynamic loader figure it out on its own (e.g.,  multilib).
+    pcall(ffi.load, "rt.so.1", true)
+end
