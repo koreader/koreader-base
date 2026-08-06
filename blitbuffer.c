koreader/koreader-base

commit ce778b689d66a20220ab2d29658e3493e5e02d3b

blitbuffer: set alpha=0xff after BB8A blend, guard stripe_width==0

blitbuffer.c
@@ -656,6 +656,7 @@ void BB_blend_rect(BlitBuffer * restrict bb, unsigned int x, unsigned int y, uns
                     Color8A * restrict dstptr;
                     BB_GET_PIXEL(bb, bb_rotation, Color8A, i, j, &dstptr);
                     dstptr->a = (uint8_t) DIV_255(dstptr->a * ainv + color->a * alpha);
+                    dstptr->alpha = 0xff;
                 }
             }
             break;
@@ -1105,7 +1106,7 @@ void BB_invert_rect(BlitBuffer * restrict bb, unsigned int x, unsigned int y, un
 }
 
 void BB_hatch_rect(BlitBuffer * restrict bb, unsigned int x, unsigned int y, unsigned int w, unsigned int h, unsigned int stripe_width, const Color8 * restrict color, uint8_t alpha) {
-    if (alpha == 0) { // NOP
+    if (alpha == 0 || stripe_width == 0) { // NOP
         return;
     }
     const uint8_t ainv = alpha ^ 0xFF;

commit 3cb0230f369214bdab0cb4dea26bee6b4452eaaa

blifbuffer: refix rounded corners
Regression in https://github.com/koreader/koreader-base/pull/2468

See https://github.com/koreader/koreader-base/pull/2468#issuecomment-5185029994

Overflow is probably of no concern since in C, `r` clamps itself to the buffer size and the Lua side doesn't leave it through if `r` is too large anyway.

blitbuffer.c
@@ -3080,9 +3080,9 @@ void BB_color_blit_from_RGB32(BlitBuffer * restrict dst, const BlitBuffer * rest
 }
 
 // Information about those three algorithms can be found on http://members.chello.at/~easyfilter/ (Zingl Alois)
-void BB_paint_rounded_corner_noAA(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, unsigned int bw, unsigned int r, uint8_t c);
-void BB_paint_rounded_corner_AA(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, unsigned int bw, unsigned int r, uint8_t c);
-void BB_paint_rounded_corner_AA_1px(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, unsigned int r, uint8_t c);
+void BB_paint_rounded_corner_noAA(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, int bw, int r, uint8_t c);
+void BB_paint_rounded_corner_AA(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, int bw, int r, uint8_t c);
+void BB_paint_rounded_corner_AA_1px(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, int r, uint8_t c);
 
 void BB_paint_rounded_corner(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, unsigned int bw, unsigned int r, uint8_t c, int anti_aliasing) {
     /*
@@ -3108,7 +3108,7 @@ void BB_paint_rounded_corner(BlitBuffer * restrict bb, unsigned int off_x, unsig
     }
 }
 
-void BB_paint_rounded_corner_noAA(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, unsigned int bw, unsigned int r, uint8_t c) {
+void BB_paint_rounded_corner_noAA(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, int bw, int r, uint8_t c) {
 
     const int bb_type = GET_BB_TYPE(bb);
     const int bb_rotation = GET_BB_ROTATION(bb);
@@ -3270,7 +3270,7 @@ void BB_paint_rounded_corner_noAA(BlitBuffer * restrict bb, unsigned int off_x, 
     setPixelAA_BBRGB32(off_x+r+x0,    off_y+h-r-1+y0); \
     setPixelAA_BBRGB32(off_x+w-r-1+x1,off_y+h-r-1+y0);
 
-void BB_paint_rounded_corner_AA_1px(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, unsigned int r, uint8_t c) { // draw a black anti-aliased circle with thickness 1
+void BB_paint_rounded_corner_AA_1px(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, int r, uint8_t c) { // draw a black anti-aliased circle with thickness 1
     const int bb_type = GET_BB_TYPE(bb);
     const int bb_rotation = GET_BB_ROTATION(bb);
     const unsigned int bb_width = BB_GET_WIDTH(bb);
@@ -3350,7 +3350,7 @@ void BB_paint_rounded_corner_AA_1px(BlitBuffer * restrict bb, unsigned int off_x
     } while (x < 0);
 }
 
-void BB_paint_rounded_corner_AA(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, unsigned int bw, unsigned int r, uint8_t c )
+void BB_paint_rounded_corner_AA(BlitBuffer * restrict bb, unsigned int off_x, unsigned int off_y, unsigned int w, unsigned int h, int bw, int r, uint8_t c )
 {        // draw a black anti-aliased circle with width bw
     const int bb_type = GET_BB_TYPE(bb);
     const int bb_rotation = GET_BB_ROTATION(bb);